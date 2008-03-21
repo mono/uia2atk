9d3ef8052f979e626d0def2301bb3e37adb4ce60
Andrés G. Aragoneses <knocte@gmail.com>
2008-03-21 08:53:40 +0000

    * mytk.c: No need to check for NULL as append() already mallocs.
    svn path=/trunk/uia2atk/; revision=98721

--- src/tests/unmanaged/atkHelloWorld/mytk.c
+++ src/tests/unmanaged/atkHelloWorld/mytk.c
@@ -5,10 +5,7 @@ static GList* top_level_windows = NULL;
 void
 mytk_add_one_top_level_window(gchar* name)
 {
-  if (!top_level_windows)
-  {
-    top_level_windows = g_list_alloc();
-  }
+  //no need to check for NULL as append() already mallocs
   top_level_windows = g_list_append(top_level_windows, (gpointer)mytk_widget_new(name));
 }
 
