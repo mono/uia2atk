b55e0dd4502f5771429dc2f6e66b126f48945e72
Andrés G. Aragoneses <knocte@gmail.com>
2008-04-23 12:07:49 +0000

    * HelloUtil.cs: Preparation for bound GSignalEmissionHook.
    svn path=/trunk/uia2atk/; revision=101535

--- src/samples/managed/atkSharpHelloWorld/HelloUtil.cs
+++ src/samples/managed/atkSharpHelloWorld/HelloUtil.cs
@@ -5,6 +5,7 @@
 //
 
 using System;
+using System.Collections.Generic;
 
 namespace atkSharpHelloWorld
 {
@@ -33,10 +34,68 @@ namespace atkSharpHelloWorld
 			return "1.1";
 		}
 		
-		internal static int AddGlobalEventListener(IntPtr listener, string event_type)
+		internal static int AddGlobalEventListener(IntPtr listener, string eventType)
 		{
-			//dummy function for now
-			return 0;
+			uint rc = 0;
+			
+			//split_string[0]: toolkit
+			//            [1]: class/interface
+			//            [2]: event type
+			// example: Gtk:AtkObject:children-changed
+			string[] splitString = eventType.Split (':');
+			
+			if ( (splitString != null) &&
+			     (splitString.Length > 2) &&
+			     (String.IsNullOrEmpty (splitString [1])) &&
+			     (String.IsNullOrEmpty (splitString [2])))
+			{
+				rc = KeepListener (listener, splitString [1], splitString [2], eventType);
+			}
+			
+			return (int)rc;
+		}
+		
+		private static uint KeepListener (IntPtr listener, string objType, string signalName, string hookData)
+		{
+			//GLib.GType type = GType.GetFromName (objType);
+			//if (type != null)
+			{
+				GLib.Signal signal; //= GLib.Signal.Lookup (signalName, type);
+				
+				//if (signal != null)
+				{
+					lock (listenerListMutex)
+					{
+						ListenerInfo info = new ListenerInfo();
+						info.Id = (uint) ListenerList.Count + 1;
+						//info.SignalId = signal.Id;
+						//info.HookId = signal.AddEmissionHook (0, listener, hookData);
+						ListenerList.Add (info.Id, info);
+						return info.Id;
+					}
+					
+				}
+				//else
+				{
+					throw new NotSupportedException ("Invalid signal type " + signalName);
+				}
+			}
+			//else
+			{
+				throw new NotSupportedException ("Invalid object type " + objType);
+			}
+		}
+		
+		private static object listenerListMutex = new object ();
+		
+		private static Dictionary <uint, ListenerInfo> ListenerList
+			= new Dictionary<uint, ListenerInfo> ();
+		
+		struct ListenerInfo
+		{
+			internal uint Id;
+			internal uint SignalId;
+			internal ulong HookId;
 		}
 	}
 }
