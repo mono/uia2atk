6917626c532336ea46b0d2551ab52eab35ce3fa1
Andrés G. Aragoneses <knocte@gmail.com>
2008-05-27 12:30:59 +0000

    * BridgeTester.cs, AtkTester.cs: AtkTester is an abstract class   intended to be inherited by the GailTester too, so I've rather   moved Calvin's test to BridgeTester, as they fit more there.
    svn path=/trunk/uia2atk/; revision=104131

--- src/UiaAtkBridge/UiaAtkBridgeTest/AtkTester.cs
+++ src/UiaAtkBridge/UiaAtkBridgeTest/AtkTester.cs
@@ -70,60 +70,6 @@ namespace UiaAtkBridgeTest
 		}
 		
 		//[Test]
-		public void AtkActionImplementor ()
-		{
-			TestButtonControlType pushButton = 
-				new TestButtonControlType("Push Button", false);
-
-			Atk.Action atkAction =
-				new Atk.ActionAdapter(new UiaAtkBridge.Button(pushButton));
-
-			Assert.AreEqual (atkAction.NActions, 1);				
-
-			// only action 0 should work
-			Assert.AreEqual (true, atkAction.DoAction(0));
-			Assert.AreEqual (false, atkAction.DoAction(-1));
-			Assert.AreEqual (false, atkAction.DoAction(1));
-			Assert.AreEqual (false, atkAction.DoAction(2));
-
-			// Should only work on SetDecription for 0
-			Assert.AreEqual (true, atkAction.SetDescription(0, "Some great description"));
-			Assert.AreEqual (false, atkAction.SetDescription(-1, "Some false great description"));
-			Assert.AreEqual (false, atkAction.SetDescription(1, "Some false great description"));
-			Assert.AreEqual (false, atkAction.SetDescription(2, "Some false great description"));
-
-			// Should only work on GetDecription for 0, the rest should be empty string, not null
-			Assert.AreEqual ("Some great description", atkAction.GetDescription(0));
-			Assert.AreEqual ("", atkAction.GetDescription(-1));
-			Assert.AreEqual ("", atkAction.GetDescription(1));
-			Assert.AreEqual ("", atkAction.GetDescription(2));
-
-			// With no keybinding set, everything should return ""
-			Assert.AreEqual ("", atkAction.GetKeybinding(0));
-			Assert.AreEqual ("", atkAction.GetKeybinding(-1));
-			Assert.AreEqual ("", atkAction.GetKeybinding(1));
-			Assert.AreEqual ("", atkAction.GetKeybinding(2));
-
-			pushButton.SetPropertyValue(AutomationElementIdentifiers.AcceleratorKeyProperty.Id, "Magic Key");
-
-			// Should only work on GetKeybinding for 0, the rest should be empty string, not null
-			Assert.AreEqual ("Magic Key", atkAction.GetKeybinding(0));
-			Assert.AreEqual ("", atkAction.GetKeybinding(-1));
-			Assert.AreEqual ("", atkAction.GetKeybinding(1));
-			Assert.AreEqual ("", atkAction.GetKeybinding(2));
-
-			// Should only work on GetKeybinding for 0, the rest should be empty string, not null
-			Assert.AreEqual ("click", atkAction.GetLocalizedName(0));
-			Assert.AreEqual ("", atkAction.GetLocalizedName(-1));
-			Assert.AreEqual ("", atkAction.GetLocalizedName(1));
-			Assert.AreEqual ("", atkAction.GetLocalizedName(2));
-
-			// Should only work on GetKeybinding for 0, the rest should be empty string, not null
-			Assert.AreEqual ("click", atkAction.GetName(0));
-			Assert.AreEqual ("", atkAction.GetName(-1));
-			Assert.AreEqual ("", atkAction.GetName(1));
-			Assert.AreEqual ("", atkAction.GetName(2));
-		}
 		
 		[Test]
 		public void AtkTextImplementor ()
