97774d0b712818c47a713c6a6b62777d849a4820
Mario Carrion <mario@carrion.mx>
2008-05-27 21:18:12 +0000

    Tests added: LinkLabelProvider and TextBoxProvider.
    svn path=/trunk/uia2atk/; revision=104206

---
+++ src/UIAutomationWinforms/UIAutomationWinformsTests/LinkLabelProviderTest.cs
@@ -0,0 +1,123 @@
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject to 
+// the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// 
+// Copyright (c) 2008 Novell, Inc. (http://www.novell.com) 
+// 
+// Authors: 
+//	Mario Carrion <mcarrion@novell.com>
+// 
+
+using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+using Mono.UIAutomation.Winforms;
+using NUnit.Framework;
+
+namespace MonoTests.Mono.UIAutomation.Winforms
+{
+	
+	[TestFixture]
+	public class LinkLabelProviderTest : BaseProviderTest
+	{
+		
+#region Tests
+		
+		[Test]
+		public void BasicPropertiesTest ()
+		{
+			LinkLabel linkLabel = new LinkLabel ();
+			LinkLabelProvider provider  = new LinkLabelProvider (linkLabel);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.LabeledByProperty,
+			              null);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.ControlTypeProperty,
+			              ControlType.Hyperlink.Id);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.LocalizedControlTypeProperty,
+			              "hyperlink");
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsContentElementProperty,
+			              true);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsControlElementProperty,
+			              true);
+		}
+		
+#endregion
+		
+#region IInvokeEvent
+		
+		[Test]
+		public void InvokeEventTest ()
+		{
+			LinkLabel linkLabel = new LinkLabel ();
+			LinkLabelProvider provider = new LinkLabelProvider (linkLabel);
+			
+			IInvokeProvider invokeProvider = (IInvokeProvider) 
+				provider.GetPatternProvider (InvokePatternIdentifiers.Pattern.Id);
+			invokeProvider.Invoke ();
+			
+			bridge.ResetEventLists ();
+			Assert.AreEqual (1,
+			                 bridge.AutomationPropertyChangedEvents.Count,
+			                 "Event count");
+		}
+		
+		[Test]
+		public void InvokeEventDisabledTest ()
+		{
+			LinkLabel linkLabel = new LinkLabel ();
+			LinkLabelProvider provider = new LinkLabelProvider (linkLabel);
+			
+			linkLabel.Enabled = false;
+			
+			IInvokeProvider invokeProvider = (IInvokeProvider) 
+				provider.GetPatternProvider (InvokePatternIdentifiers.Pattern.Id);
+			try {
+				invokeProvider.Invoke ();
+				Assert.Fail ("Exception not thrown");
+			} catch (ElementNotEnabledException) { } 
+		}
+		
+#endregion
+		
+#region BaseProviderTest Overrides
+		
+		protected override IRawElementProviderSimple GetSimpleProvider (Control control)
+		{
+			return new LinkLabelProvider ((LinkLabel) control);
+		}
+		
+		protected override Control GetControlInstance ()
+		{
+			return new LinkLabel ();
+		}
+		
+#endregion
+
+	}
+}
---
+++ src/UIAutomationWinforms/UIAutomationWinformsTests/TextBoxProviderTest.cs
@@ -0,0 +1,220 @@
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject to 
+// the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// 
+// Copyright (c) 2008 Novell, Inc. (http://www.novell.com) 
+// 
+// Authors: 
+//	Mario Carrion <mcarrion@novell.com>
+// 
+
+using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+using Mono.UIAutomation.Winforms;
+using NUnit.Framework;
+
+namespace MonoTests.Mono.UIAutomation.Winforms
+{
+	// TODO: Include ITextProvider, IScrollProvider tests.
+	[TestFixture]
+	public class TextBoxProviderTest : BaseProviderTest
+	{
+		
+#region Tests
+		
+		[Test]
+		public void EditPropertiesTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.ControlTypeProperty,
+			              ControlType.Edit.Id);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.LocalizedControlTypeProperty,
+			              "edit");
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsContentElementProperty,
+			              true);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsControlElementProperty,
+			              true);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsPasswordProperty,
+			              false);
+		}
+		
+		public void DocumentPropertiesTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			textbox.Multiline = true;
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.ControlTypeProperty,
+			              ControlType.Document.Id);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.LocalizedControlTypeProperty,
+			              "document");
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsContentElementProperty,
+			              true);
+			
+			TestProperty (provider,
+			              AutomationElementIdentifiers.IsControlElementProperty,
+			              true);
+		}
+		
+		[Test]
+		public void ValuePatternTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			object valueProvider = provider.GetPatternProvider (ValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNotNull (valueProvider, "Not returning ValuePatternIdentifiers.");
+			Assert.IsTrue (valueProvider is IValueProvider, "Not returning ValuePatternIdentifiers");
+		}
+		
+		[Test]
+		public void TextPatternTest () 
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			object textProvider = provider.GetPatternProvider (TextPatternIdentifiers.Pattern.Id);
+			Assert.IsNotNull (textProvider, "Not returning TextPatternIdentifiers.");
+			Assert.IsTrue (textProvider is ITextProvider, "Not returning TextPatternIdentifiers.");
+		}
+
+		[Test]
+		public void RangeValuePatternTest () 
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			object rangeProvider = provider.GetPatternProvider (RangeValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNull (rangeProvider, "Returned RangeValuePatternIdentifiers.");
+		}
+
+#endregion
+		
+#region IValueProvider Tests
+		
+		[Test]
+		public void IsNotIValueProviderTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			textbox.Multiline = true;
+			
+			object valueProvider = 
+				provider.GetPatternProvider (ValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNull (valueProvider, "Is returning ValuePatternIdentifiers.");
+			
+			textbox.Multiline = false;
+			valueProvider = provider.GetPatternProvider (ValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNotNull (valueProvider, "Is not returning ValuePatternIdentifiers.");
+		}
+		
+		[Test]
+		public void IValueProviderIsReadOnlyTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			IValueProvider valueProvider = (IValueProvider)
+				provider.GetPatternProvider (ValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNotNull (valueProvider, "Not returning ValuePatternIdentifiers.");
+
+			textbox.ReadOnly = true;
+			Assert.AreEqual (valueProvider.IsReadOnly, true, "Is read only");
+			
+			textbox.ReadOnly = false;
+			Assert.AreEqual (valueProvider.IsReadOnly, false, "Is not only");
+		}
+		
+		[Test]
+		public void IValueProviderValueTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			IValueProvider valueProvider = (IValueProvider)
+				provider.GetPatternProvider (ValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNotNull (valueProvider, "Not returning ValuePatternIdentifiers.");
+			
+			Assert.AreEqual (valueProvider.Value, string.Empty, "Empty value");
+
+			string value = "Hello world";
+			
+			textbox.Text = value;
+			Assert.AreEqual (valueProvider.Value, value, "HelloWorld value");
+		}
+		
+		[Test]
+		public void IValueProviderSetValueTest ()
+		{
+			TextBox textbox = new TextBox ();
+			TextBoxProvider provider = new TextBoxProvider (textbox);
+			
+			IValueProvider valueProvider = (IValueProvider)
+				provider.GetPatternProvider (ValuePatternIdentifiers.Pattern.Id);
+			Assert.IsNotNull (valueProvider, "Not returning ValuePatternIdentifiers.");
+			
+			string value = "Hello world";
+
+			valueProvider.SetValue (value);
+			Assert.AreEqual (valueProvider.Value, value, "Different value");
+
+			try {
+				textbox.ReadOnly = true;
+				valueProvider.SetValue (value);
+				Assert.Fail ("ElementNotEnabledException not thrown.");
+			} catch (ElementNotEnabledException) { }
+		}
+		
+		
+#endregion
+		
+#region BaseProviderTest Overrides
+		
+		protected override IRawElementProviderSimple GetSimpleProvider (Control control)
+		{
+			return new TextBoxProvider ((TextBox)control);
+		}
+		
+		protected override Control GetControlInstance ()
+		{
+			return new TextBox ();
+		}
+		
+#endregion
+
+	}
+}
