facfe80f8164d6c33a6c024ee264a068c13a49de
Andrés G. Aragoneses <knocte@gmail.com>
2008-05-30 10:29:13 +0000

    * formtest.sh: Fix Run after the move.
    svn path=/trunk/uia2atk/; revision=104499

--- src/samples/managed/FormTest/formtest.sh
+++ src/samples/managed/FormTest/formtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=../../../../../../../../gtk-sharp/atk/glue/.libs/
+export LD_LIBRARY_PATH=../../../../../../../gtk-sharp/atk/glue/.libs/
 
 exec mono FormTest.exe
