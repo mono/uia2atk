ded167c26040359529b13c9dc3d92b525696b489
Andrés G. Aragoneses <knocte@gmail.com>
2008-06-05 16:43:23 +0000

    * FormTest.mdp, formtest.sh: Better to copy in the script.
    svn path=/trunk/uia2atk/; revision=105063

--- src/samples/managed/FormTest/formtest.sh
+++ src/samples/managed/FormTest/formtest.sh
@@ -2,4 +2,6 @@
 
 export LD_LIBRARY_PATH=../../../../../../../gtk-sharp/atk/glue/.libs/
 
+cp /usr/lib/gtk-2.0/modules/libatk-bridge.so ../../../../../../../gtk-sharp/atk/glue/.libs/ && \
+cp ../../../../../UiaAtkBridge/bridge-glue/bin/Debug/libbridge-glue.so ../../../../../../../gtk-sharp/atk/glue/.libs/ && \
 exec mono FormTest.exe
