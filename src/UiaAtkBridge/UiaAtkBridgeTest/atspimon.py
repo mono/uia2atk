a9e07bb05e4b3f039cb8a124167f8207f2c89980
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2008-06-26 23:00:45 +0000

    change 'source' to an arg instead of an option.  Check to make sure the source app is actually open.  Check for open file before we try to close it.  Update argument parsing to handle xml option.  Add Andrés's code for XML logging.
    svn path=/trunk/uia2atk/; revision=106703

--- src/UiaAtkBridge/UiaAtkBridgeTest/atspimon.py
+++ src/UiaAtkBridge/UiaAtkBridgeTest/atspimon.py
@@ -40,29 +40,34 @@ class Settings(object):
   log_path = None
   source_app = None
   log_file = None
+  xml_format = None
 
   def __init__(self):
     self.argument_parser()
-    try:
-      Settings.log_file = open(Settings.log_path, 'w')
-    except IOError:
-      output("Error opening log file!")
-      abort(1)
+    if Settings.log_path:
+      try:
+        Settings.log_file = open(Settings.log_path, 'w')
+      except IOError:
+        output("Error opening log file!")
+        abort(1)
 
   def help(self):
+    output("Usage:  atspimon.py [-hqlx] NAME")
+    output("Where NAME is the application to monitor.")
+    output('Find NAME in the "Name" column of Accerciser')
+    output("")
     output("Common Options:")
     output("  -h | --help        Print help information (this message).")
     output("  -q | --quiet       Don't print anything.")
     output("  -l | --log=        Where the log should be stored.")
-    output("  -s | --source=     The accessible application to monitor.")
-    output('                     Use the "Name" column in Accerciser.')
+    output("  -x | --xml         Use XML format for events output.")
 
   def argument_parser(self):
     opts = []
     args = []
     try:
       opts, args = getopt.getopt(sys.argv[1:],\
-                     "hql:s:",["help","quiet","log=","source="])
+                     "hxql:s:",["help","xml","quiet","log=","source="])
     except getopt.GetoptError:
       self.help()
 
@@ -75,8 +80,29 @@ class Settings(object):
         abort(0)
       if o in ("-l","--log"):
         Settings.log_path = a
-      if o in ("-s","--source"):
-        Settings.source_app = a
+      if o in ("-x","--xml"):
+        Settings.xml_format = True
+
+    if len(args) > 1:
+      output("You may not specify more than one application to monitor.")
+      self.help()
+    elif len(args) < 1:
+      output("You must specify an application to monitor.")
+      self.help()
+    else:
+      reg = pyatspi.Registry()
+      desktop = reg.getDesktop(0)
+      try:
+        desktop.queryTable()
+      except NotImplementedError:
+        pass
+      apps = [app.name for app in desktop]
+      Settings.source_app = args[0]
+      if not Settings.source_app in apps:
+        output("'%s' not found on the desktop" % Settings.source_app)
+        abort(2)
+
+
 
 class Monitor:
 
@@ -116,15 +142,24 @@ class Monitor:
 
     if not self._eventFilter(event):
       return
+    
+    if Settings.xml_format:
+      output('<event type="%s" detail1="%s" detail2="%s">%s</event>' % 
+                                             (event.type,
+                                             event.detail1,
+                                             event.detail2,
+                                             event.any_data), False)
+      output('\n', False)
+    else:
+      output('%s(%s, %s, %s)\n\tsource: ' % (event.type,
+                                             event.detail1,
+                                             event.detail2,
+                                             event.any_data), False)
+      output(str(event.source), False)
+      output('\n\tapplication: ', False)
+      output(str(event.host_application), False)
+      output('\n', False)
 
-    output('%s(%s, %s, %s)\n\tsource: ' % (event.type,
-                                           event.detail1,
-                                           event.detail2,
-                                           event.any_data), False)
-    output(str(event.source), False)
-    output('\n\tapplication: ', False)
-    output(str(event.host_application), False)
-    output('\n', False)
  
   def begin(self):
     output("Registering Event Listener...")
@@ -137,7 +172,9 @@ class Monitor:
     pyatspi.Registry.deregisterEventListener(self._handleEvent,
                                                *self.events)
     pyatspi.Registry.stop()
-    Settings.log_file.close()
+    if Settings.log_file:
+      if not Settings.log_file.closed:
+        Settings.log_file.close()
 
 class Main(object):
 
