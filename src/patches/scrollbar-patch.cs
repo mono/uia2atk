6f00af3dd26070ef5407528762e249d853d5e12d
Mario Carrion <mario@carrion.mx>
2008-06-30 17:45:32 +0000

    ScrollBar Winform Control patch updated.
    svn path=/trunk/uia2atk/; revision=106906

--- src/patches/scrollbar-patch.cs
+++ src/patches/scrollbar-patch.cs
@@ -1,68 +1,85 @@
 Index: class/Managed.Windows.Forms/System.Windows.Forms/ScrollBar.cs
 ===================================================================
---- class/Managed.Windows.Forms/System.Windows.Forms/ScrollBar.cs	(revision 106688)
+--- class/Managed.Windows.Forms/System.Windows.Forms/ScrollBar.cs	(revision 106905)
 +++ class/Managed.Windows.Forms/System.Windows.Forms/ScrollBar.cs	(working copy)
-@@ -80,6 +80,16 @@
+@@ -80,6 +80,21 @@
  		bool thumb_entered;
  		#endregion	// Local Variables
  
-+		#region UIA Events	
++		#region UIA Framework Events	
 +#if NET_2_0
-+    		// There are used by externally by UIA framework
-+    		internal event EventHandler LargeIncrementEvent;
-+    		internal event EventHandler LargeDecrementEvent;
-+    		internal event EventHandler SmallIncrementEvent;
-+    		internal event EventHandler SmallDecrementEvent;
++    		//UIA Framework events for Invoke Control Pattern used in
++    		//the ScrollBar Provider children buttons.
++    		// - LargeIncrementCalled. Used by the LargeIncrement UIA Button (Space between Thumb and bottom/right Button)
++    		// - LargeIncrementCalled. Used by the LargeDecrement UIA Button (Space between Thumb and top/left Button)
++    		// - SmallIncrementCalled. Used by the SmallIncrement UIA Button (bottom/right Button)
++    		// - SmallDecrementCalled. Used by the SmallDecrement UIA Button (top/left Button)
++    		internal event EventHandler LargeIncrementCalled;
++    		internal event EventHandler LargeDecrementCalled;
++    		internal event EventHandler SmallIncrementCalled;
++    		internal event EventHandler SmallDecrementCalled;
 +#endif
 +		#endregion
 +
  		private enum TimerType
  		{
  			HoldButton,
-@@ -788,6 +798,11 @@
+@@ -788,6 +803,14 @@
  			event_args = new ScrollEventArgs (ScrollEventType.EndScroll, Value);
  			OnScroll (event_args);
      			Value = event_args.NewValue;
 +		
 +#if NET_2_0
-+    			if (LargeIncrementEvent != null)
-+	    			LargeIncrementEvent (this, null);
++			//UIA Framework event invoked when the "LargeIncrement 
++			//Button" is "clicked" either by using the Invoke Pattern
++			//or the space between the thumb and the bottom/right button
++    			if (LargeIncrementCalled != null)
++	    			LargeIncrementCalled (this, null);
 +#endif
      		}
  
      		private void LargeDecrement ()
-@@ -802,6 +817,11 @@
+@@ -802,6 +825,14 @@
  			event_args = new ScrollEventArgs (ScrollEventType.EndScroll, Value);
  			OnScroll (event_args);
      			Value = event_args.NewValue;
 +    			
 +#if NET_2_0
-+    			if (LargeDecrementEvent != null)
-+	    			LargeDecrementEvent (this, null);
++			//UIA Framework event invoked when the "LargeDecrement 
++			//Button" is "clicked" either by using the Invoke Pattern
++			//or the space between the thumb and the top/left button
++    			if (LargeDecrementCalled != null)
++	    			LargeDecrementCalled (this, null);
 +#endif
      		}
  
      		private void OnResizeSB (Object o, EventArgs e)
-@@ -1296,6 +1316,11 @@
+@@ -1296,6 +1327,14 @@
  			event_args = new ScrollEventArgs (ScrollEventType.EndScroll, Value);
  			OnScroll (event_args);
  			Value = event_args.NewValue;
 +			
 +#if NET_2_0
-+    			if (SmallIncrementEvent != null)
-+	    			SmallIncrementEvent (this, null);
++			//UIA Framework event invoked when the "SmallIncrement 
++			//Button" (a.k.a bottom/right button) is "clicked" either
++			// by using the Invoke Pattern or the button itself
++    			if (SmallIncrementCalled != null)
++	    			SmallIncrementCalled (this, null);
 +#endif
      		}
  
      		private void SmallDecrement ()
-@@ -1310,6 +1335,11 @@
+@@ -1310,6 +1349,14 @@
  			event_args = new ScrollEventArgs (ScrollEventType.EndScroll, Value);
  			OnScroll (event_args);
  			Value = event_args.NewValue;
 +			
 +#if NET_2_0
-+    			if (SmallDecrementEvent != null)
-+	    			SmallDecrementEvent (this, null);
++			//UIA Framework event invoked when the "SmallDecrement 
++			//Button" (a.k.a top/left button) is "clicked" either
++			// by using the Invoke Pattern or the button itself
++    			if (SmallDecrementCalled != null)
++	    			SmallDecrementCalled (this, null);
 +#endif			
      		}
  
