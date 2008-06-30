a6858202fd21f7005e25e92eebaf853c358b964c
Andrés G. Aragoneses <knocte@gmail.com>
2008-06-30 18:46:07 +0000

    * formtest.sh: Fix run script to make it work in the parallel mono env.
    svn path=/trunk/uia2atk/; revision=106912

--- src/samples/managed/FormTest/formtest.sh
+++ src/samples/managed/FormTest/formtest.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=../../../../../../../gtk-sharp/atk/glue/.libs/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../../../UiaAtkBridge/bridge-glue/.libs/
 
-cp /usr/lib/gtk-2.0/modules/libatk-bridge.so ../../../../../../../gtk-sharp/atk/glue/.libs/ && \
-cp ../../../../../UiaAtkBridge/bridge-glue/bin/Debug/libbridge-glue.so ../../../../../../../gtk-sharp/atk/glue/.libs/ && \
+if [ -f /usr/lib/gtk-2.0/modules/libatk-bridge.so ]; then
+	echo We are in a 32bits env
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/gtk-2.0/modules/
+elif [ -f /usr/lib64/gtk-2.0/modules/libatk-bridge.so ]; then
+	echo We are in a 64bits env
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/gtk-2.0/modules/
+else
+	echo libatk-bridge.so not found && exit
+fi
+
 exec mono FormTest.exe
