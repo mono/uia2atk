99b4689d661915db5bcb65230c65685fd4c0b743
Andrés G. Aragoneses <knocte@gmail.com>
2008-07-21 16:00:11 +0000

    * gailtest.sh: We need to use NUnit 2.4.7 because it has a feature to   prevent additional appdomains usage (domain=none). However this   feature is undocumented and very awkward to use (that's why we need   to copy all the nunit assemblies into our bin directory).
    svn path=/trunk/uia2atk/; revision=108366

--- src/UiaAtkBridge/GailTest/gailtest.sh
+++ src/UiaAtkBridge/GailTest/gailtest.sh
@@ -2,6 +2,14 @@
 
 export LD_LIBRARY_PATH=../../../../../../gtk-sharp/glib/glue/.libs/
 
-exec nunit-console2 GailTest.dll
+#method for NUnit 2.2.0:
+#exec nunit-console2 GailTest.dll
 
+#method for NUnit 2.4.7
+cp `find /usr/ -name nunit-console.exe 2> /dev/null | grep -v "\.0"` .
+cp `find /usr/ -name nunit-console-runner.dll 2> /dev/null | grep -v "\.0"` .
+cp `find /usr/ -name nunit.util.dll 2> /dev/null | grep -v "\.0" | grep -v NAnt` .
+cp `find /usr/ -name nunit.core.dll 2> /dev/null | grep -v "\.0" | grep -v NAnt | grep -v monodevelop` .
+cp `find /usr/ -name nunit.core.interfaces.dll 2> /dev/null | grep -v "\.0"` .
+exec /usr/bin/mono ./nunit-console.exe GailTest.dll -domain=none
 
