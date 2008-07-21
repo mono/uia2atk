bc337b7fbc1ca469911f2efb2e26d644e43c2783
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2008-07-21 23:01:49 +0000

    * drivers/update_uia2atk_rpms.sh: Bash script to download the latest uia2atk related rpms and install them.  Needs some error checking.
    svn path=/trunk/uia2atk/; revision=108427

---
+++ test/drivers/update_uia2atk_rpms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# get the latest rpms at http://build1.sled.lab.novell.com/uia/
+# and update the machine 
+
+# create a temporary directory
+TD=`mktemp -d`
+cd $TD
+wget --accept=rpm --progress=dot -r -np -nd -l1 http://build1.sled.lab.novell.com/uia/
+rpm -Uvh *.rpm
+
+# destroy the evidence
+cd ..
+rpm -r $TD
