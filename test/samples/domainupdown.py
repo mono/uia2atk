47370315af76e19a7c5e2c21a6d5f965416019a0
Ray Wang <raywang@mono-cvs.ximian.com>
2008-08-06 09:15:41 +0000

    DomainUpDown control implemented
    svn path=/trunk/uia2atk/; revision=109744

---
+++ test/samples/domainupdown.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env ipy
+
+##############################################################################
+# Written by:  Ray Wang <rawang@novell.com>
+# Date:        08/06/2008
+# Description: the sample for winforms control:
+#              DomainUpDown
+##############################################################################
+
+# The docstring below is used in the generated log file
+"""
+This sample will show "DomainUpDown" control in the form.
+It can be used for Autotest tools(e.g. Strongwind) to test the behaviors of controls.
+"""
+
+# imports
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+
+from System.Windows.Forms import Application, Form, Label, DomainUpDown
+from System.Drawing import Point, Size
+
+class DomainUpDownSample(Form):
+    """DomainUpDown control class"""
+
+    def __init__(self):
+        """DomainUpDownSample class init function."""
+
+        # set up form
+        self.Text = "DomainUpDown control"
+
+        # set up label
+        self.label = Label()
+        self.label.Size = Size(250, 30)
+        self.label.Text = "Up/Down DomainUpDown control to see the items"
+        self.label.Location = Point(10, 0)
+
+        # set up domainupdown
+        self.domain_up_down = DomainUpDown()
+        self.domain_up_down.Location = Point(10, 50)
+        self.domain_up_down.Sorted = True
+        self.domain_up_down.Items.Add("Austin")
+        self.domain_up_down.Items.Add("Beijing")
+        self.domain_up_down.Items.Add("Cambridge")
+        self.domain_up_down.Items.Add("Madrid")
+        self.domain_up_down.Items.Add("Provo")
+        self.domain_up_down.Items.Add("San Diego")
+        self.domain_up_down.SelectedItemChanged += self.selected_item_changed
+
+        # add control
+        self.Controls.Add(self.label)
+        self.Controls.Add(self.domain_up_down)
+
+    def selected_item_changed(self, sender, event):
+        self.label.Text = "SelectedIndex: " + \
+            str(self.domain_up_down.SelectedIndex) + \
+            "\n" + \
+            "SelectedItem: "  + \
+            str(self.domain_up_down.SelectedItem)
+
+# run application
+form = DomainUpDownSample()
+Application.EnableVisualStyles()
+Application.Run(form)
--- test/samples/numericupdown.py
+++ test/samples/numericupdown.py
@@ -21,9 +21,6 @@ class NumericUpDownApp(Form):
     def __init__(self):
         self.Text = "NumericUpDown Example"
 
-        self.Width = 400
-        self.Height = 400
-
         self.mainLabel1 = Label()
         self.mainLabel1.Text = "Examples for: NumericUpDown"
         self.mainLabel1.Location = Point(10,10)
@@ -36,9 +33,13 @@ class NumericUpDownApp(Form):
         self.numercupdown1.Maximum = 2500
         self.numercupdown1.Minimum = -100
         self.numercupdown1.UserEdit = True
+        self.numercupdown1.ValueChanged += self.value_changed
 
         self.Controls.Add(self.numercupdown1)
 
+    def value_changed(self, sender, event):
+        self.mainLabel1.Text = "The Current Value of the counter is: %d" % \
+                                self.numercupdown1.Value
 
 form = NumericUpDownApp()
 Application.Run(form)
