d2d07cae305d8b0da70b9fc2f024152691059bbd
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2008-08-12 06:31:59 +0000

    Added checking for sort order (ascending vs. descending).  Wanted to clean up this test and add checking for sort order so the test could be used as an example on http://www.mono-project.com/Accessibility:_Testing_Howto
    svn path=/trunk/uia2atk/; revision=110207

--- test/testers/gtktreeview/gtktreeviewframe.py
+++ test/testers/gtktreeview/gtktreeviewframe.py
@@ -8,15 +8,14 @@
 
 import sys
 import os
-import gtkactions as a
-
+ 
 from strongwind import *
 from gtktreeview import *
-
-
+ 
+ 
 # class to represent the main window.
 class GtkTreeViewFrame(accessibles.Frame):
-
+ 
     # constants
     # the available widgets on the window
     COLUMN_ZERO = "Column 0"
@@ -24,7 +23,27 @@ class GtkTreeViewFrame(accessibles.Frame):
     PARENT_TWO = "parent 1"
     PARENT_TREE = "parent 2"
     PARENT_FOUR = "parent 3"
-
+ 
+    # the ordering of table cell names we expect when we call the
+    # findAllTableCells method
+    ASCENDING = ('parent 0', 'parent 1', 'parent 2', 'parent 3',
+                 'child 0 of parent 0', 'child 1 of parent 0',
+                 'child 2 of parent 0', 'child 0 of parent 1',
+                 'child 1 of parent 1', 'child 2 of parent 1',
+                 'child 0 of parent 2', 'child 1 of parent 2',
+                 'child 2 of parent 2', 'child 0 of parent 3',
+                 'child 1 of parent 3', 'child 2 of parent 3')
+    DESCENDING = ('parent 3', 'parent 2', 'parent 1', 'parent 0',
+                  'child 2 of parent 3', 'child 1 of parent 3',
+                  'child 0 of parent 3', 'child 2 of parent 2',
+                  'child 1 of parent 2', 'child 0 of parent 2',
+                  'child 2 of parent 1', 'child 1 of parent 1',
+                  'child 0 of parent 1', 'child 2 of parent 0',
+                  'child 1 of parent 0', 'child 0 of parent 0')
+ 
+    # Use the constructor to find some of the accessibles we will be testing
+    # and store an object for each accessible in a variable that can be used
+    # from the test script.
     def __init__(self, accessible):
         super(GtkTreeViewFrame, self).__init__(accessible)
         self.column0 = self.findTableColumnHeader(self.COLUMN_ZERO)
@@ -32,40 +51,71 @@ class GtkTreeViewFrame(accessibles.Frame):
         self.parent1 = self.findTableCell(self.PARENT_TWO)
         self.parent2 = self.findTableCell(self.PARENT_TREE)
         self.parent3 = self.findTableCell(self.PARENT_FOUR)
-
-    # set expand to send "expand or contract" action. now i know why my
-    # returning couldn't be divided regularly, because strongwind haven't set
-    # this action, so we need define it with procedurelogger.action to return
-    # "Action:" info.
-    def expand(self, treeview):
-        procedurelogger.action('expand %s.' % treeview)
-        treeview.expandOrContract()
-
-    #set contract to send "expand or contract" action. same as expend
-    def contract(self, treeview):
-        procedurelogger.action('contract %s.' % treeview)
-        treeview.expandOrContract()
-
-    #set Click action for TableColumnHeader
-    def tchClick (self,test):
-        procedurelogger.action('click %s.' % test)
-        treeview = self.findTableColumnHeader("%s" % test)
-        treeview.click()
-
-    #check if status list in "interface viewer" in accerciser have "expanded" 
-    #status when doing expand or contract action.
+ 
+    # expand accessible by performing an "expand or contract" action.
+    # this method will also contract the accessible if used out of order
+    def expand(self, parent):
+        procedurelogger.action('expand %s.' % parent)
+        parent.expandOrContract()
+ 
+    # contract accessible by performing an "expand or contract" action.
+    # this method will also expand the accessible if used out of order
+    def contract(self, parent):
+        procedurelogger.action('contract %s.' % parent)
+        parent.expandOrContract()
+ 
+    # perform a click action for TableColumnHeader
+    def tchClick (self, thc):
+        procedurelogger.action('click %s.' % thc)
+        thc.click()
+ 
+    # assert that the accessible has the "contracted" state after performing
+    # the "expand or contract" action.  states can be found on the "status" list
+    # on the "interface viewer" tab in Accerciser.
     def assertContracted(self, accessible):
         'Raise exception if accessible does not match the given result'   
-	procedurelogger.expectedResult('%s is %s.' % (accessible, "contracted"))
+        procedurelogger.expectedResult('%s is %s.' % (accessible, "contracted"))
         def resultMatches():
             return not accessible.expanded
-	
+        
         assert retryUntilTrue(resultMatches)
-
+ 
+    # assert that the accessible has the "expanded" state after performing
+    # the "expand or contract" action.
     def assertExpanded(self, accessible):
         'Raise exception if accessible does not match the given result'   
-	procedurelogger.expectedResult('%s is %s.' % (accessible, "expanded"))
+        procedurelogger.expectedResult('%s is %s.' % (accessible, "expanded"))
         def resultMatches():
             return accessible.expanded
-	
+        
+        assert retryUntilTrue(resultMatches)
+ 
+    # assert that the sorting of the TreeView is ascending by comparing the 
+    # order in which the table cells are found to the order we expect them
+    # to be in (self.ASCENDING).
+    def assertAscending(self):
+        'Raise exception if the sorting of the tree view is not ascending'   
+        procedurelogger.expectedResult('TreeView sorting is ascending')
+        self.table_cells = self.findAllTableCells(None, checkShowing=False)
+        tcs = [table_cell.name for table_cell in  self.table_cells]
+ 
+        def resultMatches():
+            self.table_cells = self.findAllTableCells(None, checkShowing=False)
+            return tuple(tcs) == self.ASCENDING
+ 
+        assert retryUntilTrue(resultMatches)
+ 
+    # assert that the sorting of the TreeView is descending by comparing the 
+    # order in which the table cells are found to the order we expect them
+    # to be in (self.DESCENDING).
+    def assertDescending(self):
+        'Raise exception if the sorting of the tree view is not descending'   
+        procedurelogger.expectedResult('TreeView sorting is descending')
+        self.table_cells = self.findAllTableCells(None, checkShowing=False)
+        tcs = [table_cell.name for table_cell in  self.table_cells]
+ 
+        def resultMatches():
+            self.table_cells = self.findAllTableCells(None, checkShowing=False)
+            return tuple(tcs) == self.DESCENDING
+ 
         assert retryUntilTrue(resultMatches)
