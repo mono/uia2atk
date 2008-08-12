09caec8b3641dfa3cfc8a4ef3c0cd0cdc89c0f88
Andrés G. Aragoneses <knocte@gmail.com>
2008-08-12 15:46:12 +0000

    * gailtest.sh: Fixes for the mono parallel env.
    svn path=/trunk/uia2atk/; revision=110251

--- src/UiaAtkBridge/GailTest/gailtest.sh
+++ src/UiaAtkBridge/GailTest/gailtest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=../../../../../../gtk-sharp/glib/glue/.libs/
-
 #method for NUnit 2.2.0:
 #exec nunit-console2 GailTest.dll
 
@@ -11,5 +9,5 @@ cp `find /usr/ -name nunit-console-runner.dll 2> /dev/null | grep -v "\.0"` .
 cp `find /usr/ -name nunit.util.dll 2> /dev/null | grep -v "\.0" | grep -v NAnt` .
 cp `find /usr/ -name nunit.core.dll 2> /dev/null | grep -v "\.0" | grep -v NAnt | grep -v monodevelop` .
 cp `find /usr/ -name nunit.core.interfaces.dll 2> /dev/null | grep -v "\.0"` .
-exec /usr/bin/mono ./nunit-console.exe GailTest.dll -nothread -domain=none
+mono ./nunit-console.exe GailTest.dll -nothread -domain=none
 
