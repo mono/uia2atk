7ee709a02c8035b7aa4c91c75fbbb2cb61aef0c9
Andrés G. Aragoneses <knocte@gmail.com>
2008-09-16 14:03:51 +0000

    * bridgetest.sh: Remove hacks only needed as workarounds to launch with   option "-domain=none" (only needed in GailTest).
    svn path=/trunk/uia2atk/; revision=113157

--- src/UiaAtkBridge/UiaAtkBridgeTest/bridgetest.sh
+++ src/UiaAtkBridge/UiaAtkBridgeTest/bridgetest.sh
@@ -6,9 +6,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../bridge-glue/.libs/
 #exec nunit-console2 UiaAtkBridgeTest.dll $* 
 
 #method for NUnit 2.4.7
-cp `find /usr/ -name nunit-console.exe 2> /dev/null | grep -v "\.0"` .
-cp `find /usr/ -name nunit-console-runner.dll 2> /dev/null | grep -v "\.0"` .
-cp `find /usr/ -name nunit.util.dll 2> /dev/null | grep -v "\.0" | grep -v NAnt` .
-cp `find /usr/ -name nunit.core.dll 2> /dev/null | grep -v "\.0" | grep -v NAnt | grep -v monodevelop` .
-cp `find /usr/ -name nunit.core.interfaces.dll 2> /dev/null | grep -v "\.0"` .
-mono ./nunit-console.exe UiaAtkBridgeTest.dll -nothread -domain=none
+mono `find /usr/ -name nunit-console.exe 2> /dev/null | grep -v "\.0"` UiaAtkBridgeTest.dll -nothread
