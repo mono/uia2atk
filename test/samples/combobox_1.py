7142122355750c22c38c1393da102f8c4be6d3bf
Ray Wang <raywang@mono-cvs.ximian.com>
2008-09-17 09:49:12 +0000

    add 2 sample and change textbox.py
    svn path=/trunk/uia2atk/; revision=113266

---
+++ test/samples/combobox_1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env ipy
+
+##############################################################################
+# Written by:  Ray Wang <rawang@novell.com>
+# Date:        09/17/2008
+# Description: the sample for winforms control:
+#              ComboBox
+##############################################################################
+
+# The docstring below is used in the generated log file
+"""
+Test accessibility of "ComboBox" control
+"""
+
+# imports
+import clr
+clr.AddReference('System.Windows.Forms')
+from System.Windows.Forms import Application, Form, ComboBox, Label, DockStyle, ComboBoxStyle
+
+class ComboBoxSample(Form):
+    """ComboBox control class"""
+
+    def __init__(self):
+        """ComboBoxSample class init function."""
+
+        # setup title
+        self.Text = "ComboBox control"
+
+        # setup label
+        self.label = Label()
+        self.label.Text = "You select " 
+        self.label.AutoSize = True
+        self.label.Dock = DockStyle.Top
+
+        # setup combobox
+        self.combobox = ComboBox()
+        self.combobox.Dock = DockStyle.Top
+        self.combobox.SelectionChangeCommitted += self.select
+        self.combobox.DropDownStyle = ComboBoxStyle.Simple
+        self.combobox.SelectedIndexChanged += self.select
+
+
+        # add items in ComboBox
+        for i in range(10):
+            self.combobox.Items.Add(str(i))
+
+        # add controls
+        self.Controls.Add(self.combobox)
+        self.Controls.Add(self.label)
+
+    # ComboBox click event
+    def select(self, sender, event):
+        """select a item"""
+        self.label.Text = "You select " + self.combobox.Text
+
+# run application
+form = ComboBoxSample()
+Application.EnableVisualStyles()
+Application.Run(form)
--- test/samples/textbox.py
+++ test/samples/textbox.py
@@ -20,80 +20,60 @@ from System.Windows.Forms import *
 class TextBoxButtonApp(Form):
 
     def __init__(self):
-        self.Text = "Simple TextBox Example"
-        self.Height = 500
-        self.Width = 350
-        self.FormBorderStyle = FormBorderStyle.Fixed3D
+        self.Text = "TextBox Control"
 
-        self.label = Label()
-        self.label.Text = "there is nothing"
-        self.label.Location = Point(25,25)
-        self.label.Height = 60
-        self.label.Width = 150
+        self.label1 = Label()
+        self.label1.Text = "Normal TextBox"
+        self.label1.Dock = DockStyle.Top
     
         self.textbox1 = TextBox()
-        self.textbox1.Text = "the default text"
-        self.textbox1.Location = Point(25,100)
-        self.textbox1.Width = 150
-        self.textbox1.Height = 60
+        self.textbox1.AcceptsTab = True
+        self.textbox1.AcceptsReturn = True
+        self.textbox1.Dock = DockStyle.Top
+        self.textbox1.Name = "self.textbox1"
+        self.textbox1.TextChanged += self.textbox1_enter
 
-##some others properties of TextBox
-        self.textbox1.Multiline = True
-        self.textbox1.ScrollBars = ScrollBars.Vertical #method:Horizontal, Both, None, Vertical
-        self.textbox1.AcceptsReturn = True #can use RETURN keyboard
-        self.textbox1.AcceptsTab = True #can use TAB keyboard
-        self.textbox1.WordWrap = True #auto wrap to the next line
-
-        self.button1 = Button()
-        self.button1.Text = "Accept"
-        self.button1.ForeColor = Color.Red
-        self.button1.Location = Point(75,180)
-        self.button1.Click += self.accept
-
-        self.button2 = Button()
-        self.button2.Text = "Reset"
-        self.button2.BackColor = Color.Green
-        self.button2.Location = Point(155,180)
-        self.button2.Click += self.reset
-
-##create a password entry textbox to display asterisks instead of the text typed
-        self.label1 = Label()
-        self.label1.Text = "your passwd is:"
-        self.label1.Location = Point(25,220)
-        self.label1.Size = Size(150,20)
+        # create a password entry textbox to display asterisks 
+        # instead of the text typed
+        self.label2 = Label()
+        self.label2.Text = "Multi-Line TextBox"
+        self.label2.Dock = DockStyle.Top
+        self.label2.Height = 80
 
         self.textbox2 = TextBox()
-        self.textbox2.Text = ""
-        self.textbox2.Location = Point(25,260)
-        self.textbox2.Size = Size(150,20)
-        self.textbox2.UseSystemPasswordChar = True
+        self.textbox2.Dock = DockStyle.Top
+        self.textbox2.Height = 100
+        self.textbox2.Multiline = True
+        self.textbox2.ScrollBars = ScrollBars.Both
+        self.textbox2.AcceptsTab = True
+        self.textbox2.AcceptsReturn = True
+        self.textbox2.WordWrap = False
+        self.textbox2.TextChanged += self.textbox2_enter
 
-        self.button3 = Button()
-        self.button3.Text = "Add Password"
-        self.button3.Location = Point(75,280)
-        self.button3.Click += self.passwdSet
+        self.label3 = Label()
+        self.label3.Text = "Password TextBox"
+        self.label3.Dock = DockStyle.Top
 
-        self.AcceptButton = self.button1 #define press button1 can accept the text
-        self.CancelButton = self.button2 #define press button1 will never accept the text
-        self.AcceptButton = self.button3
+        self.textbox3 = TextBox()
+        self.textbox3.Dock = DockStyle.Top
+        self.textbox3.UseSystemPasswordChar = True
+        self.textbox3.TextChanged += self.textbox3_enter
 
-        self.Controls.Add(self.label)
-        self.Controls.Add(self.textbox1)
-        self.Controls.Add(self.button1)
-        self.Controls.Add(self.button2)
-        self.Controls.Add(self.label1)
+        self.Controls.Add(self.textbox3)
+        self.Controls.Add(self.label3)
         self.Controls.Add(self.textbox2)
-        self.Controls.Add(self.button3)
+        self.Controls.Add(self.label2)
+        self.Controls.Add(self.textbox1)
+        self.Controls.Add(self.label1)
 
-    def accept(self, sender, event):
-        self.label.Text = self.textbox1.Text
+    def textbox1_enter(self, sender, event):
+        self.label1.Text = self.textbox1.Text
 
-    def reset(self, sender, event):
-        self.label.Text = "nothing so far"
-        self.textbox1.Text = "the default text"
-    
-    def passwdSet(self, sender, event):
-        self.label1.Text = "your passwd is: %s" % self.textbox2.Text
+    def textbox2_enter(self, sender, event):
+        self.label2.Text = self.textbox2.Text
+
+    def textbox3_enter(self, sender, event):
+        self.label3.Text = self.textbox3.Text
 
 form = TextBoxButtonApp()
 Application.Run(form)
