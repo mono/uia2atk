0b79a88269e2bd55899c4a7fd7b2e73ff32f5df0
Calen Chen <cachen@mono-cvs.ximian.com>
2008-09-22 06:59:54 +0000

    *  testers/checkbox/checkboxframe.py: modify statesCheck to new way *  testers/checkbox_basic_ops_states.py: update comments and add test to move focus and add test to check states's change
    svn path=/trunk/uia2atk/; revision=113681

--- test/testers/checkbox/checkboxframe.py
+++ test/testers/checkbox/checkboxframe.py
@@ -32,39 +32,77 @@ class CheckBoxFrame(accessibles.Frame):
         self.check3 = self.findCheckBox(self.CHECK_THREE)
         self.check4 = self.findCheckBox(self.CHECK_FORE)
 
-    #check checkbox's all expectant actions
+    #Check CheckBox's actions
     def actionsCheck(self, accessible):
-        procedurelogger.action('diff %s\'s actions list' % accessible)
-        ca = accessible._accessible.queryAction()
-        initallists = []
-        for lists in range(ca.nActions):
-            initallists .append(ca.getName(lists))
+        procedurelogger.action('Check %s\'s actions' % accessible)
 
-        procedurelogger.expectedResult('%s\'s inital actions "%s" live up to our expectation' % (accessible,initallists))
-        def resultMatches():
-            return sorted(initallists) == sorted(actions.CheckBox.actions)
-        assert retryUntilTrue(resultMatches)
+        # get list of expected actions 
+        expected_actions = actions.CheckBox.actions
+
+        # get list of actual actions of the accessible
+        qa = accessible._accessible.queryAction()
+        actual_actions = [qa.getName(i) for i in range(qa.nActions)]
+
+        procedurelogger.expectedResult('Actions: %s' % actual_actions)
+
+        # get a list of actual states that are missing or extraneous
+        missing_actions = set(expected_actions).difference(set(actual_actions))
+        extra_actions = set(actual_actions).difference(set(expected_actions))
+
+        # if missing_actions and extra_actions are empty, the test case passes
+        # otherwise, throw an exception
+        is_same = len(missing_actions) == 0 and len(extra_actions) == 0
+        assert is_same, "\n  %s: %s\n  %s: %s" %\
+                                             ("Missing actual actions: ",
+                                               missing_actions,
+                                              "Extraneous actual actions: ",
+                                               extra_actions) 
 
     #check checkbox's all expectant states
-    def statesCheck(self, accessible):
-        procedurelogger.action('check %s\'s all states' % accessible)
+    def statesCheck(self, accessible, control,
+                                invalid_states=[], add_states=[]):
+        """Check the states of an accessible using the default states
+        of the accessible (specified by control class in states.py) as
+        the default expected states.
+       
+        Keyword arguments:
+        accessible -- the accessible whose states will be checked
+        control -- the class name of the control whose states we want to check
+        invalid_states -- a list of states that should be removed from the
+        list of default expected states
+        add_states -- a list of states that should be added to the list of
+        default expected states
 
-        procedurelogger.expectedResult('%s\'s all states can be found' % accessible)
-        for a in states.CheckBox.states:
-            state = getattr(accessible, a)
-            assert state, "Expected state: %s" % (a)
+        """
+        procedurelogger.action('Check %s\'s states' % accessible)
+        # create a list of all states for button except "sensitive"
+        states_list = states.__getattribute__(control).states
+        expected_states = \
+                  [s for s in states_list if s not in invalid_states]
+        expected_states = set(expected_states).union(set(add_states))
 
-    #check disable checkbox's states
-    def statesDisableCheck(self, accessible):
-        procedurelogger.action('check %s\'s all states' % accessible)
+        procedurelogger.expectedResult('States:  %s' % expected_states)
 
-        procedurelogger.expectedResult('%s\'s all states can\'t be found except "showing"' % accessible)
-        for a in states.CheckBox.states:
-            state = getattr(accessible, a)
-            if a == 'showing':
-                assert state, "Expected state: %s" % (a)
-            else:
-                assert not state, "Not expected state: %s" % (a)
+        # get a list of all actual states for accessible
+        actual_states = accessible._accessible.getState().getStates()
+        # need to convert the numbers retrieved above into their associated
+        # strings
+        actual_states = [pyatspi.stateToString(s) for s in actual_states]
+
+        # assert there are no elements in expected_states that are not
+        # in actual_states
+        missing_states = set(expected_states).difference(set(actual_states))
+
+        # assert there are no elements in actual_states that are not
+        # in expected_states
+        extra_states = set(actual_states).difference(set(expected_states))
+
+        is_same = len(missing_states) == 0 and len(extra_states) == 0
+        assert is_same, "\n  %s: %s\n  %s: %s" %\
+                                             ("Missing actual states: ",
+                                               missing_states,
+                                              "Extraneous actual states: ",
+                                               extra_states) 
 
 
     #give 'click' action
