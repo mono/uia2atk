6cd82ffaa68dd25b5adcba4d9d4ea2c6549b1621
Ray Wang <raywang@mono-cvs.ximian.com>
2008-09-27 02:24:24 +0000

    add a CheckedListBox control which has CheckOnClick property is False.
    svn path=/trunk/uia2atk/; revision=114254

--- test/samples/checkedlistbox.py
+++ test/samples/checkedlistbox.py
@@ -28,36 +28,63 @@ class CheckedListBoxSample(Form):
         self.Text = "CheckedListBox control"
 
         # setup label
-        self.label = Label()
-        #self.label.Height = 100
-        self.label.AutoSize = True
-        self.label.Dock = DockStyle.Top
-        self.label.Text = ""
+        self.label1 = Label()
+        self.label1.AutoSize = True
+        self.label1.Dock = DockStyle.Top
+        self.label1.Text = "CheckOnClick True"
         
-        # setup checkedlistbox
-        self.checkedlistbox = CheckedListBox()
-        self.checkedlistbox.Dock = DockStyle.Top
-        self.checkedlistbox.SelectedIndexChanged += self.change
-        self.checkedlistbox.CheckOnClick = True
+        self.label2 = Label()
+        self.label2.AutoSize = True
+        self.label2.Dock = DockStyle.Top
+        self.label2.Text = "CheckOnClick False"
+        
+        # setup checkedlistboxs
+        self.checkedlistbox1 = CheckedListBox()
+        self.checkedlistbox1.Dock = DockStyle.Top
+        self.checkedlistbox1.SelectedIndexChanged += self.change
+        self.checkedlistbox1.CheckOnClick = True
+
+        self.checkedlistbox2 = CheckedListBox()
+        self.checkedlistbox2.Dock = DockStyle.Top
+        self.checkedlistbox2.SelectedIndexChanged += self.change
+        self.checkedlistbox2.CheckOnClick = False
 
         # add items in CheckedListBox
         for i in range(20):
-            self.checkedlistbox.Items.Add(str(i))
+            self.checkedlistbox1.Items.Add(str(i))
+            self.checkedlistbox2.Items.Add(str(i))
 
         # add controls
-        self.Controls.Add(self.checkedlistbox)
-        self.Controls.Add(self.label)
+        self.Controls.Add(self.checkedlistbox2)
+        self.Controls.Add(self.label2)
+        self.Controls.Add(self.checkedlistbox1)
+        self.Controls.Add(self.label1)
 
     # CheckedListBox check change event
     def change(self, sender, event):
         """select a item"""
 
-        items = "" 
-        for i in range(20):
-            status = str(self.checkedlistbox.GetItemCheckState(i))
-            if status == "Checked":
-                items += "%d " % i
-        self.label.Text = "Item " + items + ": " + "Checked"
+#        items = "" 
+#        for i in range(20):
+#            status = str(self.checkedlistbox1.GetItemCheckState(i))
+#            if status == "Checked":
+#                items += "%d " % i
+
+        if sender is self.checkedlistbox1:
+            items = self.check_items(self.checkedlistbox1)
+            self.label1.Text = "Item " + items + ": " + "Checked"
+
+        if sender is self.checkedlistbox2:
+            items = self.check_items(self.checkedlistbox2)
+            self.label2.Text = "Item " + items + ": " + "Checked"
+
+    def check_items(self, control):
+       items = "" 
+       for i in range(20):
+           status = str(control.GetItemCheckState(i))
+           if status == "Checked":
+               items += "%d " % i
+       return items
 
 # run application
 form = CheckedListBoxSample()
