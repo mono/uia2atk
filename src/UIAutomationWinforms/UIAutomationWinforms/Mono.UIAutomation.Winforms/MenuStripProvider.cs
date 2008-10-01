77a24fddee7fb3e236705b9e3cb9b0f2914115d5
Sandy Armstrong <sanfordarmstrong@gmail.com>
2008-10-01 05:06:09 +0000

    * UIAutomationWinforms/UIAutomationWinforms/Makefile.am,   UIAutomationWinforms/configure.ac,   UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms.mdp:   Require glib-sharp, atk-sharp, and Mono.WebBrowser for use in the   WebBrowserProvider.  Add MenuStripProvider and WebBrowserProvider   to build. * UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms/MenuStripProvider.cs:   Beginning of MenuStripProvider implementation.  Not yet enabled in   ProviderFactory. * UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms/WebBrowserProvider.cs:   Beginning of WebBrowserProvider implementation.  Not yet enabled in   ProviderFactory.
    svn path=/trunk/uia2atk/; revision=114548

---
+++ src/UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms/MenuStripProvider.cs
@@ -0,0 +1,113 @@
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject to 
+// the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// 
+// Copyright (c) 2008 Novell, Inc. (http://www.novell.com) 
+// 
+// Authors: 
+//      Sandy Armstrong <sanfordarmstrong@gmail.com>
+// 
+
+
+using System;
+using System.Windows.Forms;
+
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+
+using AEIds = System.Windows.Automation.AutomationElementIdentifiers;
+
+namespace Mono.UIAutomation.Winforms
+{
+	internal class MenuStripProvider : FragmentRootControlProvider
+	{
+		private MenuStrip menu;
+		
+		public MenuStripProvider (MenuStrip menu) : base (menu)
+		{
+			this.menu = menu;
+			menu.ItemAdded += OnItemAdded;
+			menu.ItemRemoved += OnItemRemoved;
+
+			foreach (ToolStripItem item in menu.Items) {
+
+			}
+		}
+
+		public override object GetPropertyValue (int propertyId)
+		{
+			if (propertyId == AEIds.ControlTypeProperty.Id)
+				return ControlType.MenuBar.Id;
+			else if (propertyId == AEIds.LocalizedControlTypeProperty.Id)
+				return "menu bar";
+			else if (propertyId == AEIds.NameProperty.Id)
+				return null;
+			else if (propertyId == AEIds.LabeledByProperty.Id)
+				return null;
+			else if (propertyId == AEIds.OrientationProperty.Id) {
+				switch (menu.Orientation) {
+				case Orientation.Vertical:
+					return OrientationType.Vertical;
+				case Orientation.Horizontal:
+				default:
+					return OrientationType.Horizontal;
+				}
+			} else if (propertyId == AEIds.AccessKeyProperty.Id)
+				return "ALT";
+			else if (propertyId == AEIds.IsKeyboardFocusableProperty.Id)
+				return true;
+			else
+				return base.GetPropertyValue (propertyId);
+		}
+
+		private void OnItemAdded (object sender, ToolStripItemEventArgs e)
+		{
+
+		}
+
+		private void OnItemRemoved (object sender, ToolStripItemEventArgs e)
+		{
+
+		}
+	}
+
+	// TODO: Move to own file
+	internal class ToolStripItemProvider : FragmentRootControlProvider
+	{
+		private ToolStripItem item;
+
+		public ToolStripItemProvider (ToolStripItem item) : base (item)
+		{
+			this.item = item;
+		}
+
+		public override object GetPropertyValue (int propertyId)
+		{
+			if (propertyId == AEIds.ControlTypeProperty.Id)
+				return ControlType.MenuItem.Id;
+			else if (propertyId == AEIds.LocalizedControlTypeProperty.Id)
+				return "menu item";
+			else if (propertyId == AEIds.NameProperty.Id)
+				return item.Text;
+			else if (propertyId == AEIds.LabeledByProperty.Id)
+				return null;
+			else
+				return base.GetPropertyValue (propertyId);
+		}
+	}
+}
