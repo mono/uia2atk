05bf32ba4f0cfeb44856aea247a12c373cf8ac72
Andrés G. Aragoneses <knocte@gmail.com>
2008-10-03 21:01:56 +0000

    * atspimon.py: Doh! Fix string. * UiaAtkBridgeTest/Makefile.am: Fix build.
    svn path=/trunk/uia2atk/; revision=114789

--- src/UiaAtkBridge/atspimon.py
+++ src/UiaAtkBridge/atspimon.py
@@ -101,7 +101,7 @@ class Settings(object):
       apps = [app.name for app in desktop if app is not None]
       for source_app in self.source_apps:
         if source_app not in apps:
-          output("The app '%s' were found on the desktop now, so waiting for it to appear..." % source_app)
+          output("The app '%s' was not found on the desktop now, so waiting for it to appear..." % source_app)
 
 class Monitor:
 
