f9183085833c0ccb44686249c674b7a26b02dd86
Mario Carrion <mario@carrion.mx>
2008-10-13 16:03:58 +0000

    2008-10-13  Mario Carrion <mcarrion@novell.com>
    * UIAutomationWinforms/Mono.UIAutomation.Winforms/AssemblyInfo.cs: 
      InternalVisibleTo attribute added.
    * UIAutomationWinformsTests/Makefile.am: Assembly signing added.
    * UIAutomationWinformsTests/AssemblyInfo.cs: Updated to handle valid
      assembly signature.
    
    
    svn path=/trunk/uia2atk/; revision=115664

--- src/UIAutomationWinforms/UIAutomationWinformsTests/AssemblyInfo.cs
+++ src/UIAutomationWinforms/UIAutomationWinformsTests/AssemblyInfo.cs
@@ -31,19 +31,19 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("UIAutomationWinformsTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Novell, Inc.")]
+[assembly: AssemblyProduct("UIAutomationWinformsTests")]
+[assembly: AssemblyCopyright("Copyright © 2008 Novell, Inc")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // If the build and revision are set to '*' they will be updated automatically.
 
-[assembly: AssemblyVersion("1.0.*.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
+//[assembly: AssemblyKeyFile("")]
