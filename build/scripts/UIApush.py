138f552eff73fc54dbc6a60daa9f86cc0c38f19d
Stephen Shaw <sshaw@decriptor.com>
2008-10-22 03:32:23 +0000

    Minor updates
    svn path=/trunk/uia2atk/; revision=116726

--- build/scripts/UIApush.py
+++ build/scripts/UIApush.py
@@ -2,10 +2,11 @@
 #!/usr/bin/env python
 import commands as c
 import os
+import subprocess
 import tempfile
 import time
 
-base_path = "/home/sshaw/wa/msvn/release/packaging/snapshot_packages"
+base_path = "/home/builder/wa/msvn/release/packaging/snapshot_packages"
 
 dir32 = {"mono":"/x86/mono",
          "olive":"/noarch/olive",
@@ -41,8 +42,8 @@ rmrpmlist = """
 current_date = time.strftime("%m%d%y")
 
 # Create both 32 and 64 tmp directories
-tmp_path_32 = tempfile.mkdtemp()
-tmp_path_64 = tempfile.mkdtemp()
+tmp_path_32 = tempfile.mkdtemp(prefix="UIA_")
+tmp_path_64 = tempfile.mkdtemp(prefix="UIA_")
 
 # Print file headers for both 32 and 64 bit files
 output_file_32 = open(os.path.join(tmp_path_32, "rpm_revs"), 'w')
@@ -53,6 +54,7 @@ output_file_64.write('RPM (64bit) Rev values for - ' + current_date + '\n\n\n')
 
 # Fill 32 bit directories
 def get32():
+
     for d in dir32:
         b = base_path + dir32[d]
         print b
@@ -60,27 +62,34 @@ def get32():
         print rev_dir
         os.chdir(b + "/" + rev_dir)
         cpcmd = 'cp *.rpm %s' % tmp_path_32
+        print cpcmd
         c.getoutput(cpcmd)
         output_file_32.write(d + " - " + rev_dir + "\n")
+    output_file_32.close()
 
 # Fill 64 bit directories
 def get64():
+
     for d in dir64:
         b = base_path + dir64[d]
         print b
         rev_dir = getLatestDir(b)
         print rev_dir
         os.chdir(b + "/" + rev_dir)
-        cpcmd = 'cp *.rpm %s' % tmp_path_64
+        cpcmd = "cp *.rpm %s" % tmp_path_64
+        print cpcmd
         c.getoutput(cpcmd)
         output_file_64.write(d + " - " + rev_dir + "\n")
+    output_file_64.close()
 
 def getLatestDir(dir):
+
     os.chdir(dir)
     lscmd = "ls -rtd */ | tail -n 1"
     return c.getoutput(lscmd)
 
 def removerpms():
+
     os.chdir(tmp_path_32)
     for rpm in rmrpmlist:
         rmcmd = 'rm %s' % rpm
@@ -92,6 +101,7 @@ def removerpms():
 
 
 def Stage2():
+
     #ssh root@build1.sled.lab.novell.com /root/bin/UIAupdate
     print "ssh - UIAupdate"
     c.getoutput('ssh builder@build1.sled.lab.novell.com /home/builder/bin/UIAupdate')
@@ -99,19 +109,28 @@ def Stage2():
     c.getoutput('ssh builder@build1.sled.lab.novell.com /home/builder/bin/UIAupdate64')
 
     #scp /tmp/uia/* root@build1.sled.lab.novell.com:/srv/www/htdocs/uia/current
-    cpcmd = 'scp %s builder@build1.sled.lab.novell.com:/srv/www/htdocs/uia/current' % tmp_path_32
+    cpcmd = 'scp %s/* builder@build1.sled.lab.novell.com:/srv/www/htdocs/uia/current' % tmp_path_32
     print "scp - 32"
     c.getoutput(cpcmd)
-    cpcmd = 'scp %s builder@build1.sled.lab.novell.com:/srv/www/htdocs/uia/64/current' % tmp_path_64
+    cpcmd = 'scp %s/* builder@build1.sled.lab.novell.com:/srv/www/htdocs/uia/64/current' % tmp_path_64
     print "scp - 64"
     c.getoutput(cpcmd)
 
+def Cleanup():
+
+    rmcmd = 'rm -r %s' %tmp_path_32
+    c.getoutput(rmcmd)
+    rmcmd = 'rm -r %s' %tmp_path_64
+    c.getoutput(rmcmd)
+
+
 def RunTests():
-    #ssh qa@uiaqa.sled.lab.novell.com python /var/qa/code/test/harness/remote_run.py -su -e mono-a11y@forge.novell.com,bgmerrell@novell.com,stshaw@novell.com -f bgmerrell@novell.com
-    results = c.getoutput('ssh qa@uiaqa.sled.lab.novell.com python /var/qa/code/test/harness/remote_run.py -su -e mono-a11y@forge.novell.com,bgmerrell@novell.com,stshaw@novell.com -f bgmerrell@novell.com')
-    # If smoke tests pass, run regression tests
-    if(results):
-        c.getoutput('ssh qa@uiaqa.sled.lab.novell.com python /var/qa/code/test/harness/remote_run.py -su -e mono-a11y@forge.novell.com,bgmerrell@novell.com,stshaw@novell.com -f bgmerrell@novell.com')
+
+    print "Running Smoke Tests"
+    results = c.getoutput('ssh qa@uiaqa.sled.lab.novell.com python /var/qa/code/test/harness/remote_run.py -s')
+    if(results == 0):
+        print "Running Regression Tests"
+        c.getoutput('ssh qa@uiaqa.sled.lab.novell.com python /var/qa/code/test/harness/remote_run.py')
 
 def SendNotifications():
     #mailx stshaw@novell.com -s 'New RPMS - http://build1.sled.lab.novell.com/uia/current' -c bgmerrell@novell.com -c rawang@novell.com -c cachen@novell.com -c ngao@novell.com < /tmp/uia/rpm_revs
@@ -119,13 +138,16 @@ def SendNotifications():
     #c.getoutput(cmdstr)
     #cmdstr = "mailx stshaw@novell.com -s 'New RPMS - http://build1.sled.lab.novell.com/uia/current' -c bgmerrell@novell.com -c rawang@novell.com -c cachen@novell.com -c ngao@novell.com < %s/rpm_revs" % tmp_path_32
     #c.getoutput(cmdstr)
-    mailcmd = "mailx stshaw@novell.com -s 'New RPM (32 and 64)'"
-    c.getoutput(mailcmd)
+    subprocess.call(['mailx', 'stshaw@novell.com', '-s', 'New RPM (32 and 64)'], stdin=open('/tmp/UIAoutput'))
+#    mailcmd = "mailx stshaw@novell.com -s 'New RPM (32 and 64)' < /tmp/UIAoutput"
+ #   c.getoutput(mailcmd)
 
 if __name__ == "__main__":
     get32()
     get64()
     removerpms()
     Stage2()
+    Cleanup()
     RunTests()
     SendNotifications()
+
