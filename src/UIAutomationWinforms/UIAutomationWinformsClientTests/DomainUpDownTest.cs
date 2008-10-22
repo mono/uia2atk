bd3bfb88da7c7760ee2f3cb2f049f87dda801337
Neville Gao <ngao@mono-cvs.ximian.com>
2008-10-22 09:29:03 +0000

    Added DomainUpDownTest.
    svn path=/trunk/uia2atk/; revision=116729

---
+++ src/UIAutomationWinforms/UIAutomationWinformsClientTests/DomainUpDownTest.cs
@@ -0,0 +1,236 @@
+﻿// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject to 
+// the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// 
+// Copyright (c) 2008 Novell, Inc. (http://www.novell.com) 
+// 
+// Authors: 
+//	Neville Gao <nevillegao@gmail.com>
+// 
+using System;
+using System.Windows.Automation;
+using System.Windows.Forms;
+using NUnit.Framework;
+
+namespace MonoTests.Mono.UIAutomation.Winforms.Client
+{
+        // According to http://msdn.microsoft.com/en-us/library/ms744847.aspx
+        [TestFixture]
+        [Description("Tests SWF.DomainUpDown as ControlType.Spinner")]
+        public class DomainUpDownTest : BaseTest
+        {
+                #region Properties Test
+
+                [Test]
+                [Description("Value: See notes. | Notes: The value of this property needs to be unique "
+                        + "across all controls in an application.")]
+                public override void MsdnAutomationIdPropertyTest()
+                {
+                        base.MsdnAutomationIdPropertyTest();
+                }
+
+                [Test]
+                [Description("Value: See notes. | Notes: The outermost rectangle that contains the whole "
+                        + "control.")]
+                public override void MsdnBoundingRectanglePropertyTest()
+                {
+                        base.MsdnBoundingRectanglePropertyTest();
+                }
+
+                [Test]
+                [Description("Value: See notes. | Notes: The spinner control's clickable point gives "
+                        + "focus to the edit portion of the control.")]
+                public override void MsdnClickablePointPropertyTest()
+                {
+                        base.MsdnClickablePointPropertyTest();
+                }
+
+                [Test]
+                [Description("Value: See notes. | Notes: If the control can receive keyboard focus, it "
+                        + "must support this property.")]
+                public override void MsdnIsKeyboardFocusablePropertyTest()
+                {
+                        base.MsdnIsKeyboardFocusablePropertyTest();
+                }
+
+                [Test]
+                [LameSpec]
+                [Description("Value: See notes. | Notes: The spinner control typically gets its name "
+                        + "from a static text label.")]
+                public override void MsdnNamePropertyTest()
+                {
+                        DomainUpDown domainUpdown = GetControl() as DomainUpDown;
+                        AutomationElement child = GetAutomationElementFromControl(domainUpdown);
+                        Assert.AreEqual(domainUpdown.Name,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.NameProperty, true),
+                                "NameProperty");
+
+                        domainUpdown.Name = "Unhappy domainUpDown";
+                        Assert.AreEqual(domainUpdown.Name,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.NameProperty, true),
+                                "NameProperty");
+                }
+
+                [Test]
+                [LameSpec]
+                [Description("Value: See notes. | Notes: Spinner controls have a static text label.")]
+                public override void MsdnLabeledByPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(null,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.LabeledByProperty, true),
+                                "LabeledByProperty");
+                }
+
+                [Test]
+                [Description("Value: Spinner | Notes: This value is the same for all UI frameworks.")]
+                public override void MsdnControlTypePropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(ControlType.Spinner,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.ControlTypeProperty, true),
+                                "ControlTypeProperty");
+                }
+
+                [Test]
+                [Description("Value: \"spinner\" | Notes: Localized string corresponding to the Spinner "
+                        + "control type.")]
+                public override void MsdnLocalizedControlTypePropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual("spinner",
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.LocalizedControlTypeProperty, true),
+                                "LocalizedControlType");
+                }
+
+                [Test]
+                [Description("Value: True | Notes: The spinner control must always be content.")]
+                public override void MsdnIsContentElementPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(true,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.IsContentElementProperty, true),
+                                "IsContentElementProperty");
+                }
+
+                [Test]
+                [LameSpec]
+                [Description("Value: True | Notes: The Spinner control must always be a control.")]
+                public override void MsdnIsControlElementPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(true,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.IsControlElementProperty, true),
+                                "IsControlElementProperty");
+                }
+
+                [Test]
+                [NotListed]
+                [Description("Is not listed. We are using false as default value.")]
+                public override void MsdnIsOffscreenPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(false,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.IsOffscreenProperty, true),
+                                "IsOffscreenProperty");
+                }
+
+                [Test]
+                [NotListed]
+                [Description("Is not listed. We are using null as default value.")]
+                public override void MsdnOrientationPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(null,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.OrientationProperty, true),
+                                "OrientationProperty");
+                }
+
+                [Test]
+                [NotListed]
+                [Description("Is not listed. We are using null as default value.")]
+                public override void MsdnHelpTextPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(null,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.HelpTextProperty, true),
+                                "HelpTextProperty");
+                }
+
+                [Test]
+                [NotListed]
+                [Description("Is not listed. We are using null as default value.")]
+                public override void MsdnAcceleratorKeyPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(null,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.AcceleratorKeyProperty, true),
+                                "AcceleratorKeyProperty");
+                }
+
+                [Test]
+                [NotListed]
+                [Description("Is not listed. We are using string.Empty as default value.")]
+                public override void MsdnAccessKeyPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(string.Empty,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.AccessKeyProperty, true),
+                                "AccessKeyProperty");
+                }
+
+                [Test]
+                [NotListed]
+                [Description("Is not listed. We are using false as default value.")]
+                public override void MsdnIsPasswordPropertyTest()
+                {
+                        AutomationElement child = GetAutomationElement();
+                        Assert.AreEqual(false,
+                                child.GetCurrentPropertyValue(AutomationElementIdentifiers.IsPasswordProperty, true),
+                                "IsPasswordProperty");
+                }
+
+                #endregion
+
+                #region Pattern Tests
+
+                [Test]
+                [Description("Support/Value: Depends | Notes: Controls that use a Spinner to set a "
+                        + "value field should support this pattern.")]
+                public override void MsdnValuePatternTest()
+                {
+                        AutomationElement element = GetAutomationElement();
+                        Assert.IsTrue(SupportsPattern(element, ValuePattern.Pattern),
+                                "ValuePattern SHOULD BE supported");
+                }
+                                
+                #endregion
+
+                #region Protected Methods
+
+                protected override Control GetControl()
+                {
+                        DomainUpDown domainUpDown = new DomainUpDown();
+                        domainUpDown.Name = "I'm a happy SWF domainUpDown :)";
+
+                        return domainUpDown;
+                }
+
+                #endregion
+        }
+}
