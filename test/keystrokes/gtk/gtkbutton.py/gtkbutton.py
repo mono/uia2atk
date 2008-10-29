5a9edce71bae1ceb8f43320f0ccacada8257db6f
Ray Wang <raywang@mono-cvs.ximian.com>
2008-10-29 10:17:21 +0000

    add gtkbutton keystrokes script
    svn path=/trunk/uia2atk/; revision=117363

---
+++ test/keystrokes/gtk/gtkbutton.py/gtkbutton.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+sequence.append(PauseAction(5000))
+sequence.append(WaitForWindowActivate("Buttons",None))
+sequence.append(KeyComboAction("Tab"))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForFocus("Button 1", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+sequence.append(KeyComboAction("space"))
+sequence.append(WaitForWindowActivate("Message Dialog",None))
+sequence.append(KeyComboAction("<ALT>F4"))
+sequence.append(utils.AssertPresentationAction(
+    "Button 1",
+    ["BRAILLE LINE:  'gtkbutton.py Application Message Dialog Dialog'",
+    "     VISIBLE:  'Message Dialog Dialog', cursor=1",
+    "BRAILLE LINE:  'gtkbutton.py Application Buttons Frame'",
+    "     VISIBLE:  'Buttons Frame', cursor=1",
+    "BRAILLE LINE:  'gtkbutton.py Application Buttons Frame Button 1 Button'",
+    "     VISIBLE:  'Button 1 Button', cursor=1",
+    "SPEECH OUTPUT: 'Message Dialog'",
+    "SPEECH OUTPUT: 'Buttons frame'",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'Button 1 button'"]))
+sequence.append(KeyComboAction("Tab"))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForFocus("Button 2", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+sequence.append(KeyComboAction("space"))
+sequence.append(WaitForWindowActivate("Message Dialog",None))
+sequence.append(KeyComboAction("<ALT>F4"))
+sequence.append(utils.AssertPresentationAction(
+    "Button 2",
+    ["BRAILLE LINE:  'gtkbutton.py Application Message Dialog Dialog'",
+    "     VISIBLE:  'Message Dialog Dialog', cursor=1",
+    "BRAILLE LINE:  'gtkbutton.py Application Buttons Frame'",
+    "     VISIBLE:  'Buttons Frame', cursor=1",
+    "BRAILLE LINE:  'gtkbutton.py Application Buttons Frame Button 2 Button'",
+    "     VISIBLE:  'Button 2 Button', cursor=1",
+    "SPEECH OUTPUT: 'Message Dialog'",
+    "SPEECH OUTPUT: 'Buttons frame'",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'Button 2 button'"]))
+sequence.append(KeyComboAction("Tab"))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForFocus("openSUSE", acc_role=pyatspi.ROLE_PUSH_BUTTON))
+sequence.append(KeyComboAction("space"))
+sequence.append(WaitForWindowActivate("Message Dialog",None))
+sequence.append(KeyComboAction("<ALT>F4"))
+sequence.append(utils.AssertPresentationAction(
+    "openSUSE Button",
+    ["BRAILLE LINE:  'gtkbutton.py Application Message Dialog Dialog'",
+    "     VISIBLE:  'Message Dialog Dialog', cursor=1",
+    "BRAILLE LINE:  'gtkbutton.py Application Buttons Frame'",
+    "     VISIBLE:  'Buttons Frame', cursor=1",
+    "BRAILLE LINE:  'gtkbutton.py Application Buttons Frame openSUSE Button'",
+    "     VISIBLE:  'openSUSE Button', cursor=1",
+    "SPEECH OUTPUT: 'Message Dialog'",
+    "SPEECH OUTPUT: 'Buttons frame'",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'openSUSE button'"]))
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
