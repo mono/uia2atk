e44082c8f4afc00111d5b8d2244a54873563d993
Ray Wang <raywang@mono-cvs.ximian.com>
2008-11-03 10:34:17 +0000

    update orca winform button keystrokes
    svn path=/trunk/uia2atk/; revision=117731

--- test/keystrokes/winforms/winbutton.py/winbutton.py
+++ test/keystrokes/winforms/winbutton.py/winbutton.py
@@ -7,24 +7,21 @@ sequence = MacroSequence()
 
 sequence.append(PauseAction(5000))
 sequence.append(WaitForWindowActivate("Buttons",None))
-#sequence.append(KeyComboAction("Tab"))
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(WaitForFocus("Button 1", acc_role=pyatspi.ROLE_PUSH_BUTTON))
 sequence.append(KeyComboAction("space"))
-sequence.append(PauseAction(1000))
+sequence.append(WaitForWindowActivate("Message Dialog",None))
 sequence.append(KeyComboAction("<ALT>F4"))
-sequence.append(PauseAction(1000))
+sequence.append(WaitForWindowActivate("Buttons",None))
 sequence.append(utils.AssertPresentationAction(
     "Button 1",
-    ["BRAILLE LINE:  'ipy Application Message Dialog Dialog'",
-    "     VISIBLE:  'Message Dialog Dialog', cursor=1",
-    "BRAILLE LINE:  'ipy Application Buttons Frame'",
-    "     VISIBLE:  'Buttons Frame', cursor=1",
+    ["BRAILLE LINE:  'ipy Application Message Dialog Frame OK Button'",
+    "     VISIBLE:  'OK Button', cursor=1",
     "BRAILLE LINE:  'ipy Application Buttons Frame Button 1 Button'",
     "     VISIBLE:  'Button 1 Button', cursor=1",
-    "SPEECH OUTPUT: 'Message Dialog'",
-    "SPEECH OUTPUT: 'Buttons frame'",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'OK button'",
     "SPEECH OUTPUT: ''",
     "SPEECH OUTPUT: 'Button 1 button'"]))
 sequence.append(KeyComboAction("Tab"))
@@ -33,16 +30,15 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(WaitForFocus("Button 2", acc_role=pyatspi.ROLE_PUSH_BUTTON))
 sequence.append(KeyComboAction("space"))
 sequence.append(KeyComboAction("<ALT>F4"))
+sequence.append(WaitForWindowActivate("Buttons",None))
 sequence.append(utils.AssertPresentationAction(
     "Button 2",
-    ["BRAILLE LINE:  'ipy Application Message Dialog Dialog'",
-    "     VISIBLE:  'Message Dialog Dialog', cursor=1",
-    "BRAILLE LINE:  'ipy Application Buttons Frame'",
-    "     VISIBLE:  'Buttons Frame', cursor=1",
+    ["BRAILLE LINE:  'ipy Application Message Dialog Frame OK Button'",
+    "     VISIBLE:  'OK Button', cursor=1",
     "BRAILLE LINE:  'ipy Application Buttons Frame Button 2 Button'",
     "     VISIBLE:  'Button 2 Button', cursor=1",
-    "SPEECH OUTPUT: 'Message Dialog'",
-    "SPEECH OUTPUT: 'Buttons frame'",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'OK button'",
     "SPEECH OUTPUT: ''",
     "SPEECH OUTPUT: 'Button 2 button'"]))
 sequence.append(KeyComboAction("Tab"))
@@ -53,14 +49,12 @@ sequence.append(KeyComboAction("space"))
 sequence.append(KeyComboAction("<ALT>F4"))
 sequence.append(utils.AssertPresentationAction(
     "openSUSE Button",
-    ["BRAILLE LINE:  'ipy Application Message Dialog Dialog'",
-    "     VISIBLE:  'Message Dialog Dialog', cursor=1",
-    "BRAILLE LINE:  'ipy Application Buttons Frame'",
-    "     VISIBLE:  'Buttons Frame', cursor=1",
+    ["BRAILLE LINE:  'ipy Application Message Dialog Frame OK Button'",
+    "     VISIBLE:  'OK Button', cursor=1",
     "BRAILLE LINE:  'ipy Application Buttons Frame openSUSE Button'",
     "     VISIBLE:  'openSUSE Button', cursor=1",
-    "SPEECH OUTPUT: 'Message Dialog'",
-    "SPEECH OUTPUT: 'Buttons frame'",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'OK button'",
     "SPEECH OUTPUT: ''",
     "SPEECH OUTPUT: 'openSUSE button'"]))
 sequence.append(utils.AssertionSummaryAction())
