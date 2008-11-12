6ff017b6153f915ce542b9d7589f2c4303873fd8
Brad Taylor <brad@getcoded.net>
2008-11-12 19:36:06 +0000

    2008-11-12  Brad Taylor  <brad@getcoded.net>
    	* configure.ac:
    	* Test/Makefile.am:
    	* Test/configure.ac:
    	* Test/AtkTest/Makefile.am:
    	* Test/GailTest/Makefile.am:
    	* Test/AtkTest/AssemblyInfo.cs
    	* Test/GailTestApp/Makefile.am:
    	* Test/GailTest/AssemblyInfo.cs:
    	* Test/GailTestApp/AssemblyInfo.cs:
    	* Test/UiaAtkBridgeTest/Makefile.am:
    	* Test/UiaAtkBridgeTest/AssemblyInfo.cs: Make the build work under
    	autotools.
    
    
    svn path=/trunk/uia2atk/; revision=118627

--- src/UiaAtkBridge/Test/GailTest/AssemblyInfo.cs
+++ src/UiaAtkBridge/Test/GailTest/AssemblyInfo.cs
@@ -41,9 +41,3 @@ using System.Runtime.CompilerServices;
 // If the build and revision are set to '*' they will be updated automatically.
 
 [assembly: AssemblyVersion("1.0.*.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
--- src/UiaAtkBridge/Test/UiaAtkBridgeTest/AssemblyInfo.cs
+++ src/UiaAtkBridge/Test/UiaAtkBridgeTest/AssemblyInfo.cs
@@ -36,14 +36,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// If the build and revision are set to '*' they will be updated automatically.
-
-[assembly: AssemblyVersion("1.0.*.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
