24c3444992ffb51c366d3d29ef251747ebe1de7e
Calen Chen <cachen@mono-cvs.ximian.com>
2008-11-23 16:40:31 +0000

    update checkedlistbox_basic_ops.py to delete focused state in the test of checking listitem20's default state
    svn path=/trunk/uia2atk/; revision=119752

--- test/testers/checkedlistbox_basic_ops.py
+++ test/testers/checkedlistbox_basic_ops.py
@@ -56,8 +56,7 @@ statesCheck(clbFrame.listbox2, "List")
 #check default states for ListItem 0 which in "CheckOnClick is True" list
 statesCheck(clbFrame.listitem[0], "ListItem")
 #check default states for ListItem 20 which in "CheckOnClick is False" list
-#with focused
-statesCheck(clbFrame.listitem[20], "ListItem", add_states=["focused"])
+statesCheck(clbFrame.listitem[20], "ListItem")
 
 #use keyCombo with "Tab" to change the focus from listitem 20 to listitem 0
 clbFrame.keyCombo("Tab", grabFocus = False)
