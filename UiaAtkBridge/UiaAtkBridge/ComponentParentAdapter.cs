mono/uia2atk

commit d6d92b08169382acee865ea46ca8e6815ba04618

* UiaAtkBridge/RadioButton.cs: * UiaAtkBridge/ComponentParentAdapter.cs: Add MemberOf relation for   radiobuttons (hopefully fixes #447885). Will enable tests ASAP.
svn path=/trunk/uia2atk/; revision=120088

UiaAtkBridge/UiaAtkBridge/ComponentParentAdapter.cs
@@ -24,6 +24,7 @@
 // 
 
 using System;
+using SCG = System.Collections.Generic;
 using System.Windows.Automation;
 using System.Windows.Automation.Provider;
 
@@ -38,6 +39,36 @@ namespace UiaAtkBridge
 		}
 		
 		private ComponentImplementorHelper componentExpert;
+
+		internal Atk.Relation RadioButsRelation { get; private set; }
+		
+		internal override void AddOneChild (Atk.Object child)
+		{
+			base.AddOneChild (child);
+
+			RadioButton rad = child as RadioButton;
+			if (rad == null)
+				return;
+
+			if (RadioButsRelation == null)
+				RadioButsRelation = new Atk.Relation (new Atk.Object [] { rad }, Atk.RelationType.MemberOf);
+			else
+				RadioButsRelation.AddTarget (rad);
+		}
+
+		internal override void RemoveChild (Atk.Object childToRemove)
+		{
+			base.RemoveChild (childToRemove);
+
+			RadioButton rad = childToRemove as RadioButton;
+			if (rad == null)
+				return;
+
+			SCG.List <Atk.Object> restRads = new SCG.List<Atk.Object> (RadioButsRelation.Target);
+			restRads.Remove ((Atk.Object)rad);
+			RadioButsRelation = new Atk.Relation (restRads.ToArray (), Atk.RelationType.MemberOf);
+		}
+
 		
 #region ComponentImplementor Methods
 

UiaAtkBridge/UiaAtkBridge/RadioButton.cs
@@ -47,6 +47,13 @@ namespace UiaAtkBridge
 			imageProvider = selProvider;
 			actionName = "click";
 		}
+
+		protected override Atk.RelationSet OnRefRelationSet ()
+		{
+			Atk.RelationSet relSet = base.OnRefRelationSet ();
+			relSet.Add (((ComponentParentAdapter)Parent).RadioButsRelation);
+			return relSet;
+		}
 		
 		public override bool DoAction (int action)
 		{

commit fa570c823301784745b2e64e93b94830e46feb5c

* UiaAtkBridge/RadioButton.cs: * UiaAtkBridge/ComponentParentAdapter.cs: Trying to fix threading issues   introduced in r120088.
svn path=/trunk/uia2atk/; revision=120370

UiaAtkBridge/UiaAtkBridge/ComponentParentAdapter.cs
@@ -58,15 +58,17 @@ namespace UiaAtkBridge
 
 		internal override void RemoveChild (Atk.Object childToRemove)
 		{
-			base.RemoveChild (childToRemove);
-
 			RadioButton rad = childToRemove as RadioButton;
-			if (rad == null)
-				return;
+			if (rad != null) {
+				Atk.Relation newRelation = null;
+				SCG.List <Atk.Object> restRads = new SCG.List<Atk.Object> (RadioButsRelation.Target);
+				restRads.Remove ((Atk.Object)rad);
+				if (restRads.Count > 0)
+					newRelation = new Atk.Relation (restRads.ToArray (), Atk.RelationType.MemberOf);
+				RadioButsRelation = newRelation;
+			}
 
-			SCG.List <Atk.Object> restRads = new SCG.List<Atk.Object> (RadioButsRelation.Target);
-			restRads.Remove ((Atk.Object)rad);
-			RadioButsRelation = new Atk.Relation (restRads.ToArray (), Atk.RelationType.MemberOf);
+			base.RemoveChild (childToRemove);
 		}
 
 		

UiaAtkBridge/UiaAtkBridge/RadioButton.cs
@@ -51,7 +51,9 @@ namespace UiaAtkBridge
 		protected override Atk.RelationSet OnRefRelationSet ()
 		{
 			Atk.RelationSet relSet = base.OnRefRelationSet ();
-			relSet.Add (((ComponentParentAdapter)Parent).RadioButsRelation);
+			var rel = ((ComponentParentAdapter)Parent).RadioButsRelation;
+			if (rel != null)
+				relSet.Add (((ComponentParentAdapter)Parent).RadioButsRelation);
 			return relSet;
 		}
 		