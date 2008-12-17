2566cf4b4a93e280a7e0e2308fee453c3123b9eb
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2008-12-17 03:31:42 +0000

    * testers/linklabel_basic_ops.py: Issue jump action for each link and mouseClick one link.  Check states after keyboard navigation.  Test is "finished". * testers/linklabel/linklabelframe.py: Finish application wrapper (for now).
    svn path=/trunk/uia2atk/; revision=121660

--- test/testers/linklabel/linklabelframe.py
+++ test/testers/linklabel/linklabelframe.py
@@ -8,6 +8,7 @@
 
 import sys
 import os
+import re
 import actions
 import states
 import pyatspi
@@ -24,6 +25,8 @@ class LinkLabelFrame(accessibles.Frame):
     LINK1 = re.compile('^openSUSE')
     LINK2 = "calculator:/usr/bin/gcalctool"
     LINK3 = "gmail:www.gmail.com"
+
+    desktop = pyatspi.Registry.getDesktop(0)
 
     def __init__(self, accessible):
         super(LinkLabelFrame, self).__init__(accessible)
@@ -43,97 +46,176 @@ class LinkLabelFrame(accessibles.Frame):
                             "%s should have %d link(s) instead of %d" \
                                % (accessible, expected_linknum, actual_linknum) 
 
-    # give 'jump' action for linklabel and make sure the appropriate
-    # application is opened (and to the appropriate URL if applicable")
-    def openLink(self, accessible, app_name, linknum, uri, is_url):
-        # get the accessible with the jump action
-        ihypertext = accessible._accessible.queryHypertext()
-        link = ihypertext.getLink(linknum)
-        obj = link.getObject(0)
-        iaction = obj.queryAction()
-        
-        # make sure the action interface only has one action and that it is
-        # jump
-        procedurelogger.action("%s %s" %\
-                               ("Check the number of actions associated with",
-                                accessible))
-        procedurelogger.expectedResult("%s has one associated action" % \
-								    accessible)
-        assert iaction.nActions == 1, \
+    def _getOpenURL(self, firefox_app):
+        # return a list of all the URLs current open in firefox
+        open_urls = []
+        if firefox_app is not None:
+            locations = firefox_app.findAllEntries("Location")
+            for location in locations:
+                open_urls.append(location.text)
+        assert len(open_urls) == 1, "There should only be one URL open"
+        return open_urls[0] 
+
+    # Give 'jump' action for linklabel and make sure the appropriate
+    # application is opened (and to the appropriate URL if applicable").
+    # For mouse_click to be True, the center of the accessible must be a link
+    # or the mouse click will be issued on a non-link and invalidate the test
+    def openLink(self, accessible, app_name, linknum, uri, is_url, mouse_click=False):
+        # get the action interface if we're not using a mouseClick
+        iaction = None
+        if not mouse_click:
+            # get the accessible with the jump action
+            ihypertext = accessible._accessible.queryHypertext()
+            num_links = ihypertext.getNLinks()
+            assert linknum < num_links, \
+                        'Invalid linknum (%d), %s only have %d links' % \
+                                                    (linknum, accessible, numlinks)
+            link = ihypertext.getLink(linknum)
+            obj = link.getObject(0)
+            is_enabled = pyatspi.STATE_ENABLED in obj.getState().getStates()
+            iaction = obj.queryAction()
+
+            # make sure the action interface only has one action and that it is
+            # jump
+            procedurelogger.action("%s %s" %\
+                                ("Check the number of actions associated with",
+                                                                   accessible))
+            procedurelogger.expectedResult("%s has one associated action" % \
+                                                                    accessible)
+            assert iaction.nActions == 1,\
                                "Only one action should exist for the LinkLabel"
-        actionName = iaction.getName(0)
-        procedurelogger.action("Check the name of the action associated with" \
-                                                                  % accessible)
-        procedurelogger.expectedResult("Action name is \"jump\"")
-        assert actionName == "jump", \
-                                "Action name for LinkLabel should be \"jump\""
+            actionName = iaction.getName(0)
+            procedurelogger.action("%s %s" % \
+                  ("Check the name of the action associated with", accessible))
+            procedurelogger.expectedResult("Action name is \"jump\"")
+            assert actionName == "jump", \
+                                 "Action name for LinkLabel should be \"jump\""
+
+        # now we can just call iaction.doAction(0) to perform the jump action
 
         # if the link is a url:
         #   make sure firefox opens to the link
         # if it's not a link:
         #   make sure the proper application opens
-        if is_url:
-            # if firefox is already open get a list of urls currently open
-            firefox_app = cache._desktop.findApplication("Firefox")
-            before_urls = []
-            before_frames = firefox_app.findAllFrames(None)
-            if firefox_app is not None:
-                locations = firefox_app.findAllEntries("Location")
-                for location in locations:
-                    before_urls.append(location.text)
+        if is_url and is_enabled:
+            # TODO: enhance this test so that it will handle the case
+            # of a firefox app already being open
 
-            # execute the jump action, this should open another firefox
+            firefox_app = pyatspi.findDescendant(self.desktop, \
+                          lambda x: x.getRoleName() == 'application' and \
+                          x.name == 'Firefox') 
+
+            assert firefox_app is None, "Firefox should not be open"
+
+            # execute the jump action, this should open firefox
             # application to the URL provided
-            iaction.doAction(0)
+            if mouse_click:
+                accessible.mouseClick()
+            else:
+                iaction.doAction(0)
             # give firefox plenty of time to start
-            sleep(10)
+            sleep(20)
 
-            after_frames = firefox_app.findAllFrames(None)
+            firefox_app = cache._desktop.findApplication("Firefox")
+
+            assert firefox_app is not None, "Firefox should have opened"
+
+            frames = firefox_app.findAllFrames(None)
 
             # only one new frame should have been opened
-            assert len(after_frames) == len(before_frames) + 1, \
-                                       "Only one frame should have been opened"
+            assert len(frames) == 1, "Only one frame should have been opened"
+   
+            frame = frames[0]
 
             # find the urls after jumping
-            after_urls = []
-            if firefox_app is not None:
-                locations = firefox_app.findAllEntries("Location")
-                for location in locations:
-                    after_urls.append(location.text)
-
-            # remove all the previous urls from the list of after_urls
-            for url in before_urls:
-                after_urls.remove(url)
-            # what's left should be the new url
-            assert len(after_urls) == 1, "Only one url should have been opened"
-            new_url = after_urls[0]
+            url = self._getOpenURL(firefox_app)
 
             # now make sure Firefox opened to the correct link
             procedurelogger.action("%s" % \
                                "Ensure the correct link was opened in Firefox")
             procedurelogger.expectedResult("%s was opened" % uri)
-            assert uri == new_url, "%s should have been opened instead of %s"\
-                                                               % (uri, new_url)
+            assert uri == url, \
+                    '"%s" should have been opened instead of "%s"' % (uri, url)
             # now close firefox by finding one of the frames, finding its
             # extents, clicking on the title bar to select the firefox window
             # and then press Alt+F4 (what a pain!)
 
             # it looks like the the most recent frame is always found last
-            opened_frame = after_frames[len(after_frames)-1]
-            icomponent = opened_frame._accessible.queryComponent()
+            icomponent = frame._accessible.queryComponent()
             bbox = icomponent.getExtents(pyatspi.DESKTOP_COORDS)
             x = bbox.x + (bbox.width / 2)
             y = bbox.y
             pyatspi.Registry.generateMouseEvent(x, y - 1, 'b1c')
-            sys.exit(33)
             self.altF4(assertClosed=False)
+            procedurelogger.expectedResult("Firefox should be closed")
             sleep(config.SHORT_DELAY)
+        elif is_url and not is_enabled:
+            # if the URI is a URL but the link should be disabled we should
+            # make sure we can still try to jump, but nothing should happen
+
+            # execute the jump action, this should open another firefox
+            # application to the URL provided
+
+            firefox_app = pyatspi.findDescendant(self.desktop, \
+                          lambda x: x.getRoleName() == 'application' and \
+                          x.name == 'Firefox')
+
+            assert firefox_app is None, "Firefox should not be open" 
+
+            if mouse_click:
+                accessible.mouseClick()
+            else:
+                iaction.doAction(0)
+
+            # give firefox plenty of time to start (in case it does try to 
+            # start)
+            sleep(10)
+
+            procedurelogger.action("%s" % \
+                               "Ensure that Firefox was not opened")
+            firefox_app = pyatspi.findDescendant(self.desktop, \
+                        lambda x: x.getRoleName() == 'application' and \
+                        x.name == 'Firefox')           
+ 
+            procedurelogger.expectedResult("Firefox was not opened")
+            assert firefox_app is None, \
+                            "no \"Firefox\" application should have been found"
+        elif not is_url:
+            # TODO: enhance this test so that it will handle the case
+            # of a gcalctool app already being open
+
+            procedurelogger.action("Check if gcalctool is open")
+            procedurelogger.expectedResult("gcalctool should not be open")
+            calc_app = pyatspi.findDescendant(self.desktop, \
+                       lambda x: x.getRoleName() == 'application' and \
+                       x.name == 'gcalctool')
+            assert calc_app is None, "No gcalctool should be open"
+
+            # jump, this should open a gcalctool app
+            if mouse_click:
+                accessible.mouseClick()
+            else:
+                iaction.doAction(0)
+
+            # give gcalctool plenty of time to open
+            sleep(20)
+
+            procedurelogger.action("Check if gcalctool is open")
+            procedurelogger.expectedResult("gcalctool should be open")
+            calc_app = pyatspi.findDescendant(self.desktop, \
+                       lambda x: x.getRoleName() == 'application' and \
+                       x.name == 'gcalctool')
+
+            assert calc_app is not None, "One gcalctool should be open"
+            
+            # close the opened gcalctool (it becomes the active window, unlike
+            # firefox, so this is a lot easier)
+            self.altF4(assertClosed=False)
+            procedurelogger.expectedResult("gcalctool should be closed")
+            sleep(config.MEDIUM_DELAY)
         else:
-            before_calc_apps = cache._desktop.findAllApplications("gcalctool")
-            print before_calc_apps
-
-
-
+            raise NotImplementedError, \
+                                "Applcation wrapper does not support this call"
 
     # make sure each link points to the expected uri
     # URI: http://en.wikipedia.org/wiki/Uniform_Resource_Identifier
@@ -148,14 +230,3 @@ class LinkLabelFrame(accessibles.Frame):
 
     def quit(self):
         self.altF4()
-
-
-class NewAppFrame(accessibles.Frame):
-
-    def __init__(self, accessible):
-        super(NewAppFrame, self).__init__(accessible)
-
-    def quit(self):
-        self.altF4
-
-
--- test/testers/linklabel_basic_ops.py
+++ test/testers/linklabel_basic_ops.py
@@ -49,27 +49,46 @@ if app is None:
 
 # just an alias to make things shorter
 llFrame = app.linkLabelFrame
+
+sleep(config.SHORT_DELAY)
+
 #check sensitive Label's default states
 statesCheck(llFrame.link1, "Label", add_states=["focusable", "focused"])
 statesCheck(llFrame.link2, "Label", add_states=["focusable"])
 statesCheck(llFrame.link3, "Label", add_states=["focusable"])
 
-#check the number of links in each LinkLabel
+# check the number of links in each LinkLabel
 llFrame.assertNLinks(llFrame.link1, 2)
 llFrame.assertNLinks(llFrame.link2, 1)
 llFrame.assertNLinks(llFrame.link3, 1)
 
-#make sure the accessibles have the expected URIs
+# make sure the accessibles have the expected URIs
 llFrame.assertURI(llFrame.link1, 0, OPENSUSE)
 llFrame.assertURI(llFrame.link1, 1, NOVELLMAIL)
 llFrame.assertURI(llFrame.link2, 0, GCALCTOOL)
 llFrame.assertURI(llFrame.link3, 0, GMAIL)
 
-#make sure we actually open the links
-llFrame.openLink(llFrame.link1, "Firefox", 0, OPENSUSE, True)
-llFrame.openLink(llFrame.link1, "Firefox", 1, NOVELLMAIL, True)
-llFrame.openLink(llFrame.link2, "gcalctool", 0, GCALCTOOL, False)
-llFrame.openLink(llFrame.link3, "Firefox", 0, GMAIL, True)
+# make sure we actually open the links
+llFrame.openLink(llFrame.link1, "Firefox", 0, OPENSUSE, is_url = True)
+llFrame.openLink(llFrame.link1, "Firefox", 1, NOVELLMAIL, is_url = True)
+llFrame.openLink(llFrame.link2, "gcalctool", 0, GCALCTOOL, is_url = False)
+llFrame.openLink(llFrame.link3, "Firefox", 0, GMAIL, is_url = True)
+
+# press down arrow and check the states
+llFrame.keyCombo("Down", grabFocus=False)
+sleep(config.SHORT_DELAY)
+statesCheck(llFrame.link1, "Label", add_states=["focusable"])
+statesCheck(llFrame.link2, "Label", add_states=["focusable", "focused"])
+statesCheck(llFrame.link3, "Label", add_states=["focusable"])
+# down arrow again should take us back to link1
+llFrame.keyCombo("Down", grabFocus=False)
+sleep(config.SHORT_DELAY)
+statesCheck(llFrame.link1, "Label", add_states=["focusable", "focused"])
+statesCheck(llFrame.link2, "Label", add_states=["focusable"])
+statesCheck(llFrame.link3, "Label", add_states=["focusable"])
+
+# now try opening a link using mouseClick
+llFrame.openLink(llFrame.link2, "gcalctool", 0, GCALCTOOL, is_url = False, mouse_click = True)
 
 print "INFO:  Log written to: %s" % config.OUTPUT_DIR
 
