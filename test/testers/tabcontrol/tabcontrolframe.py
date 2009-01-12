1852cb8df02e465e2f2b09503ecc7413a09a24cd
Calen Chen <cachen@mono-cvs.ximian.com>
2009-01-12 10:02:55 +0000

    delete some extra constants
    svn path=/trunk/uia2atk/; revision=123026

--- test/testers/tabcontrol/tabcontrolframe.py
+++ test/testers/tabcontrol/tabcontrolframe.py
@@ -24,13 +24,6 @@ class TabControlFrame(accessibles.Frame):
     TAB1 = "Tab 1"
     TAB2 = "Tab 2"
     TAB3 = "Tab 3"
-    LABEL0 = "I'm in tab page 0"
-    LABEL1 = "I'm in tab page 1"
-    LABEL2 = "I'm in tab page 2"
-    LABEL3 = "I'm in tab page 3"
-    BUTTON = "Button"
-    CHECKBOX = "CheckBox"
-    RADIOBUTTON = "RadioButton"
 
     def __init__(self, accessible):
         super(TabControlFrame, self).__init__(accessible)
@@ -39,14 +32,6 @@ class TabControlFrame(accessibles.Frame):
         self.tabpage1 = self.findPageTab(self.TAB1)
         self.tabpage2 = self.findPageTab(self.TAB2)
         self.tabpage3 = self.findPageTab(self.TAB3)
-        self.label0 = self.tabpage0.findLabel(self.LABEL0)
-        self.label1 = self.tabpage1.findLabel(self.LABEL1, checkShowing=False)
-        self.label2 = self.tabpage2.findLabel(self.LABEL2, checkShowing=False)
-        self.label3 = self.tabpage3.findLabel(self.LABEL3, checkShowing=False)
-        self.button = self.tabpage0.findPushButton(self.BUTTON)
-        self.checkbox = self.tabpage2.findCheckBox(self.CHECKBOX, checkShowing=False)
-        self.radiobutton = self.tabpage3.findRadioButton(self.RADIOBUTTON, checkShowing=False)
-        self.textbox = self.tabpage1.findText(None, checkShowing=False)
    
     #assert AtkSelection implementation
     def assertSelectionChild(self, accessible, childIndex):
