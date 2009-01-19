0e53fcfe5270ef71c3600871f29fba20b1b5885a
Calen Chen <cachen@mono-cvs.ximian.com>
2009-01-19 08:07:11 +0000

    add Document properety, give Text property for printpreviewdialog
    svn path=/trunk/uia2atk/; revision=123772

--- test/samples/printpreviewdialog.py
+++ test/samples/printpreviewdialog.py
@@ -19,13 +19,16 @@ clr.AddReference('System.Drawing')
 from System.Windows.Forms import (
     Application, Button, Form, Label, PrintPreviewDialog, DockStyle
 )
-from System.Drawing import Size, Point
+from System.Drawing import Size, Point, Font, FontStyle, Brushes
+from System.Drawing.Printing import PrintDocument
 
 class PrintPreviewDialogSample(Form):
     """PrintPreviewDialog control class"""
 
     def __init__(self):
         """PrintPreviewDialogSample class init function."""
+        # initialize a PrintDocument object
+        self.doc_to_print = PrintDocument()
 
         # setup title
         self.Text = "PrintPreviewDialog control"
@@ -38,15 +41,28 @@ class PrintPreviewDialogSample(Form):
 
         # setup printpreviewdialog
         self.printpreviewdialog = PrintPreviewDialog()
+        self.printpreviewdialog.Text = "PrintPreviewDialog"
         self.printpreviewdialog.ClientSize = Size(500, 300)
         self.printpreviewdialog.Location = Point(29, 29)
         self.printpreviewdialog.MinimumSize = Size(375, 250)
         self.printpreviewdialog.UseAntiAlias = True
+        self.printpreviewdialog.Document = self.doc_to_print
+        self.doc_to_print.PrintPage += self.doc_to_print_PrintPage
 
         # add controls
         self.Controls.Add(self.button)
 
+    def doc_to_print_PrintPage(self, sender, event):
+        # Insert code to render the page here.
+        # This code will be called when the control is drawn.
+        # The following code will render a simple
+        # message on the document in the control.
+        text = "In doc_to_print_PrintPage method."
+        print_font = Font("Arial", 20, FontStyle.Bold)
+        event.Graphics.DrawString(text, print_font, Brushes.Black, 50, 10)
+
     def click(self, sender, event):
+        self.printpreviewdialog.Document = self.doc_to_print
         self.printpreviewdialog.ShowDialog()
 
 # run application
