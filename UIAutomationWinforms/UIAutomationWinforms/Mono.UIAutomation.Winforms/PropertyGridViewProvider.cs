fe3b42f1f08fe5e601dfa7527936ddd91812c7b8
Neville Gao <ngao@mono-cvs.ximian.com>
2009-01-22 11:24:50 +0000

    Initial implement PropertyGrid.
    svn path=/trunk/uia2atk/; revision=124178

---
+++ UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms/PropertyGridViewProvider.cs
@@ -0,0 +1,61 @@
+// Permission is hereby granted, free of charge, to any person obtaining 
+// a copy of this software and associated documentation files (the 
+// "Software"), to deal in the Software without restriction, including 
+// without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to 
+// permit persons to whom the Software is furnished to do so, subject to 
+// the following conditions: 
+//  
+// The above copyright notice and this permission notice shall be 
+// included in all copies or substantial portions of the Software. 
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// 
+// Copyright (c) 2009 Novell, Inc. (http://www.novell.com) 
+// 
+// Authors: 
+// 	Neville Gao <nevillegao@gmail.com>
+// 
+
+using System;
+using SWF = System.Windows.Forms;
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+using Mono.Unix;
+using Mono.UIAutomation.Winforms.Behaviors;
+
+namespace Mono.UIAutomation.Winforms
+{
+	[MapsComponent (typeof (SWF.PropertyGridInternal.PropertyGridView))]
+	internal class PropertyGridViewProvider : FragmentRootControlProvider
+	{
+		#region Constructor
+
+		public PropertyGridViewProvider (SWF.PropertyGridInternal.PropertyGridView propertyGridView)
+			: base (propertyGridView)
+		{
+		}
+
+		#endregion
+	
+		#region SimpleControlProvider: Specializations
+
+		protected override object GetProviderPropertyValue (int propertyId)
+		{
+			if (propertyId == AutomationElementIdentifiers.ControlTypeProperty.Id)
+				return ControlType.Table.Id;
+			else if (propertyId == AutomationElementIdentifiers.LocalizedControlTypeProperty.Id)
+				return Catalog.GetString ("table");
+			else
+				return base.GetProviderPropertyValue (propertyId);
+		}
+
+		#endregion
+	}
+}
