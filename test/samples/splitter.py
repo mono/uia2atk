1118a64bebc3e208d9687355362154202ea548b7
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2009-01-26 19:27:40 +0000

    * samples/splitter.py: Add more labels to the top panel.
    svn path=/trunk/uia2atk/; revision=124552

--- test/samples/splitter.py
+++ test/samples/splitter.py
@@ -29,15 +29,30 @@ class SplitterSample(Form):
         self.Text = "Splitter control"
 
         # setup labels
+        self.label0 = Label()
+        self.label0.Dock = DockStyle.Top
+        self.label0.BackColor = Color.DarkCyan
+        self.label0.Text = "label0 on one side against splitter"
+
         self.label1 = Label()
         self.label1.Dock = DockStyle.Top
         self.label1.BackColor = Color.DarkCyan
         self.label1.Text = "label1 on one side against splitter"
 
-        self.label2 = Label()
-        self.label2.Dock = DockStyle.Fill
-        self.label2.Text = "label2 on the other side against splitter"
-        self.label2.BackColor = Color.Coral
+	self.label2 = Label()
+        self.label2.Dock = DockStyle.Top
+        self.label2.BackColor = Color.DarkCyan
+        self.label2.Text = "label2 on one side against splitter"
+	
+	self.label3 = Label()
+        self.label3.Dock = DockStyle.Top
+        self.label3.BackColor = Color.DarkCyan
+        self.label3.Text = "label3 on one side against splitter"
+
+        self.label4 = Label()
+        self.label4.Dock = DockStyle.Fill
+        self.label4.Text = "label4 on the other side against splitter"
+        self.label4.BackColor = Color.Coral
 
 
         # setup splitter
@@ -46,9 +61,12 @@ class SplitterSample(Form):
         self.splitter.BorderStyle = BorderStyle.Fixed3D
 
         # add controls
-        self.Controls.Add(self.label2)
+        self.Controls.Add(self.label4)
         self.Controls.Add(self.splitter)
+        self.Controls.Add(self.label0)
         self.Controls.Add(self.label1)
+        self.Controls.Add(self.label2)
+        self.Controls.Add(self.label3)
 
 # run application
 form = SplitterSample()
