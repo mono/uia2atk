b670582ab4713060c3fcb63fe9c1f0bd33321506
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2009-02-02 15:25:16 +0000

    * samples/splitter_horizontal.py: Replace with deleted splitter.py, which I had modified.  It already has a partial test written against it. * samples/splitter_vertical.py: Replace with overwritten splitter_vertical.py, which I had written and had begun testing.
    svn path=/trunk/uia2atk/; revision=125353

--- test/samples/splitter_vertical.py
+++ test/samples/splitter_vertical.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env ipy
 
 ##############################################################################
-# Written by:  Ray Wang <rawang@novell.com>
-# Date:        06/23/2008
-# Description: the sample for winforms control:
+# Written by:  Brian G. Merrell <bgmerrell@novell.com>
+# Date:        01/29/2009
+# Description: a sample for winforms control:
 #              Splitter
 ##############################################################################
 
 # The docstring below is used in the generated log file
 """
-Test accessibility of "Splitter" control
+"Splitter" control sample
 """
 
 # imports
 import clr
+import System
 clr.AddReference('System.Windows.Forms')
 clr.AddReference('System.Drawing')
-from System.Windows.Forms import Application, DockStyle, Form, Splitter, Label, BorderStyle
+from System.Windows.Forms import (Application, DockStyle, Form, Splitter,
+                                  Label, BorderStyle, TreeView, ListView,
+                                  Control)
 from System.Drawing import Color
 
 class SplitterSample(Form):
@@ -25,30 +28,40 @@ class SplitterSample(Form):
     def __init__(self):
         """SplitterSample class init function."""
 
-        # setup title
-        self.Text = "Splitter control"
+        # Create TreeView, ListView, and Splitter controls.
+        self.treeView1 = TreeView()
+        self.listView1 = ListView()
+        self.splitter1 = Splitter()
 
-        # setup label1
-        self.label1 = Label()
-        self.label1.Dock = DockStyle.Left
-        self.label1.BackColor = Color.DarkCyan
-        self.label1.Text = "label1 on one side against splitter"
+        # Set the TreeView control to dock to the left side of the form.
+        self.treeView1.Dock = DockStyle.Left;
+        # Set the Splitter to dock to the left side of the TreeView control.
+        self.splitter1.Dock = DockStyle.Left;
+        # Set the minimum size the ListView control can be sized to.
+        self.splitter1.MinExtra = 100;
+        # Set the minimum size the TreeView control can be sized to.
+        self.splitter1.MinSize = 75;
+        # Set the ListView control to fill the remaining space on the form.
+        self.listView1.Dock = DockStyle.Fill;
+        # Add a TreeView and a ListView item to identify the controls on the
+        # form.
+        self.treeView1.Nodes.Add("TreeView Node");
+        self.listView1.Items.Add("ListView Item");
 
-        # setup splitter
-        self.splitter = Splitter()
-        self.splitter.Dock = DockStyle.Left
-        self.splitter.BorderStyle = BorderStyle.Fixed3D
-
-        # setup label2
-        self.label2 = Label()
-        self.label2.Dock = DockStyle.Fill
-        self.label2.Text = "label2 on the other side against splitter"
-        self.label2.BackColor = Color.Coral
+        # Add the controls in reverse order to the form to ensure proper
+        # location.
+        array_Control = System.Array[Control]
+        self.Controls.AddRange(array_Control((self.listView1,
+                                              self.splitter1,
+                                              self.treeView1)))
 
         # add controls
-        self.Controls.Add(self.label2)
-        self.Controls.Add(self.splitter)
-        self.Controls.Add(self.label1)
+        #self.Controls.Add(self.label4)
+        #self.Controls.Add(self.splitter)
+        #self.Controls.Add(self.label0)
+        #self.Controls.Add(self.label1)
+        #self.Controls.Add(self.label2)
+        #self.Controls.Add(self.label3)
 
 # run application
 form = SplitterSample()
