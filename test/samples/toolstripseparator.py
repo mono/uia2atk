86422165256ead2028b54a50a370b168c5e9c3e4
Calen Chen <cachen@mono-cvs.ximian.com>
2009-02-03 07:43:14 +0000

    add new example with three separators
    svn path=/trunk/uia2atk/; revision=125492

---
+++ test/samples/toolstripseparator.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env ipy
+
+##############################################################################
+# Written by:  Calen Chen <cachen@novell.com>
+# Date:        02/03/2008
+# Description: This is a test application sample for winforms control:
+#              ToolStripSeparator
+##############################################################################
+
+import clr
+import System
+import System.IO
+
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+
+from System.Windows.Forms import *
+from System.Drawing import *
+import System.Drawing.Text
+
+
+class RunApp(Form):
+
+    def __init__(self):
+        self.Text = "ToolStripSeparator control"
+        self.Width = 300
+        self.Height = 300
+        self.FormBorderStyle = FormBorderStyle.Fixed3D
+
+##ToolStrip:
+        self.ts = ToolStrip()
+        self.Controls.Add(self.ts)
+
+##ToolStripLabel1
+        self.tsl1 = ToolStripLabel("Font:")
+        self.ts.Items.Add(self.tsl1)
+
+##ToolStripSeparator1
+        self.toolstripseparator1 = ToolStripSeparator()
+        self.ts.Items.Add(self.toolstripseparator1)
+
+##ToolStripLabel1
+        self.tsl2 = ToolStripLabel("Size:")
+        self.ts.Items.Add(self.tsl2)
+
+##ToolStripSeparator2
+        self.toolstripseparator2 = ToolStripSeparator()
+        self.ts.Items.Add(self.toolstripseparator2)
+
+##ToolStripLabel3
+        self.tsl3 = ToolStripLabel("Color:")
+        self.ts.Items.Add(self.tsl3)
+
+##ToolStripSeparator3
+        self.toolstripseparator3 = ToolStripSeparator()
+        self.ts.Items.Add(self.toolstripseparator3)
+
+form = RunApp()
+Application.Run(form)
+
+
