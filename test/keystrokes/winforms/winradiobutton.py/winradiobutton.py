22f6136a178af6cfe1d58d37bd632449ea758e02
Marco Skambraks <mskambraks@mono-cvs.ximian.com>
2009-02-06 16:06:09 +0000

    braille line and visible was different in the test line=& y visible=&=y - now both have a checked radiobutton
    svn path=/trunk/uia2atk/; revision=126059

--- test/keystrokes/winforms/winradiobutton.py/winradiobutton.py
+++ test/keystrokes/winforms/winradiobutton.py/winradiobutton.py
@@ -12,27 +12,27 @@ sequence.append(WaitForFocus("Banana", acc_role=pyatspi.ROLE_RADIO_BUTTON))
 sequence.append(KeyComboAction("space"))
 sequence.append(utils.AssertPresentationAction(
     "Apple Radio Button",
-    ["BRAILLE LINE:  'ipy Application Radio Button Frame & y Banana RadioButton'",
-    "     VISIBLE:  '&=y Banana RadioButton', cursor=1",
-    "SPEECH OUTPUT: ''",
-    "SPEECH OUTPUT: 'Banana selected radio button'"]))
+    ["BRAILLE LINE:  '&=y Banana RadioButton'",
+     "     VISIBLE:  '&=y Banana RadioButton', cursor=1",
+     "SPEECH OUTPUT: ''",
+     "SPEECH OUTPUT: 'Banana selected radio button'"]))
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(WaitForFocus("Cherry", acc_role=pyatspi.ROLE_RADIO_BUTTON))
 sequence.append(KeyComboAction("space"))
 sequence.append(utils.AssertPresentationAction(
     "Banana Radio Button",
-    ["BRAILLE LINE:  'ipy Application Radio Button Frame & y Cherry RadioButton'",
-    "     VISIBLE:  '&=y Cherry RadioButton', cursor=1",
-    "SPEECH OUTPUT: ''",
-    "SPEECH OUTPUT: 'Cherry selected radio button'"]))
+    ["BRAILLE LINE:  '&=y Cherry RadioButton'",
+     "     VISIBLE:  '&=y Cherry RadioButton', cursor=1",
+     "SPEECH OUTPUT: ''",
+     "SPEECH OUTPUT: 'Cherry selected radio button'"]))
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Down"))
 sequence.append(WaitForFocus("Apple", acc_role=pyatspi.ROLE_RADIO_BUTTON))
 sequence.append(KeyComboAction("space"))
 sequence.append(utils.AssertPresentationAction(
     "Cherry Radio Button",
-    ["BRAILLE LINE:  'ipy Application Radio Button Frame & y Apple RadioButton'",
+    ["BRAILLE LINE:  '&=y Apple RadioButton'",
     "     VISIBLE:  '&=y Apple RadioButton', cursor=1",
     "SPEECH OUTPUT: ''",
     "SPEECH OUTPUT: 'Apple selected radio button'"]))
