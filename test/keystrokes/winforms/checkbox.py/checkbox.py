6b0757a66061c7875dd055b3763b9a931051ea2d
Marco Skambraks <mskambraks@mono-cvs.ximian.com>
2009-02-13 15:10:35 +0000

    initial test for checkbox.py not ready yet - problem with Bananas checkbox checkbox triggers focus event, but no braille and speech output
    svn path=/trunk/uia2atk/; revision=126843

---
+++ test/keystrokes/winforms/checkbox.py/checkbox.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+sequence.append(WaitForWindowActivate("CheckBox control",None))
+sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForFocus("Bananas", acc_role=pyatspi.ROLE_CHECK_BOX))
+sequence.append(utils.AssertPresentationAction(
+    "focus on Bananas",
+    ["BRAILLE LINE:  'CheckBox control Frame'",
+     "     VISIBLE:  'CheckBox control Frame', cursor=1",
+     "BRAILLE LINE:  '< > Bananas CheckBox'",
+     "     VISIBLE:  '< > Bananas CheckBox', cursor=1",
+     "SPEECH OUTPUT: 'CheckBox control frame'",
+     "SPEECH OUTPUT: ''",
+     "SPEECH OUTPUT: 'Bananas check box not checked'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("space"))
+sequence.append(utils.AssertPresentationAction(
+    "Banas checked",
+    ["BRAILLE LINE:  '<x> Bananas CheckBox'",
+    "     VISIBLE:  '<x> Bananas CheckBox', cursor=1",
+    "SPEECH OUTPUT: 'checked'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Tab"))
+sequence.append(WaitForFocus("Chicken", acc_role=pyatspi.ROLE_CHECK_BOX))
+sequence.append(utils.AssertPresentationAction(
+    "switch focus to Chicken",
+    ["BRAILLE LINE:  '< > Chicken CheckBox'",
+    "     VISIBLE:  '< > Chicken CheckBox', cursor=1",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'Chicken check box not checked'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("space"))
+sequence.append(utils.AssertPresentationAction(
+    "Chciken checked",
+    ["BRAILLE LINE:  '<x> Chicken CheckBox'",
+    "     VISIBLE:  '<x> Chicken CheckBox', cursor=1",
+    "SPEECH OUTPUT: 'checked'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Down"))
+sequence.append(WaitForFocus("Stuffed Peppers", acc_role=pyatspi.ROLE_CHECK_BOX))
+sequence.append(utils.AssertPresentationAction(
+    "switch focus to Stuffed Peppers",
+    ["BRAILLE LINE:  '<x> Stuffed Peppers CheckBox'",
+    "     VISIBLE:  '<x> Stuffed Peppers CheckBox', cursor=1",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'Stuffed Peppers check box checked'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Tab"))
+sequence.append(WaitForFocus("Fried Lizard", acc_role=pyatspi.ROLE_CHECK_BOX))
+sequence.append(utils.AssertPresentationAction(
+    "switch focus to Fried Lizard",
+    ["BRAILLE LINE:  '< > Fried Lizard CheckBox'",
+    "     VISIBLE:  '< > Fried Lizard CheckBox', cursor=1",
+    "SPEECH OUTPUT: ''",
+    "SPEECH OUTPUT: 'Fried Lizard check box not checked'"]))
+
+
+
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
