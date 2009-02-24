4df12411c1144e548543f4bd794e1da3e2c0a6f3
Mike Gorse <mgorse@mono-cvs.ximian.com>
2009-02-24 17:02:20 +0000

    * tools/hierarchy.py: Print states.
    svn path=/trunk/uia2atk/; revision=127870

--- test/tools/hierarchy.py
+++ test/tools/hierarchy.py
@@ -7,7 +7,7 @@ import sys
 
 def printHierarchy (obj, depth):
 	indentStr = ""
-	print depth, ": ", obj.name, " (", obj.getRoleName(), ")"
+	print str(depth) + ": " + obj.name + " (" + obj.getRoleName() + "): " + states(obj)
 	try:
 		q = obj.queryText()
 		print "  text: ", q.getText(0,-1)
@@ -24,9 +24,30 @@ def printHierarchy (obj, depth):
 	for i in range (0, count):
 		printHierarchy (obj.getChildAtIndex (i), depth + 1)
 
+def states(acc):
+    stateSet = acc.getState()
+    states = stateSet.getStates()
+    state_strings = []
+    for state in states:
+        state_strings.append(pyatspi.stateToString(state))
+    state_string = ' '.join(state_strings)
+    return state_string
+
+def relations(acc):
+    # create the relations string
+    relations = acc.getRelationSet()
+    if relations:
+        relation_strings = []
+        for relation in relations:
+            relation_strings.append( \
+                          pyatspi.relationToString(relation.getRelationType()))
+        rel_string = ' '.join(relation_strings)
+    else:
+        rel_string = ''
+
 reg = pyatspi.Registry
 desktop = reg.getDesktop(0)
-#obj = pyatspi.findDescendant(desktop, lambda x: (x.name == 'ipy' or x.name == sys.argv[1].split('/')[-1]) and x.getRole() == pyatspi.ROLE_APPLICATION)
-#if (obj):
-	#printHierarchy (obj, 0)
-printHierarchy (desktop, 0)
+obj = pyatspi.findDescendant(desktop, lambda x: (x.name == 'ipy' or x.name == sys.argv[1].split('/')[-1]) and x.getRole() == pyatspi.ROLE_APPLICATION)
+if (obj):
+	printHierarchy (obj, 0)
+#printHierarchy (desktop, 0)
