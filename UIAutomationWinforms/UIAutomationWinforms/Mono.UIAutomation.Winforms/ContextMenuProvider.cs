a1f9f7e4f9aa265139cd4c6271d5fa79c33f46bd
Mike Gorse <mgorse@mono-cvs.ximian.com>
2009-02-24 20:19:42 +0000

    * Mono.UIAutomation.Winforms/ContextMenuProvider.cs: Check for null   window.
    svn path=/trunk/uia2atk/; revision=127896

--- UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms/ContextMenuProvider.cs
+++ UIAutomationWinforms/UIAutomationWinforms/Mono.UIAutomation.Winforms/ContextMenuProvider.cs
@@ -57,6 +57,8 @@ namespace Mono.UIAutomation.Winforms
 			else if (propertyId == AEIds.LabeledByProperty.Id)
 				return null;
 			else if (propertyId == AEIds.BoundingRectangleProperty.Id) {
+				if (contextMenu.Wnd == null)
+					return System.Windows.Rect.Empty;
 				System.Drawing.Rectangle rect = contextMenu.Rect;
 				rect.Y -= rect.Height;
 				return Helper.RectangleToRect (contextMenu.Wnd.RectangleToScreen (rect));
