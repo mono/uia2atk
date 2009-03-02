e7c09946da1940e7acbcddf3cc1ac49155446cbb
Mario Carrion <mario@carrion.mx>
2009-03-02 21:34:51 +0000

    2009-04-02  Mario Carrion  <mcarrion@novell.com>
    	* UIAutomationMoonlight: Folder added.
    
    svn path=/branches/uia2atk/post-1.0-research/; revision=128380

---
+++ UIAutomationMoonlight/SilverlightExamples/CustomControl/CustomControl/MyCustomControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+
+namespace CustomControl
+{
+    public partial class MyCustomControl : UserControl
+    {
+        public MyCustomControl()
+        {
+            InitializeComponent();
+        }
+
+        public StackPanel StackPanel
+        {
+            get { return stackPanel; }
+        }
+
+        public void AddNewItem()
+        {
+            TextBox newItem = new TextBox ();
+            newItem.Text = string.Format ("Item #{0}", stackPanel.Children.Count + 1);
+            stackPanel.Children.Add(newItem);
+
+            AutomationPeer peer;
+            if ((peer = FrameworkElementAutomationPeer.FromElement(this)) != null)
+                peer.RaiseAutomationEvent(AutomationEvents.StructureChanged);
+        }
+
+        protected override AutomationPeer OnCreateAutomationPeer ()
+        {
+            return new MyCustomControlPeer(this);
+        }
+    }
+
+    class MyCustomControlPeer : FrameworkElementAutomationPeer
+    {
+        public MyCustomControlPeer(MyCustomControl control)
+            : base(control)
+        {
+            myCustomControl = control;
+        }
+
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            List<AutomationPeer> list = new List<AutomationPeer>();
+            foreach (UIElement element in myCustomControl.stackPanel.Children)
+                list.Add(FrameworkElementAutomationPeer.CreatePeerForElement(element));
+            return list;
+        }
+
+        protected override bool IsKeyboardFocusableCore()
+        {
+            return false;
+        }
+
+        MyCustomControl myCustomControl;
+    }
+}
---
+++ UIAutomationMoonlight/SilverlightExamples/DefaultControls/DefaultControls/Page.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace DefaultControls
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+
+            List<Data> source = new List<Data>();
+            int itemsCount = 50;
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                source.Add(new Data()
+                {
+                    FirstName = "First",
+                    LastName = "Last",
+                    Age = i,
+                    Available = i % 2 == 0,
+                    Birthday = DateTime.Today.AddYears(-i),
+                    Position = (Position) (i % 2)
+                });
+            }
+
+            myDataGrid.ItemsSource = source;
+
+            // Automatic columns
+            List<Data1> myAutomaticSource = new List<Data1> ();
+            for (int i = 0; i < itemsCount; i++)
+            {
+                myAutomaticSource.Add(new Data1()
+                {
+                    FirstName = "First",
+                    LastName = "Last"
+                });
+            }
+            myAutomaticDatagrid.ItemsSource = myAutomaticSource;
+        }
+    }
+
+    public class Data
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool Available { get; set; }
+        public DateTime Birthday { get; set; }
+        public Position Position { get; set; }
+    }
+
+    public class Data1
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public enum Position
+    {
+        Employee = 0,
+        Contractor = 1,
+        Inactive = 2
+    }
+}
