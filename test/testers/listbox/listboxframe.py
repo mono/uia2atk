12e8bb0b1bb3e50f6ebf96301ff067da67355aeb
Ray Wang <raywang@mono-cvs.ximian.com>
2009-03-03 13:35:41 +0000

    update listbox and listview_list regression test
    svn path=/trunk/uia2atk/; revision=128440

--- test/testers/listbox/listboxframe.py
+++ test/testers/listbox/listboxframe.py
@@ -19,14 +19,13 @@ class ListBoxFrame(accessibles.Frame):
     def __init__(self, accessible):
         super(ListBoxFrame, self).__init__(accessible)
         self.label = self.findLabel(self.LABEL)
-        self.listbox = self.findList(None)
-        self.listitem = dict([(x, self.findListItem(str(x))) for x in range(20)])            
+        self.treetable = self.findTreeTable(None)
+        self.tablecell = dict([(x, self.findTableCell(str(x))) for x in range(20)])            
 
-
-    def click(self, listitem):
+    def click(self, tablecell):
         """give 'click' action"""
-        listitem.click()
 
+        tablecell.click()
 
     def assertLabel(self, itemname):
         """Raise exception if the accessible does not match the given result"""
@@ -37,31 +36,27 @@ class ListBoxFrame(accessibles.Frame):
 	
         assert retryUntilTrue(resultMatches)
 
-
     def assertText(self, textValue=None):
         """assert Text implementation for ListItem role"""
         procedurelogger.action('check ListItem\'s Text Value')
 
         for textValue in range(20):
             procedurelogger.expectedResult('item "%s"\'s Text is %s' % \
-                                        (self.listitem[textValue], textValue))
-            assert self.listitem[textValue].text == str(textValue)
-
+                                        (self.tablecell[textValue], textValue))
+            assert self.tablecell[textValue].text == str(textValue)
 
     def assertSelectionChild(self, accessible, childIndex):
         """assert Selection implementation"""
+
         procedurelogger.action('select childIndex %s in "%s"' % \
                                         (childIndex, accessible))
-
         accessible.selectChild(childIndex)
-
 
     def assertClearSelection(self, accessible):
         """clear selections"""
-        procedurelogger.action('clear selection in "%s"' % accessible)
 
+        procedurelogger.action('clear selection in "%s"' % accessible)
         accessible.clearSelection()
-    
 
     # close sample application after running the test
     def quit(self):
