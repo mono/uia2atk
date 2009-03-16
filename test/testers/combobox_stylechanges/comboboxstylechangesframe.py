ec8c56e0aaf2a2cae895a43c6b5e0945063d2c37
Brian G. Merrell <bgmerrell@mono-cvs.ximian.com>
2009-03-16 14:32:18 +0000

    * testers/combobox_stylechanges_basic_ops.py, * testers/combobox_stylechanges/comboboxstylechangesframe.py: Add methods to change the style and find the relevant accessibles when the style changes.
    svn path=/trunk/uia2atk/; revision=129448

--- test/testers/combobox_stylechanges/comboboxstylechangesframe.py
+++ test/testers/combobox_stylechanges/comboboxstylechangesframe.py
@@ -12,8 +12,42 @@ import time
 
 class ComboBoxStyleChangesFrame(accessibles.Frame):
 
+    # constants
+    # the available widgets on the window
+    LABEL1 = "You select 1"
+    DROPDOWNBUTTON = "DropDown"
+    DROPDOWNLISTBUTTON = "DropDownList"
+    SIMPLEBUTTON = "Simple"
+
     def __init__(self, accessible):
         super(ComboBoxStyleChangesFrame, self).__init__(accessible)
+        self.dropdownbutton = self.findPushButton(self.DROPDOWNBUTTON)
+        self.dropdownlistbutton = self.findPushButton(self.DROPDOWNLISTBUTTON)
+        self.simplebutton = self.findPushButton(self.SIMPLEBUTTON)
+
+    # create some find<style> methods.  the test writer must call the
+    # respective find<style> method before testing that style and after
+    # testing any other style.
+    def startDropDownStyle(self):
+        self.dropdownbutton.click()
+        sleep(config.SHORT_DELAY)
+        self.label1 = self.findLabel(self.LABEL1)
+        self.combobox = self.findComboBox(None)
+        self.textbox = self.findText(None)
+
+    def startDropDownListStyle(self):
+        self.dropdownlistbutton.click()
+        sleep(config.SHORT_DELAY)
+        self.label1 = self.findLabel(self.LABEL1)
+        self.combobox = self.findComboBox(None)
+        self.menu = self.findMenu("", checkShowing=False)
+
+    def startSimpleStyle(self):
+        self.simplebutton.click()
+        sleep(config.SHORT_DELAY)
+        self.combobox = self.findComboBox(None)
+        self.textbox = self.findText(None)
+        self.menu = self.findMenu("", checkShowing=False)
 
     def quit(self):
         self.altF4()
