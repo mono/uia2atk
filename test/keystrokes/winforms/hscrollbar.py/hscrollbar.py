359d322390124b3c1b614bce8eb5a66450ec2faf
Marco Skambraks <mskambraks@mono-cvs.ximian.com>
2009-04-06 11:35:42 +0000

    - added vscrollbar and hscrollbar test
    svn path=/trunk/uia2atk/; revision=131107

---
+++ test/keystrokes/winforms/hscrollbar.py/hscrollbar.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForWindowActivate("HScrollBar control",None))
+sequence.append(utils.AssertPresentationAction(
+    "hscrollbar frame active",
+    ["BRAILLE LINE:  'HScrollBar control Frame'",
+     "     VISIBLE:  'HScrollBar control Frame', cursor=1",
+      "SPEECH OUTPUT: 'HScrollBar control frame'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_Subtract"))
+sequence.append(utils.AssertPresentationAction(
+    "switch to flat-review",
+    ["BRAILLE LINE:  'Value:'",
+     "     VISIBLE:  'Value:', cursor=1",
+     "SPEECH OUTPUT: 'Value'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_9"))
+sequence.append(utils.AssertPresentationAction(
+    "flat-review line down",
+    ["BRAILLE LINE:  'horizontal ScrollBar 0%'",
+     "     VISIBLE:  'horizontal ScrollBar 0%', cursor=1",
+      "SPEECH OUTPUT: 'horizontal scroll bar 0 percent'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_Divide"))
+sequence.append(utils.AssertPresentationAction(
+    "left mouse click",
+    ["BRAILLE LINE:  'horizontal ScrollBar 16%'",
+     "     VISIBLE:  'horizontal ScrollBar 16%', cursor=1"]))
+
+
+
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
---
+++ test/keystrokes/winforms/vscrollbar.py/vscrollbar.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from macaroon.playback import *
+import utils
+
+sequence = MacroSequence()
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForWindowActivate("VScrollBar control",None))
+sequence.append(utils.AssertPresentationAction(
+    "vscrollbar frame active",
+    ["BRAILLE LINE:  'VScrollBar control Frame'",
+     "     VISIBLE:  'VScrollBar control Frame', cursor=1",
+      "SPEECH OUTPUT: 'VScrollBar control frame'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_Subtract"))
+sequence.append(utils.AssertPresentationAction(
+    "switch to flat-review",
+    ["BRAILLE LINE:  'Value: vertical ScrollBar 0%'",
+     "     VISIBLE:  'Value: vertical ScrollBar 0%', cursor=1",
+     "SPEECH OUTPUT: 'Value:'"]))
+     
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_6"))
+sequence.append(KeyComboAction("KP_Divide"))
+sequence.append(utils.AssertPresentationAction(
+    "flat-review move right and do a left mouse click",
+    ["BRAILLE LINE:  'Value: vertical ScrollBar 0%'",
+     "     VISIBLE:  'Value: vertical ScrollBar 0%', cursor=8",
+     "BRAILLE LINE:  'Value: 20'",
+     "     VISIBLE:  'Value: 20', cursor=1",
+     "BRAILLE LINE:  'Value: 20'",
+    "     VISIBLE:  'Value: 20', cursor=1",
+    "BRAILLE LINE:  'Value: vertical ScrollBar 16%'",
+    "     VISIBLE:  'Value: vertical ScrollBar 16%', cursor=8"]))
+
+
+sequence.append(utils.AssertionSummaryAction())
+
+sequence.start()
