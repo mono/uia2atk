0aa3be8965d1eb73bceb6a055b355a2e0ee97d18
Calen Chen <cachen@mono-cvs.ximian.com>
2009-04-08 10:37:06 +0000

    update datagrid frame wraper and tests
    svn path=/trunk/uia2atk/; revision=131313

--- test/testers/datagrid/datagridframe.py
+++ test/testers/datagrid/datagridframe.py
@@ -30,63 +30,72 @@ class DataGridFrame(accessibles.Frame):
         super(DataGridFrame, self).__init__(accessible)
         self.label = self.findLabel(self.LABEL)
         self.treetable = self.findTreeTable(None)
-        #there are 4 columns under TreeTable
+        # there are 4 columns under TreeTable
         self.bool_column = self.treetable.findTableColumnHeader(self.COLUMN_A, checkShowing=False)
         self.edittext_column = self.treetable.findTableColumnHeader(self.COLUMN_C, checkShowing=False)
         self.readtext_column = self.treetable.findTableColumnHeader(self.COLUMN_B, checkShowing=False)
         self.combobox_column = self.treetable.findTableColumnHeader(self.COLUMN_D, checkShowing=False)
-        #there are 4 kind of TableCells under TreeTable
-        self.nullbool_cell = self.treetable.findTableCell("(null)", checkShowing=False)
+        # there are 4 kind of TableCells under TreeTable
+        self.null_cell = self.treetable.findTableCell("(null)", checkShowing=False)
         self.true_cell = self.treetable.findTableCell("True", checkShowing=False)
         self.false_cell = self.treetable.findTableCell("False", checkShowing=False)
         self.edit_cells = self.treetable.findAllTableCells(re.compile("Edit*"), checkShowing=False)
         self.read_cells = self.treetable.findAllTableCells(re.compile("Read*"), checkShowing=False)
         self.combobox_cells = self.treetable.findAllTableCells(re.compile("Box*"), checkShowing=False)
         self.item_menuitems = self.treetable.findAllMenuItems(re.compile("Item*"), checkShowing=False)
-        #search for initial position for assert order test
+        # search for initial position for assert order test
         self.read0_position = self.read_cells[0]._getAccessibleCenter()
         self.edit0_position = self.edit_cells[0]._getAccessibleCenter()
         self.read2_position = self.read_cells[2]._getAccessibleCenter()
         self.edit2_position = self.edit_cells[2]._getAccessibleCenter()
 
-    #give 'click' action
     def click(self,accessible):
+        """
+        Wrap strongwind click action to add log
+
+        """
         procedurelogger.action("click %s" % accessible)
         accessible.click()
 
-    #assert Label is changed due to CurrentCellChanged
     def assertLabel(self, newlabel):
+        """
+        Assert label is changed to newlabel
 
+        """
         procedurelogger.expectedResult('label is changed to "%s"' % newlabel)
 
         assert self.label.text == newlabel, "%s not match %s" % \
                                            (self.label.text, newlabel)
 
-    #assert defaul AtkText for all widgets
     def assertDefaultText(self, testtype, actual=[], expected=[]):
-        #test column header
+        """
+        Ensure that each widgets has the text value that is expected by compare
+	actual list and expected list via different testtype
+
+        """
+        # test column header type
         if testtype == "ColumnHeader":
             actual = [self.bool_column.text, self.edittext_column.text, self.readtext_column.text, self.combobox_column.text]
             expected = ["BoolColumn", "TextBox_Edit", "TextBox_Read", "ComboBox"]
 
-        #test boolean
+        # test boolean type
         elif testtype == "BoolColumn":
             actual = [self.nullbool_cell.text, self.true_cell.text, self.false_cell.text]
             expected = ["(null)", "True", "False"]
 
-        #test edit text box
+        # test edit text box type
         elif testtype == "TextBox_Edit":
             for i in range(3):
                 actual.append(self.edit_cells[i].text)
                 expected.append("Edit%s" % i)
 
-        #test read text box
+        # test read text box type
         elif testtype == "TextBox_Read":
             for i in range(3):
                 actual.append(self.read_cells[i].text)
                 expected.append("Read%s" % i)
 
-        #test combobox type
+        # test combobox type type
         elif testtype == "Combobox":
             for i in range(3):
                 actual.append(self.box_cells[i].text)
@@ -96,22 +105,22 @@ class DataGridFrame(accessibles.Frame):
             procedurelogger.action("check Text default Value for %s" % e)
 
             procedurelogger.expectedResult("%s Text is %s" % (a, e))
-            assert a == e, "%s not matsh %s" % (a, e)
+            assert a == e, "%s not match %s" % (a, e)
 
-    #assert Text implementation for cells
-    def assertText(self, accessible, textvalue):
+    def assertText(self, accessible, newtext):
+        """
+        Ensure accessible's Text is changed to newtext
+
+        """
         procedurelogger.action("check %s Text Value" % accessible)
 
-        assert accessible.text == textvalue, "%s not match %s" % (accessible, textvalue)
+        assert accessible.text == newtext, "%s not match %s" % (accessible, newtext)
 
-    #assert Name implementation for cells
-    def assertName(self, accessible, namevalue):
-        procedurelogger.expectedResult('%s Name is %s' % (accessible, namevalue))
-
-        assert accessible.name == namevalue, "%s not match %s" % (accessible, namevalue)
-
-    #assert Selection implementation
     def assertSelectionChild(self, accessible, childIndex):
+        """
+        Select childIndex to test its parent's AtkSelection
+
+        """
         procedurelogger.action('selecte childIndex %s in "%s"' % (childIndex, accessible))
 
         accessible.selectChild(childIndex)
@@ -121,8 +130,12 @@ class DataGridFrame(accessibles.Frame):
 
         accessible.clearSelection()
 
-    #assert Table implementation for List role to check row and column number is matched
     def assertTable(self, accessible, row=4, col=1):
+        """
+        Assert AtkTable implementation to ensure TreeTable has row and column 
+	number that is expected
+
+        """
         procedurelogger.action('check "%s" Table implemetation' % accessible)
         itable = accessible._accessible.queryTable()
 
@@ -130,31 +143,54 @@ class DataGridFrame(accessibles.Frame):
         assert itable.nRows == row and itable.nColumns == col, "Not match Rows %s and Columns %s" \
                                                                   % (itable.nRows, itable.nColumns)
 
-    #assert item's order after click TableColumnHeader
-    def assertOrder(self, itemone=None):        
-        if itemone == "Read2":
-            procedurelogger.expectedResult('Read2 and Edit2 change position to %s and %s' % (self.read0_position, self.edit0_position))
+    def clickColumnHeaderToSortOrder(self, accessible, actionway, firstitem=None):  
+        """
+        Click TableColumnHeader to sort column's order, then check if the 
+        order is changed
+
+        """
+        # Action
+        if actionway is "click":
+            self.click(accessible)
+            sleep(config.SHORT_DELAY)
+        elif actionway is "mouseClick":
+            accessible.mouseClick()
+            sleep(config.SHORT_DELAY)
+
+        # Expected result     
+        if firstitem == "Read2":
+            procedurelogger.expectedResult('Read2 and Edit2 change position to \
+			%s and %s' % (self.read0_position, self.edit0_position))
             read2_new_position = self.read_cells[2]._getAccessibleCenter()
             edit2_new_position = self.edit_cells[2]._getAccessibleCenter()
 
-            assert read2_new_position == self.edit0_position and \
+            assert read2_new_position == self.read0_position and \
                    edit2_new_position == self.edit0_position
-        elif itemone == "Read0":
-            procedurelogger.expectedResult('Read0 and Edit0 change position to %s and %s' % (self.Read0_position, self.Edit0_position))
+        elif firstitem == "Read0":
+            procedurelogger.expectedResult('Read0 and Edit0 change position to \
+			%s and %s' % (self.Read0_position, self.Edit0_position))
             Read0_new_position = self.read_cells[0]._getAccessibleCenter()
             Edit0_new_position = self.edit_cells[1]._getAccessibleCenter()
 
             assert Read0_new_position == self.Read0_position and \
                    Edit0_new_position == self.Edit0_position
 
-    #enter Text Value to make sure the ReadOnly TextBox is uneditable
-    def enterTextValue(self, accessible, entertext, oldtext=None):
+    def changeText(self, accessible, newtext, oldtext=None):
+        """
+        Check EditableText for accessibles which under TextBox_Edit column;
+	Check Text for accessibles which under TextBox_Read column by change 
+	oldtext to newtext. 
+
+        If accessible is editable that text is changed, 
+	otherwise, if accessible is not editable that oldtext is remained 
+
+        """
         procedurelogger.action('try input %s in %s which is uneditable' % (entertext, accessible))
 
         if entertext == "editable":
             procedurelogger.expectedResult("%s text is %s" % (accessible,entertext))
             assert accessible.text == entertext
-        else:
+        elif entertext == "uneditable":
             try:
                 accessible.text = entertext
             except NotImplementedError:
@@ -166,6 +202,6 @@ class DataGridFrame(accessibles.Frame):
             assert accessible.text == oldtext
 
     
-    #close application main window after running test
+    # close application main window after running test
     def quit(self):
         self.altF4()
--- test/testers/datagrid_basic_ops.py
+++ test/testers/datagrid_basic_ops.py
@@ -43,9 +43,9 @@ if app is None:
 # just an alias to make things shorter
 dgFrame = app.dataGridFrame
 
-#####################################################
-##check default Text
-#####################################################
+####################
+# check default Text
+####################
 dgFrame.assertDefaultText("ColumnHeader")
 
 dgFrame.assertDefaultText("BoolColumn")
@@ -56,137 +56,144 @@ dgFrame.assertDefaultText("TextBox_Read")
 
 dgFrame.assertDefaultText("ComboBox")
 
-######################################################
-##check Actions
-##check States
-##AtkAction test to change label and states
-##AtkComponent test by mouse click
-##key navigate to change focus
-######################################################
+###############
+# check Actions
+###############
 
-#check DataGrid TableColumnHeader's actions list
-actionsCheck(dgFrame.bool_column, "TableColumnHeader")
-actionsCheck(dgFrame.readtext_column, "TableColumnHeader")
-actionsCheck(dgFrame.edittext_column, "TableColumnHeader")
-actionsCheck(dgFrame.combobox_column, "TableColumnHeader")
+# check DataGrid TableColumnHeaders' actions
+columnheaders = dgFrame.findAllTableColumnHeaders(None)
+for i in range(len(columnheaders)):
+    actionsCheck(columnheaders[i], "TableColumnHeader")
 
-#check DataGridBoolColumn TableCell actions list
-actionsCheck(dgFrame.nullbool_cell, "TableCell")
-actionsCheck(dgFrame.edit_cells[0], "TableCell")
-actionsCheck(dgFrame.read_cells[0], "TableCell")
+# check DataGrid TableCells' actions
+tablecells = dgFrame.findAllTableCells(None)
+for i in range(len(tablecells)):
+    actionsCheck(tablecells[i], "TableCell")
 
-#check TreeTable 's states
+##############
+# check States
+##############
+
+# check TreeTable 's states
 statesCheck(dgFrame.treetable, "TreeTable", add_states=['focused'])
 
-#check TableColumnHeader's states
-statesCheck(dgFrame.bool_column, "TableColumnHeader")
-statesCheck(dgFrame.readtext_column, "TableColumnHeader")
-statesCheck(dgFrame.edittext_column, "TableColumnHeader")
-statesCheck(dgFrame.combobox_column, "TableColumnHeader")
+# check TableColumnHeaders' states
+for i in range(len(columnheaders)):
+    statesCheck(columnheaders[i], "TableColumnHeader")
 
-#focus on first TableCell(row0, col0) default
-statesCheck(dgFrame.nullbool_cell, "TableCell", add_states=["focused"])
+# focus on first TableCell(row0, col0) default
+## BUG480831: cells under BoolColumn shouldn't implement Atk.EditableText
+#statesCheck(dgFrame.null_cell, "TableCell", add_states=["focused"])
 
-#mouse click bool_column doesn't raise "focused" because HasKeyboardFocus is False
+# mouse click columnheader doesn't raise "focused" because HasKeyboardFocus is False
 dgFrame.bool_column.mouseClick()
 sleep(config.SHORT_DELAY)
 statesCheck(dgFrame.bool_column, "TableColumnHeader")
 
-#mouse click combobox_column doesn't raise "focused" because HasKeyboardFocus is False
 dgFrame.combobox_column.mouseClick()
 sleep(config.SHORT_DELAY)
 statesCheck(dgFrame.combobox_column, "TableColumnHeader")
 
-#mouse click TableCell 'Edit0' to raise "focused"
+############################################################
+# AtkAction, keyCombo, mouseClick to change label and states
+############################################################
+
+# mouse click TableCell 'Edit0' to raise "focused"
 dgFrame.edit_cells[0].mouseClick()
 sleep(config.SHORT_DELAY)
 statesCheck(dgFrame.edit_cells[0], "TableCell", add_states=["focused", "editable"])
 dgFrame.assertLabel("row:0 col:1 Value:Edit0")
 
-#keyRight move focus to 'Read0'
+# keyRight move focus to 'Read0'
 dgFrame.keyCombo("Right", grabFocus=False)
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.read_cells[0], "TableCell", add_states=["focused"])
+## BUG479796: read_cells shouldn't have editable state
+#statesCheck(dgFrame.read_cells[0], "TableCell", add_states=["focused"])
 statesCheck(dgFrame.edit_cells[0], "TableCell", add_states=["editable"])
 dgFrame.assertLabel("row:0 col:2 Value:Read0")
 
-#keyDown move focus to 'Read1'
+# keyDown move focus to 'Read1'
 dgFrame.keyCombo("Down", grabFocus=False)
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.read_cells[1], "TableCell", add_states=["focused"])
-statesCheck(dgFrame.read_cells[0], "TableCell")
-dgFrame.assertLabel("row:1 col:1 Value:Read1")
+## BUG479796: read_cells shouldn't have editable state
+#statesCheck(dgFrame.read_cells[1], "TableCell", add_states=["focused"])
+#statesCheck(dgFrame.read_cells[0], "TableCell")
+dgFrame.assertLabel("row:1 col:2 Value:Read1")
 
-#do click action for TableCell 'Edit2' to raise "checked"
+# click TableCell 'Edit2' to raise "selected" for cells which in the same row
 dgFrame.click(dgFrame.edit_cells[2])
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.edit_cells[2], "TableCell", add_states=["focused", "editable"])
-statesCheck(dgFrame.read_cells[1], "TableCell")
-dgFrame.assertLabel("row:2 col:1 Value:Edit2")
+statesCheck(dgFrame.edit_cells[2], "TableCell", add_states=["selected", "editable"])
+## BUG479796, BUG480831: read_cells and bool_cells and dropdown_combobox shouldn't have editable state
+#statesCheck(dgFrame.combobox_cells[2], "TableCell", add_states=["selected"])
+#statesCheck(dgFrame.false_cell, "TableCell", add_states=["selected"])
+#statesCheck(dgFrame.read_cells[2], "TableCell", add_states=["selected"])
+# read_cells[1] get rid of selected but still focused
+#statesCheck(dgFrame.read_cells[1], "TableCell", add_states=["focused"])
+dgFrame.assertLabel("row:1 col:2 Value:Read1")
 
-#do click move focus to 'Box1' 
+# do click move focus to 'Box1' 
 dgFrame.click(dgFrame.combobox_cells[1])
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.combobox_cells[1], "TableCell", add_states=["focused"])
-statesCheck(dgFrame.edit_cells[2], "TableCell", add_states=["editable"])
-dgFrame.assertLabel("row:0 col:3 Value:Box1")
+statesCheck(dgFrame.edit_cells[1], "TableCell", add_states=["selected", "editable"])
+## BUG479796, BUG480831: read_cells and bool_cells and dropdown_combobox shouldn't have editable state
+#statesCheck(dgFrame.combobox_cells[1], "TableCell", add_states=["selected"])
+#statesCheck(dgFrame.true_cell, "TableCell", add_states=["selected"])
+#statesCheck(dgFrame.read_cells[1], "TableCell", add_states=["selected"])
+# edit_cells[2] still selected
+statesCheck(dgFrame.edit_cells[2], "TableCell", add_states=["selected", "editable"])
+dgFrame.assertLabel("row:1 col:2 Value:Read1")
 
-########################
-##test AtkSelection
-########################
+###################
+# test AtkSelection
+###################
 
-#select index0 'BoolColumn to raise selected
+# select ColumnHeader doesn't raise selected
 dgFrame.assertSelectionChild(dgFrame.treetable, 0)
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.bool_column, "TableColumnHeader", add_states = ["selected"])
+statesCheck(dgFrame.bool_column, "TableColumnHeader")
 
-#select index5 'Edit0' to raise selected for row1 TableCells
-dgFrame.assertSelectionChild(dgFrame.treetable, 3)
+# select index5 'Edit0' to raise selected for row1 TableCells
+dgFrame.assertSelectionChild(dgFrame.treetable, 5)
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.nullbool_cell, "TableCell", add_states=["selected"])
+## BUG479796, BUG480831: read_cells and bool_cells and dropdown_combobox shouldn't have editable state
+#statesCheck(dgFrame.null_cell, "TableCell", add_states=["selected"])
 statesCheck(dgFrame.edit_cells[0], "TableCell", add_states=["selected", "editable"])
-statesCheck(dgFrame.read_cells[0], "TableCell", add_states=["selected"])
-statesCheck(dgFrame.combobox_cells[0], "TableCell", add_states=["selected"])
+#statesCheck(dgFrame.read_cells[0], "TableCell", add_states=["selected"])
+#statesCheck(dgFrame.combobox_cells[0], "TableCell", add_states=["selected"])
 
-#clear selection to get rid of "selected" state from row1 TableCells
+# clear selection to get rid of "selected" state from row1 TableCells
 dgFrame.assertClearSelection(dgFrame.treetable)
 sleep(config.SHORT_DELAY)
-statesCheck(dgFrame.nullbool_cell, "TableCell")
-statesCheck(dgFrame.edit_cells[0], "TableCell")
-statesCheck(dgFrame.read_cells[0], "TableCell")
-statesCheck(dgFrame.combobox_cells[0], "TableCell")
+## BUG479796, BUG480831: read_cells and bool_cells and dropdown_combobox shouldn't have editable state
+#statesCheck(dgFrame.null_cell, "TableCell")
+statesCheck(dgFrame.edit_cells[0], "TableCell", add_states=["editable"])
+#statesCheck(dgFrame.read_cells[0], "TableCell")
+#statesCheck(dgFrame.combobox_cells[0], "TableCell")
 
-#####################################################
-##test AtkTable
-#####################################################
+###############
+# test AtkTable
+###############
 
-#check table's table implementation
+# check table's table implementation
 dgFrame.assertTable(dgFrame.treetable, row=3, col=4)
 
-###########################################################
-##test AtkAction for ColumnHeader to sort order
-###########################################################
+###############################################
+# test AtkAction for ColumnHeader to sort order
+###############################################
 
-#check item's order after click column header
-dgFrame.click(dgFrame.bool_column)
-sleep(config.SHORT_DELAY)
-dgFrame.assertOrder(itemone="Read2")
+# check item's order after click column header
+##click action doesn't work BUG476304
+#dgFrame.clickColumnHeaderToSortOrder(dgFrame.bool_column, "click", firstitem="Read2")
+#dgFrame.clickColumnHeaderToSortOrder(dgFrame.readtext_column, "click", firstitem="Read0")
 
-dgFrame.click(dgFrame.readtext_column)
-sleep(config.SHORT_DELAY)
-dgFrame.assertOrder(itemone="Read0")
+# check item's order after mouse click column header, also the test can check 
+# column header's position
+## BUG493176: TableCell doesn't exchange position
+#dgFrame.clickColumnHeaderToSortOrder(dgFrame.edittext_column, "mouseClick", firstitem="Read2")
+#dgFrame.clickColumnHeaderToSortOrder(dgFrame.combobox_column, "mouseClick", firstitem="Read0")
 
-#check item's order after mouse click column header, also the test can check 
-#column header's position
-dgFrame.edittext_column.mouseClick()
-sleep(config.SHORT_DELAY)
-dgFrame.assertOrder(itemone="Read2")
-
-dgFrame.combobox_column.mouseClick()
-sleep(config.SHORT_DELAY)
-dgFrame.assertOrder(itemone="Read0")
-
-#close application frame window
+# close application frame window
 dgFrame.quit()
 
 print "INFO:  Log written to: %s" % config.OUTPUT_DIR
