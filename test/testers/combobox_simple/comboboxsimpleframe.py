c7a5d54d36229d2b15f785bdb8680cdc969425dd
Calen Chen <cachen@mono-cvs.ximian.com>
2009-05-05 07:55:10 +0000

    check in changes from combobox_simple review(http://reviews.mono-a11y.org/r/150/)
    svn path=/trunk/uia2atk/; revision=133564

--- test/testers/combobox_simple/comboboxsimpleframe.py
+++ test/testers/combobox_simple/comboboxsimpleframe.py
@@ -1,6 +1,7 @@
 # vim: set tabstop=4 shiftwidth=4 expandtab
 ##############################################################################
 # Written by:  Cachen Chen <cachen@novell.com>
+#              Ray Wang <rawang@novell.com>
 # Date:        11/10/2008
 # Description: Application wrapper for combobox_simple
 #              be called by ../combobox_simple_ops.py
@@ -12,38 +13,55 @@ from strongwind import *
 
 class ComboBoxSimpleFrame(accessibles.Frame):
     """the profile of the combobox_simple sample"""
+    LABEL = "You select "
 
     def __init__(self, accessible):
         super(ComboBoxSimpleFrame, self).__init__(accessible)
-
+        self.label = self.findLabel(self.LABEL)
         self.combobox = self.findComboBox(None)
         self.textbox = self.findText(None)
-        self.tree = self.findTreeTable(None)
+        self.treetable = self.findTreeTable(None)
         self.tablecell = dict([(x, self.findTableCell(str(x))) for x in range(10)])
 
-        print "####################"
-        print len(self.tree)
-        print len(self.combobox)
-        print "####################"
-
     def click(self,accessible):
+        """'click' action"""
+        procedurelogger.action("click %s" % accessible)
         accessible.click()
 
-    def inputText(self, accessible, text):
-        try:
-            accessible.text = text
-        except NotImplementedError:
-            pass
+    def editableTextIsUnimplemented(self, accessible):
+        """make sure EditableText of accessible is unimplemented"""
+        procedurelogger.action("check %s's text" % accessible)
 
-    def assertText(self, accessible, text):
+        procedurelogger.expectedResult("%s's text is uneditable" % accessible)
+        try:
+            accessible._accessible.queryEditableText()
+        except NotImplementedError:
+            return
+        assert False, "EditableText shouldn't be implemented"
+
+    def assertLabel(self, expected_label):
+        """make sure label's text is expected"""
+        procedurelogger.expectedResult("Lable's text is %s" % expected_label)
+        assert self.label.text == expected_label, \
+                                  "actual label is %s, expected label is %s" % \
+                                               (self.label.text, expected_label)
+
+    def assertText(self, accessible, expected_text, actionlog=None):
+        """make sure accessible's text is expected"""
+        if actionlog is True:
+            procedurelogger.action("check %s's text" % accessible)
+
         procedurelogger.expectedResult('%s\'s text is %s' % \
-                                            (accessible, accessible.text))
-        assert accessible.text == text, "%s is not equal to %s" % \
-                                            (accessible.text, text)
+                                            (accessible, expected_text))
+        assert accessible.text == expected_text, \
+                                    "actual text is %s, expected text is %s" % \
+                                            (accessible.text, expected_text)
     
     def assertTable(self, accessible, row=None, col=None):
-        """assert Table implementation"""
+        """
+        assert Table implementation, make sure row and col's number are expected
 
+        """
         procedurelogger.action('check "%s" Table implemetation' % accessible)
         itable = accessible._accessible.queryTable()
         procedurelogger.expectedResult('"%s" have %s Rows and %s Columns' % \
@@ -53,10 +71,12 @@ class ComboBoxSimpleFrame(accessibles.Frame):
                                         (itable.nRows, itable.nColumns)
 
     def selectChild(self, accessible, index):
+        """assert Selection implementation"""
         procedurelogger.action('select item%s in "%s"' % (index, accessible))
         accessible.selectChild(index)
 
     def clearSelection(self, accessible):
+        """assert ClearSelection implementation"""
         procedurelogger.action('clear selection in "%s"' % (accessible))
         accessible.clearSelection()
 
--- test/testers/combobox_simple_ops.py
+++ test/testers/combobox_simple_ops.py
@@ -4,7 +4,7 @@
 # Written by:  Cachen Chen <cachen@novell.com>
 #              Ray Wang <rawang@novell.com>
 # Date:        11/10/2008
-# Description: main test script of toolstripcombobox
+# Description: main test script for combobox simple style
 #              ../samples/combobox_simple.py is the test sample script
 #              combobox_simple/* are the wrappers of combobox_simple test sample
 ##############################################################################
@@ -44,37 +44,33 @@ cbFrame = app.comboBoxSimpleFrame
 ##############################
 # check Combobox children's AtkAction
 ##############################
-#check tablecell's actions
-#for i in range(10):
-#    actionsCheck(cbFrame.tablecell[i], "TableCell")
+# check tablecell's actions
+for i in range(10):
+    actionsCheck(cbFrame.tablecell[i], "TableCell")
 
 ##############################
 # check Combobox children's AtkAccessible
 ##############################
-#check ComboBox's states list
-# TODO: BUG483233, Extraneous "focuable" and "manages descendants" states, 
+# check ComboBox's states list
+# TODO: BUG483223, Extraneous "focuable" and "manages descendants" states, 
 # and reluctant "focused" state
 #statesCheck(cbFrame.combobox, "ComboBox")
-# TODO: BUG483234, Extraneous "focuable" state
-#statesCheck(cbFrame.textbox, "TextBox", add_states=["focused"])
-# TODO: BUG483235, Extraneous "selectable" state, 
+statesCheck(cbFrame.textbox, "TextBox", add_states=["selectable", "focused"])
+# TODO: BUG483225, Extraneous "selectable" state, 
 # missing "manages descendants" state, and reluctant "focused" state
-#statesCheck(cbFrame.tree, "TreeTable")
+#statesCheck(cbFrame.treetable, "TreeTable")
 # TODO: BUG483236, missing 'focusable', 'transient' and  'single line' states
 #for i in range(10):
 #    statesCheck(cbFrame.tablecell[i], "TableCell")
 
 ##############################
-# check Combobox AtkSelection
+# test AtkText
 ##############################
-# TODO: put some tests here
+# check "text" for TableCells
+for i in range(10):
+    cbFrame.assertText(cbFrame.tablecell[i], str(i), actionlog=True)
 
-##############################
-# check "text" in combo box
-##############################
-# TODO: put some tests for Screamable Content here
-
-# check text's "Text(Editable)"
+# check TextBox's Text is editable
 cbFrame.textbox.typeText("2")
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "2")
@@ -83,73 +79,106 @@ cbFrame.textbox.enterText("9")
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "9")
 
-##############################
-# check "treetable"'s AtkSelection and AtkTable
-##############################
-# TODO: mark all statesCheck due to BUG483236
-# check AtkSelecton
-cbFrame.selectChild(cbFrame.tree, 0)
+# check "tablecell"'s Text is not editable
+cbFrame.editableTextIsUnimplemented(cbFrame.tablecell[0])
 sleep(config.SHORT_DELAY)
-cbFrame.assertText(cbFrame.textbox, "0")
-#statesCheck(cbFrame.tablecell[0], "TableCell", add_states=["selected"])
+cbFrame.assertText(cbFrame.tablecell[0], "0")
 
-cbFrame.selectChild(cbFrame.tree, 9)
+#############################
+# test AtkAction
+#############################
+cbFrame.click(cbFrame.tablecell[5])
+sleep(config.SHORT_DELAY)
+cbFrame.assertText(cbFrame.textbox, "5")
+cbFrame.assertLabel("You select 5")
+
+cbFrame.click(cbFrame.tablecell[9])
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "9")
+cbFrame.assertLabel("You select 9")
+
+##############################
+# check Combobox AtkSelection
+##############################
+# set index 0 to select text
+#cbFrame.selectChild(cbFrame.combobox, 0)
+#sleep(config.SHORT_DELAY)
+## BUG488474, selectChild called the selection interface's selectChild
+## method, which is not working.
+#cbFrame.assertText(cbFrame.textbox, "0")
+## doesn't rise 'selected' state for Menu and Text due to BUG456341
+#statesCheck(cbFrame.textbox, "Text", add_states=["focused", "selected"])
+
+##############################
+# check "treetable"'s AtkSelection
+##############################
+# TODO: mark all statesCheck due to BUG483226
+# check AtkSelecton
+cbFrame.selectChild(cbFrame.treetable, 0)
+sleep(config.SHORT_DELAY)
+cbFrame.assertText(cbFrame.textbox, "0")
+cbFrame.assertLabel("You select 0")
+#statesCheck(cbFrame.tablecell[0], "TableCell", add_states=["selected"])
+
+cbFrame.selectChild(cbFrame.treetable, 9)
+sleep(config.SHORT_DELAY)
+cbFrame.assertText(cbFrame.textbox, "9")
+cbFrame.assertLabel("You select 9")
 #statesCheck(cbFrame.tablecell[9], "TableCell", add_states=["selected"])
 
 cbFrame.tablecell[1].mouseClick()
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "1")
+cbFrame.assertLabel("You select 1")
 #statesCheck(cbFrame.tablecell[1], "TableCell", add_states=["selected"])
 
 cbFrame.tablecell[8].mouseClick()
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "8")
+cbFrame.assertLabel("You select 8")
 #statesCheck(cbFrame.tablecell[8], "TableCell", add_states=["selected"])
 
 # press Up/Down 
 cbFrame.keyCombo("Up", grabFocus=False)
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "7")
+cbFrame.assertLabel("You select 7")
 #statesCheck(cbFrame.tablecell[7], "TableCell", add_states=["selected"])
 
 cbFrame.keyCombo("Down", grabFocus=False)
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "8")
+cbFrame.assertLabel("You select 8")
 #statesCheck(cbFrame.tablecell[8], "TableCell", add_states=["selected"])
 
 # press PageUp/PageDown
 cbFrame.keyCombo("PageUp", grabFocus=False)
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "0")
+cbFrame.assertLabel("You select 0")
 #statesCheck(cbFrame.tablecell[0], "TableCell", add_states=["selected"])
 
 cbFrame.keyCombo("PageDown", grabFocus=False)
 sleep(config.SHORT_DELAY)
 cbFrame.assertText(cbFrame.textbox, "9")
+cbFrame.assertLabel("You select 9")
 #statesCheck(cbFrame.tablecell[9], "TableCell", add_states=["selected"])
 
-# TODO: mark all statesCheck due to BUG483235
+# TODO: mark all statesCheck due to BUG483225
 # check clear Selection
-cbFrame.clearSelection(cbFrame.tree)
-sleep(config.SHORT_DELAY)
+#cbFrame.clearSelection(cbFrame.treetable)
+#sleep(config.SHORT_DELAY)
 #statesCheck(cbFrame.tree, "TreeTable")
 
-# TODO: mark all statesCheck due to BUG483233
-cbFrame.clearSelection(cbFrame.combobox)
-sleep(config.SHORT_DELAY)
+# TODO: mark all statesCheck due to BUG483223
+#cbFrame.clearSelection(cbFrame.combobox)
+#sleep(config.SHORT_DELAY)
 #statesCheck(cbFrame.combobox, "ComboBox")
 
-# check AtkTable
-cbFrame.assertTable(cbFrame.tree, 10, 1)
-
 ##############################
-# check "tablecell"'s AtkText
+# check "treetable"'s AtkTable
 ##############################
-cbFrame.inputText(cbFrame.tablecell[0], "10")
-sleep(config.SHORT_DELAY)
-cbFrame.assertText(cbFrame.tablecell[0], "0")
+cbFrame.assertTable(cbFrame.treetable, 10, 1)
 
 ##############################
 # End
