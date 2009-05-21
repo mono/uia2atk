e30beb04a7e901e636353a8b1659f614319669a1
Calen Chen <cachen@mono-cvs.ximian.com>
2009-05-21 08:43:23 +0000

    update colordialog tests from review board
    svn path=/trunk/uia2atk/; revision=134513

--- test/testers/colordialog/colordialogframe.py
+++ test/testers/colordialog/colordialogframe.py
@@ -1,4 +1,3 @@
-
 ##############################################################################
 # Written by:  Cachen Chen <cachen@novell.com>
 # Date:        02/23/2009
@@ -15,6 +14,7 @@ from strongwind import *
 from colordialog import *
 from sys import path
 from helpers import *
+from states import *
 
 # class to represent the main window.
 class ColorDialogFrame(accessibles.Frame):
@@ -23,64 +23,84 @@ class ColorDialogFrame(accessibles.Frame):
     # the available widgets on the window
     BUTTON1 = "ColorDialog Button"
     LABEL = "Examples for: ColorDialog"
+    NUMPANELS = 5
+    NUMTEXTS = 6
+    NUMCOLORBUTTONS = 64
 
     def __init__(self, accessible):
         super(ColorDialogFrame, self).__init__(accessible)
-        self.colordialog_button = self.findPushButton(self.BUTTON1)
+        self.color_dialog_button = self.findPushButton(self.BUTTON1)
         self.main_label= self.findLabel(self.LABEL)
         self.main_text = self.findText(None)
 
-    def click(self, button):
+    def openColorDialog(self):
+        '''
+        Opens the color dialog by clicking on the color_dialog_button and 
+        then finds the dialog accessible for the color dialog
+        '''
+        self.color_dialog_button.click()
+        # A longer wait here, because there are a lot of accessibles that
+        # might need time to load
+        sleep(config.MEDIUM_DELAY)
+
+    def findAllColorDialogAccessibles(self):
         """
-        wrap strongwind click action to be used by click button to invoke 
-        color dialog
-
+        Use strongwind find* method to search for all widgets in Color dialog. 
         """
+        # find the 4 push buttons in the dialog, the "Add To Custom Colors"
+        # button isn't showing initially
+        procedurelogger.action('Search for all widgets from "Color" Dialog')
 
-        button.click()
+        # find the "Color" dialog, first of all
+        self.color_dialog = self.app.findDialog("Color")
 
-        # click button will invoke dialog page
-        self.colordialog = self.app.findDialog("Color")
+        self.ok_button = self.color_dialog.findPushButton("OK")
+        self.cancel_button = self.color_dialog.findPushButton("Cancel")
+        self.def_custom_colors_button = \
+                     self.color_dialog.findPushButton("Define Custom Colors >>")
+        self.add_color_button = \
+                        self.color_dialog.findPushButton("Add To Custom Colors",
+                                                        checkShowing=False)
 
-    def AssertWidgets(self):
-        """
-        Use strongwind Find* method to search for all widgets in Color Dialog, 
-        Make sure all widgets are showing
+        # There should be five panels.
+        self.panels = self.color_dialog.findAllPanels(None, checkShowing=False)
+        assert len(self.panels) == 5, \
+                        "There should be exactly %s panels, found %s" % \
+                        (self.NUMPANELS, len(self.panels))
+        self.colors_panel = self.panels[0]
+        self.custom_colors_panel = self.panels[1]
+        self.color_shader_panel = self.panels[2]
+        self.slider_panel = self.panels[3]
+        self.color_panel = self.panels[4]
 
-        """
-        procedurelogger.action("search for all widgets from ColorDialog windows")
+        # There are NUMCOLORBUTTONS SmallColorControls showing as PushButton
+        self.small_color_buttons = self.panels[0].findAllPushButtons(None)
+        assert len(self.small_color_buttons) == self.NUMCOLORBUTTONS, \
+                           "Expected exactly %s buttons, found %s" % \
+                           (self.NUMCOLORBUTTONS, len(self.small_color_buttons))
 
-        # there are 4 push buttons in dialog
-        procedurelogger.expectedResult("4 PushButtons are showing up")
-        self.ok_button = self.colordialog.findPushButton("OK")
-        self.cancel_button = self.colordialog.findPushButton("Cancel")
-        self.definecolor_button = self.colordialog.findPushButton("Define Custom Colors >>")
-        self.addcolor_button = self.colordialog.findPushButton("Add To Custom Colors", checkShowing=False)
-        # click definecolor_button
-        self.click(self.definecolor_button)
-        sleep(config.SHORT_DELAY)
-        # there are 1 panel for SmallColorControls and 4 panels for custom colors
-        procedurelogger.expectedResult("5 Panels are showing up")      
-        self.panels = self.colordialog.findAllPanels(None)
-        assert len(self.panels) == 5, "%s not match 4" % len(self.panels)
-        # there are 64 SmallColorControls showing as PushButton
-        procedurelogger.expectedResult("64 SmallColorControls are showing up as PushButton")
-        self.smallcolor_buttons = self.panels[0].findAllPushButtons(None)
-        assert len(self.smallcolor_buttons) == 64, "%s not match 64" % len(self.smallcolor_buttons)
-        # there are 9 Labels in dialog
-        procedurelogger.expectedResult("9 Labels are showing up")
-        self.base_label = self.colordialog.findLabel("Base Colors:")
-        self.user_label = self.colordialog.findLabel("User Colors:")
-        self.color_label = self.colordialog.findLabel("Color")
-        self.hue_label = self.colordialog.findLabel("Hue:")
-        self.sat_label = self.colordialog.findLabel("Sat:")
-        self.bri_label = self.colordialog.findLabel("Bri:")
-        self.Red_label = self.colordialog.findLabel("Red:")
-        self.Green_label = self.colordialog.findLabel("Green:")
-        self.Blue_label = self.colordialog.findLabel("Blue:")
-        # there are 6 TextBoxs in dialog
-        procedurelogger.expectedResult("6 TextBoxs are showing up")
-        self.texts = self.colordialog.findAllTexts(None)
+        # Find "Base Colors" and "User Colors" labels from default Color
+        # dialog
+        self.base_colors_label = self.color_dialog.findLabel("Base Colors:")
+        self.user_colors_label = self.color_dialog.findLabel("User Colors:")
+
+        # Find the other labels that show on the custom colors dialog
+        self.color_label = \
+                        self.color_dialog.findLabel("Color", checkShowing=False)
+        self.hue_label = self.color_dialog.findLabel("Hue:", checkShowing=False)
+        self.sat_label = self.color_dialog.findLabel("Sat:", checkShowing=False)
+        self.bri_label = self.color_dialog.findLabel("Bri:", checkShowing=False)
+        self.red_label = self.color_dialog.findLabel("Red:", checkShowing=False)
+        self.green_label = \
+                       self.color_dialog.findLabel("Green:", checkShowing=False)
+        self.blue_label = \
+                        self.color_dialog.findLabel("Blue:", checkShowing=False)
+
+        # there are NUMTEXTS text accessibles in the Color dialog
+        self.texts = self.color_dialog.findAllTexts(None, checkShowing=False)
+        assert len(self.texts) == self.NUMTEXTS, \
+                               "Expected exactly %s texts, found %s" % \
+                               (self.NUMTEXTS, len(self.texts))
         self.hue_text = self.texts[0]
         self.sat_text = self.texts[1]
         self.bri_text = self.texts[2]
@@ -88,79 +108,168 @@ class ColorDialogFrame(accessibles.Frame):
         self.breen_text = self.texts[4]
         self.blue_text = self.texts[5]
 
+    def checkColorDialogStates(self, is_custom_showing):
+        '''
+        This method checks the states of some predetermined accessibles.  The
+        expected states are those states before the "Define Custom Colors"
+        button is pressed
+        '''
+
+        # Check the states of the "Color" dialog, first of all
+        statesCheck(self.color_dialog,
+                    "Dialog",
+                    add_states=["active", "modal"], 
+                    invalid_states=["resizable"])
+
+        # BUG484216 Color panel has extra focusable state
+        # statesCheck(self.colors_panel, "Panel")
+
+        # Now check the states of the "Color" dialog's children
+
+        # Check the states of the two showing labels 
+        statesCheck(self.base_colors_label, "Label")
+        statesCheck(self.user_colors_label, "Label")
+
+        # Checking the states of a few of the SmallColorControl push button
+        # accessibles should be good enough
+        statesCheck(self.small_color_buttons[0], "Button")
+        statesCheck(self.small_color_buttons[1], "Button")
+        statesCheck(self.small_color_buttons[-1], "Button")
+        statesCheck(self.small_color_buttons[-2], "Button")
+
+        # Check the states of the three buttons that should be showing
+        statesCheck(self.cancel_button, "Button")
+        # if the "Define Custom Colors" button has been pressed, it becomes
+        # insensitive.  The OK button also isn't focused anymore.
+        if is_custom_showing:
+            statesCheck(self.ok_button, "Button")
+            statesCheck(self.def_custom_colors_button,
+                        "Button",
+                        invalid_states=[FOCUSABLE, ENABLED, SENSITIVE])
+        else:
+            statesCheck(self.def_custom_colors_button, "Button")
+            statesCheck(self.ok_button, "Button", add_states=[FOCUSED])
+
+        # Now check the states of things that may or may not be showing
+        # depending on whether the "Define Custom Colors" button was
+        # clicked yet.  States may differ depending on the boolean value of
+        # is_custom_showing.
+        if is_custom_showing:
+            invalid_states = []
+        else:
+            invalid_states = [VISIBLE, SHOWING]
+
+        statesCheck(self.add_color_button,
+                    "Button",
+                    invalid_states)
+
+        statesCheck(self.custom_colors_panel,
+                    "Panel",
+                    invalid_states)
+        statesCheck(self.color_shader_panel,
+                    "Panel",
+                    invalid_states)
+        statesCheck(self.slider_panel,
+                    "Panel",
+                    invalid_states)
+        statesCheck(self.color_panel,
+                    "Panel",
+                    invalid_states)
+
+        # Checking a couple of the labels should be fine
+        statesCheck(self.color_label,
+                    "Label",
+                    invalid_states)
+        statesCheck(self.blue_label,
+                    "Label",
+                    invalid_states)
+
+        # Checking a couple of the texts should be fine
+        statesCheck(self.hue_text,
+                    "Text",
+                    invalid_states)
+        statesCheck(self.blue_text,
+                    "Text",
+                    invalid_states)
+
     def assertSmallColorText(self):
         """
         Check Text for all SmallColorControls under Base Colors and User 
         Colors, Make sure SmallColorControls show None as Text value
-
         """
-        procedurelogger.action("test the text of SmallColorControls")
+        procedurelogger.action("Check the text of SmallColorControls")
+        procedurelogger.expectedResult("The text of all SmallColorControls should be blank")
 
-        for i in range(64):
-            procedurelogger.expectedResult("the text of %s are None" % self.smallcolor_buttons[i])
-            assert self.smallcolor_buttons[i].text == "", "%s not match None" \
-                                         % self.smallcolor_buttons[i].text
-
-    def assertSmallColorName(self, accessible=None, colorname=None):
+        for small_color_button in self.small_color_buttons:
+            actual_text = small_color_button.text
+            assert actual_text == "", 'Text was "%s", expected blank ("")' % \
+                                      (actual_text)
+            
+    def selectColorAndAssertLabelChange(self,
+                                        small_color_button_index,
+                                        expected_text):
         """
-        Check Name for all SmallColorControls under Base Colors and User 
-        Colors, Make sure SmallColorControls show its color name as its Name 
-
+        Click a SmallColorControl and then click OK button.  The label text on
+        the main application frame should then match the expected_text
+        parameter
         """
-        procedurelogger.action("test the name of SmallColorControls")
+        procedurelogger.action('Select a color and then make sure the ColorDialog control frame label changes to the correct text')
 
-        procedurelogger.expectedResult("the name of SmallColorControl are not None")
-        for i in range(64):
-            names = self.smallcolor_buttons[i].name
-            assert names != "", "name is None"
+        # open the ColorDialog and find the accessibles
+        self.color_dialog_button.click(log=True)
+        sleep(config.MEDIUM_DELAY)
+        self.findAllColorDialogAccessibles()
 
-        if colorname != None:
-            procedurelogger.expectedResult("the name of SmallColorControl is %s" % colorname)
-            assert accessible.name == colorname, "%s is not match %s" % \
-                                                  (accessible.name, colorname)
-
-    def clickSmallColorToChangeLabel(self, smallcolorbutton):
-        """
-        Click SmallColorControl and click OK button to select one color, Label 
-        will shows which color to be selected
-
-        """
-        #Action
-        smallcolorbutton.click()
+        # click on the SmallColorControl and then click OK
+        self.small_color_buttons[small_color_button_index].click(log=True)
         sleep(config.SHORT_DELAY)
-        self.ok_button.click()
-        sleep(config.SHORT_DELAY)
+        print "blaalaa"
+        self.ok_button.click(log=True)
+        sleep(config.MEDIUM_DELAY)
 
         #Expected result
-        procedurelogger.expectedResult('lable shows "%s" being selected' % smallcolorbutton.name)
-
-        assert self.main_label.text == "Color [%s]" % smallcolorbutton.name, \
-                                         "%s not match %s" % (self.main_label.text, smallcolorbutton.name)
+        procedurelogger.expectedResult('Label reads "%s"' % expected_text)
+        actual_text = self.main_label.text
+        assert actual_text == expected_text, \
+                                'Label text was "%s", expected "%s"' %  \
+                                (actual_text, expected_text)
 
     # assert the size of an image in the button
-    def assertImageSize(self, button, width=-1, height=-1):
+    def assertImageSize(self, button, expected_width=-1, expected_height=-1):
         """
-        Check Image Size for buttons
-
+        Check Image Size for button
         """
-        procedurelogger.action("assert %s's image size" % button)
-        size = button.imageSize
+        procedurelogger.action("Assert %s's image size" % button)
+        actual_width, actual_height = button.imageSize
 
-        procedurelogger.expectedResult('"%s" image size is %s x %s' %
-                                                  (button, width, height))
+        procedurelogger.expectedResult('"%s" image size is %s x %s' % \
+                                     (button, expected_width, expected_height))
 
-        assert width == size[0], "%s (%s), %s (%s)" %\
+        assert actual_width == expected_width, "%s (%s), %s (%s)" %\
                                             ("expected width",
-                                              width,
+                                              expected_width,
                                              "does not match actual width",
-                                              size[0])
-        assert height == size[1], "%s (%s), %s (%s)" %\
+                                              actual_width)
+        assert actual_width == expected_width, "%s (%s), %s (%s)" %\
                                             ("expected height",
-                                              height,
+                                              expected_height,
                                              "does not match actual height",
-                                              size[1]) 
+                                              actual_height)
 
-
+    def assertComponentSize(self, accessible, expected_width, expected_height):
+        """
+        Ensure that the accessible has the size we expect
+        """
+        procedurelogger.action('Ensure that the %s has the size we expect' % \
+                               accessible)
+        procedurelogger.expectedResult('Size is %sx%s' % \
+                                             (expected_width, expected_height))
+        actual_width, actual_height = \
+                              accessible._accessible.queryComponent().getSize()
+        assert actual_width == expected_width, \
+                "Width was %s, expected %s" % (actual_width, expected_width)
+        assert actual_height == expected_height, \
+                "Height was %s, expected %s" % (actual_height, expected_height)
 
     # close application main window after running test
     def quit(self):
--- test/testers/colordialog_basic_ops.py
+++ test/testers/colordialog_basic_ops.py
@@ -47,83 +47,81 @@ cdFrame = app.colorDialogFrame
 #####################################################
 # search for all widgets from color dialog
 #####################################################
-
 # click button to show colordialog page, then check subwidgets
-cdFrame.click(cdFrame.colordialog_button)
+cdFrame.openColorDialog()
+cdFrame.findAllColorDialogAccessibles()
+
+# check the states of the default Color dialog
+cdFrame.checkColorDialogStates(False)
+
+# mouse click should raise focused state
+cdFrame.small_color_buttons[-3].mouseClick()
 sleep(config.SHORT_DELAY)
-cdFrame.AssertWidgets()
-
-#####################################################################
-# States test for dialog, SmallColorControls and Panels 
-#####################################################################
-
-statesCheck(cdFrame.colordialog, "Dialog", add_states=["active", "modal"], \
-                                           invalid_states=["resizable"])
-##color panel has extra focusable state BUG484217
-#statesCheck(cdFrame.panels[0], "Panel")
-
-#statesCheck(cdFrame.panels[1], "Panel")
-
-statesCheck(cdFrame.smallcolor_buttons[0], "Button")
-
-statesCheck(cdFrame.smallcolor_buttons[63], "Button")
-
-# mouse click may raise focused state
-cdFrame.smallcolor_buttons[53].mouseClick()
-sleep(config.SHORT_DELAY)
-statesCheck(cdFrame.smallcolor_buttons[53], "Button", add_states=["focused"])
+statesCheck(cdFrame.small_color_buttons[-3], "Button", add_states=["focused"])
 
 # keyTab move focused to the next SmallColorControl
 cdFrame.keyCombo("Tab", grabFocus=False)
 sleep(config.SHORT_DELAY)
-statesCheck(cdFrame.smallcolor_buttons[52], "Button", add_states=["focused"])
-statesCheck(cdFrame.smallcolor_buttons[53], "Button")
+statesCheck(cdFrame.small_color_buttons[-4], "Button", add_states=["focused"])
+statesCheck(cdFrame.small_color_buttons[-3], "Button")
 
-# AtkAction doesn't raise focused state
-cdFrame.click(cdFrame.smallcolor_buttons[50])
+# perform click action should raise focused state
+# BUG504593 Button does not receive focus when click action is performed on it
+#cdFrame.small_color_buttons[-5].click(log=True)
+#sleep(config.SHORT_DELAY)
+#statesCheck(cdFrame.small_color_buttons[-5], "Button", add_states=["focused"])
+#statesCheck(cdFrame.small_color_buttons[-4], "Button")
+
+# click the "Define Custom Colors" button
+cdFrame.def_custom_colors_button.click(log=True)
 sleep(config.SHORT_DELAY)
-statesCheck(cdFrame.smallcolor_buttons[50], "Button")
 
-####################################################################
-# Text test for SmallColorControls under Base Colors and User Colors
-####################################################################
+# now check the states of the Color dialog again, this time the custom
+# color accessibles should be visible and showing
+cdFrame.checkColorDialogStates(True)
 
-# SmallColorControls' Text are None
+# assert that SmallColorControls' Text are blank (i.e., "")
 cdFrame.assertSmallColorText()
-
 
 ####################################################################
 # Name test for SmallColorControls under Base Colors and User Colors
 ####################################################################
-
-##SmallColorControls are missing names for orca to announce due to BUG488998
-#cdFrame.assertSmallColorName()
-
-#cdFrame.assertSmallColorName(cdFrame.smallcolor_buttons[0], "White")
+# Depending on how BUG488998 is resolved, we may or may not want to
+# add tests to check the names of the SmallColorControl buttons
 
 ################################################
 # AtkImage test for SmallColorControls
 ################################################
+cdFrame.assertImageSize(cdFrame.small_color_buttons[10])
+cdFrame.assertImageSize(cdFrame.small_color_buttons[40])
 
-cdFrame.assertImageSize(cdFrame.smallcolor_buttons[10])
-
-cdFrame.assertImageSize(cdFrame.smallcolor_buttons[40])
+################################################
+# AtkComponent test for SmallColorControls
+################################################
+cdFrame.assertComponentSize(cdFrame.small_color_buttons[10], 25, 23)
+cdFrame.assertComponentSize(cdFrame.small_color_buttons[40], 25, 23)
 
 ###########################################################################
 # AtkAction test for SmallColorControls to select color, Label shows which 
 # color is selected
 ###########################################################################
 
-##do click action may multi select SmallColorControl BUG478541
-#cdFrame.clickSmallColorToChangeLabel(cdFrame.smallcolor_buttons[0])
+# close the Color dialog
+cdFrame.color_dialog.altF4()
 
 # click ColorDialog Button to open color dialog again
-cdFrame.click(cdFrame.colordialog_button)
+cdFrame.color_dialog_button.click(log=True)
 sleep(config.SHORT_DELAY)
-cdFrame.AssertWidgets()
+cdFrame.findAllColorDialogAccessibles()
+cdFrame.checkColorDialogStates(False)
 
 # close dialog
-cdFrame.cancel_button.click()
+cdFrame.cancel_button.click(log=True)
+sleep(config.SHORT_DELAY)
+
+# BUG504655 at-spi 'click' action cannot be used to select a color
+#cdFrame.selectColorAndAssertLabelChange(-9, "Color [Yellow]")
+#cdFrame.selectColorAndAssertLabelChange(-10, "Color [Red]")
 
 # close application frame window
 cdFrame.quit()
