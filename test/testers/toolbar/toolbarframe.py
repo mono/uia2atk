0b9fca152b26c26ab3b2904a084466eaacf8f53b
Calen Chen <cachen@mono-cvs.ximian.com>
2009-06-10 10:22:26 +0000

    commit Brian's reviews
    svn path=/trunk/uia2atk/; revision=135829

--- test/testers/toolbar/toolbarframe.py
+++ test/testers/toolbar/toolbarframe.py
@@ -1,4 +1,3 @@
-
 ##############################################################################
 # Written by:  Cachen Chen <cachen@novell.com>
 # Date:        02/04/2009
@@ -14,7 +13,6 @@ import states
 from strongwind import *
 from toolbar import *
 from helpers import *
-
 
 # class to represent the main window.
 class ToolBarFrame(accessibles.Frame):
@@ -32,73 +30,76 @@ class ToolBarFrame(accessibles.Frame):
 
     def assertChildrenNumber(self, children_type, expected_num=None):
         """
-        search for toolbar's children by find children_type, ensure the number 
-	of that kind of children_type are expected
+        search for toolbar's children by find children_type, ensure the number
+        of that kind of children_type are expected
         """
-        procedurelogger.action("how many %s items on toolbar" % children_type)
-
+        procedurelogger.action("check how many %s items are on the toolbar" % \
+                                                                 children_type)
+        procedurelogger.expectedResult("%s %s on toolbar" % \
+                                                 (expected_num, children_type))
+    
         if children_type == "push button":
             self.pushbuttons = self.toolbar.findAllPushButtons(None)
             expected_num = self.PUSHBUTTON_NUM
             assert len(self.pushbuttons) == expected_num, \
-                                        "actual %s, expected %s push button" % \
-                                          (len(self.pushbuttons), expected_num)
+                                       "actual %s, expected %s push button" % \
+                                       (len(self.pushbuttons), expected_num)
         elif children_type == "label":
             self.labels = self.toolbar.findAllLabels(None)
             expected_num = self.LABEL_NUM
             assert len(self.labels) == expected_num,  \
-                                              "actual %s, expected %s label" % \
-                                               (len(self.labels), expected_num)
+                                             "actual %s, expected %s label" % \
+                                              (len(self.labels), expected_num)
         elif children_type == "separator":
             self.separators = self.toolbar.findAllSeparators(None)
             expected_num = self.SEPARATOR_NUM
             assert len(self.separators) == expected_num, \
-                                          "actual %s, expected %s separator" % \
-                                            (len(self.separators), expected_num)
+                                         "actual %s, expected %s separator" % \
+                                         (len(self.separators), expected_num)
         elif children_type == "combo box":
-            self.comboboxs = self.toolbar.findAllComboBoxs(None)
+            self.comboboxes = self.toolbar.findAllComboBoxes(None)
             expected_num = self.COMBOBOX_NUM
-            assert len(self.comboboxs) == expected_num, \
-                                           "actual %s, expected %s combobox" % \
-                                             (len(self.comboboxs), expected_num)
+            assert len(self.comboboxes) == expected_num, \
+                                          "actual %s, expected %s combobox" % \
+                                          (len(self.comboboxes), expected_num)
         elif children_type == "menu":
             self.menus = self.toolbar.findAllMenus(None, checkShowing=False)
             expected_num = self.MENU_NUM
             assert len(self.menus) == expected_num, \
-                 "actual %s, expected %s menu" % (len(self.menus), expected_num)
+                                            "actual %s, expected %s menu" % \
+                                            (len(self.menus), expected_num)
         elif children_type == "menu item":
-            self.menuitems = self.toolbar.findAllMenuItems(None, checkShowing=False)
+            self.menuitems = \
+                        self.toolbar.findAllMenuItems(None, checkShowing=False)
             expected_num = self.MENUITEM_NUM
             assert len(self.menuitems) == expected_num, \
-                                               "actual %s, expected %s menu" % \
-                                             (len(self.menuitems), expected_num)
+                                            "actual %s, expected %s menu" % \
+                                            (len(self.menuitems), expected_num)
         else:
-            print "no %s in this sample" % children_type
+            raise ValueError, "no %s in this sample" % children_type
 
-        procedurelogger.expectedResult("%s %s on toolbar" % \
-                                                  (expected_num, children_type))
 
     def checkComboBox(self):
 
         # combobox Actions
-        actionsCheck(self.comboboxs[0], "ComboBox")
+        actionsCheck(self.comboboxes[0], "ComboBox")
 
         # combobox States, should default focus on combobox
-        statesCheck(self.comboboxs[0], "ComboBox", \
+        statesCheck(self.comboboxes[0], "ComboBox", \
                                             add_states=["focused"])
 
         # doing click action to drop down menu 
-        self.comboboxs[0].click(log=True)
+        self.comboboxes[0].click(log=True)
         sleep(config.SHORT_DELAY)
         procedurelogger.expectedResult("drop_down menu is showing up")
         assert self.menus[0].showing, "combobox drop_down menu doesn't showing"
 
         # doing click action again to close menu
-        self.comboboxs[0].click(log=True)
+        self.comboboxes[0].click(log=True)
         sleep(config.SHORT_DELAY)
         procedurelogger.expectedResult("drop_down menu is closed")
-        assert not self.menus[0].showing, "combobox drop_down menu still showing"      
-
+        assert not self.menus[0].showing, \
+                                        "combobox drop_down menu still showing"      
     def checkComboBoxMenuItem(self):
 
         # combobox Actions and States
