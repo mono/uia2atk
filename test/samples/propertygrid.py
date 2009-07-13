cc68a0698f3a3c4b117a1d61cac830d327a028d0
Calen Chen <cachen@mono-cvs.ximian.com>
2009-07-13 06:34:42 +0000

    update propertygrid sample
    svn path=/trunk/uia2atk/; revision=137764

--- test/samples/propertygrid.py
+++ test/samples/propertygrid.py
@@ -10,32 +10,37 @@
 # The docstring below is used in the generated log file
 """
 This sample will show "PropertyGrid" control in the form.
-It can be used for Autotest tools(e.g. Strongwind) to test the behaviors of controls.
+It can be used for Autotest tools(e.g. Strongwind) to test the behaviors of 
+controls.
 """
 
 # imports
 import clr
 clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+
 from System.Windows.Forms import (
-    Application, Form, Label, PropertyGrid, DockStyle
+    Application, Form, Label, PropertyGrid, DockStyle, TextBox
 )
+from System.Drawing import * 
 
 class PropertyGridSample(Form):
     """PropertyGrid control class"""
 
     def __init__(self):
         """PropertyGridSample class init function."""
+        self.count = 0
 
         # setup title
         self.Text = "PropertyGrid control"
         self.Height = 500
 
-        # setup labels
-        self.label = Label()
-        #self.label.AutoSize = True
-        self.label.Height = 50
-        self.label.Dock = DockStyle.Top
-        self.label.Text = "The Property Grid of Label control"
+        # setup text
+        self.text = TextBox()
+        #self.text.AutoSize = True
+        self.text.Height = 100
+        self.text.Dock = DockStyle.Top
+        self.text.Text = "The Property Grid of text control"
 
         # setup propertygrid
         self.propertygrid = PropertyGrid()
@@ -43,11 +48,16 @@ class PropertyGridSample(Form):
         self.propertygrid.Dock = DockStyle.Top
         self.propertygrid.TabIndex = 1
         self.propertygrid.Text = "Property Grid"
-        self.propertygrid.SelectedObject = self.label
+        self.propertygrid.SelectedObject = self.text
+        self.propertygrid.PropertyValueChanged += self.color_change
 
         # add controls
         self.Controls.Add(self.propertygrid)
-        self.Controls.Add(self.label)
+        self.Controls.Add(self.text)
+
+    def color_change(self, control, event):
+        self.text.Text = "The color of label is: %s %s %s" % \
+               (self.text.BackColor.R,self.text.BackColor.G,self.text.BackColor.B)
 
 
 # run application
