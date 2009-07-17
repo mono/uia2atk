0dfb27a5a0b4d61f74d4b2a5e6973ac15519a6ab
Marco Skambraks <mskambraks@mono-cvs.ximian.com>
2009-07-17 09:40:53 +0000

    - modified listbox test - check for treeview
    svn path=/trunk/uia2atk/; revision=138097

--- test/keystrokes/winforms/listbox_single_column.py/listbox_single_column.py
+++ test/keystrokes/winforms/listbox_single_column.py/listbox_single_column.py
@@ -7,37 +7,33 @@ sequence = MacroSequence()
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(WaitForWindowActivate("ListBox control",None))
-sequence.append(WaitForFocus("0", acc_role=pyatspi.ROLE_LIST_ITEM))
+#sequence.append(WaitForFocus("0", acc_role=pyatspi.ROLE_TABLE_CELL))
 sequence.append(utils.AssertPresentationAction(
     "list focus",
     ["BRAILLE LINE:  'ListBox control Frame'",
      "     VISIBLE:  'ListBox control Frame', cursor=1",
-     "BRAILLE LINE:  'List'",
-      "     VISIBLE:  'List', cursor=1",
-      "BRAILLE LINE:  '0 ListItem'",
-      "     VISIBLE:  '0 ListItem', cursor=1",
+      "BRAILLE LINE:  '0'",
+      "     VISIBLE:  '0', cursor=1",
       "SPEECH OUTPUT: 'ListBox control frame'",
-      "SPEECH OUTPUT: ''",
-      "SPEECH OUTPUT: 'list'",
       "SPEECH OUTPUT: ''",
       "SPEECH OUTPUT: '0'"]))
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("End"))
-sequence.append(WaitForFocus("19", acc_role=pyatspi.ROLE_LIST_ITEM))
+#sequence.append(WaitForFocus("19", acc_role=pyatspi.ROLE_TABLE_CELL))
 sequence.append(utils.AssertPresentationAction(
     "jump to last entry",
-    ["BRAILLE LINE:  '19 ListItem'",
-     "     VISIBLE:  '19 ListItem', cursor=1",
+    ["BRAILLE LINE:  '19'",
+     "     VISIBLE:  '19', cursor=1",
      "SPEECH OUTPUT: ''",
      "SPEECH OUTPUT: '19'"]))
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("Home"))
-sequence.append(WaitForFocus("0", acc_role=pyatspi.ROLE_LIST_ITEM))
+#sequence.append(WaitForFocus("0", acc_role=pyatspi.ROLE_TABLE_CELL))
 sequence.append(utils.AssertPresentationAction(
     "jump back to first entry",
-    ["BRAILLE LINE:  '0 ListItem'",
-     "     VISIBLE:  '0 ListItem', cursor=1",
+    ["BRAILLE LINE:  '0'",
+     "     VISIBLE:  '0', cursor=1",
      "SPEECH OUTPUT: ''",
      "SPEECH OUTPUT: '0'"]))
 sequence.append(utils.StartRecordingAction())
@@ -50,13 +46,13 @@ sequence.append(utils.AssertPresentationAction(
 
 sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("5"))
-sequence.append(WaitForFocus("5", acc_role=pyatspi.ROLE_LIST_ITEM))
+#sequence.append(WaitForFocus("5", acc_role=pyatspi.ROLE_TABLE_CELL))
 sequence.append(utils.AssertPresentationAction(
     "jump to item 5",
-    ["BRAILLE LINE:  '5 ListItem'",
-     "     VISIBLE:  '5 ListItem', cursor=1",
-     "BRAILLE LINE:  '5 ListItem'",
-     "     VISIBLE:  '5 ListItem', cursor=1",
+    ["BRAILLE LINE:  '5'",
+     "     VISIBLE:  '5', cursor=1",
+     "BRAILLE LINE:  '5'",
+     "     VISIBLE:  '5', cursor=1",
      "SPEECH OUTPUT: ''",
      "SPEECH OUTPUT: '5'"]))
 
@@ -64,9 +60,8 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("KP_Enter"))
 sequence.append(utils.AssertPresentationAction(
     "ask: which item and how many",
-    ["BRAILLE LINE:  '5 ListItem'",
-     "     VISIBLE:  '5 ListItem', cursor=1",
-     "SPEECH OUTPUT: 'list item'",
+    ["BRAILLE LINE:  '5'",
+     "     VISIBLE:  '5', cursor=1",
      "SPEECH OUTPUT: '5'",
      "SPEECH OUTPUT: 'item 6 of 20'"]))
 
