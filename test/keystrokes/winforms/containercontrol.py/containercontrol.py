46c2f97c3f3efad6c7c2e696f3e7db5ad9fefe1e
Marco Skambraks <mskambraks@mono-cvs.ximian.com>
2009-08-14 11:47:12 +0000

    - hopefully completed containercontrol.py
    svn path=/trunk/uia2atk/; revision=139906

--- test/keystrokes/winforms/containercontrol.py/containercontrol.py
+++ test/keystrokes/winforms/containercontrol.py/containercontrol.py
@@ -17,6 +17,47 @@ sequence.append(utils.AssertPresentationAction(
      "SPEECH OUTPUT: ''",
      "SPEECH OUTPUT: 'panel'"]))
 
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_8"))
+sequence.append(utils.AssertPresentationAction(
+    "switch to flat-review",
+    ["BRAILLE LINE:  'Press Tab, please'",
+     "     VISIBLE:  'Press Tab, please', cursor=1",
+     "SPEECH OUTPUT: 'Press Tab, please'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Tab"))
+sequence.append(utils.AssertPresentationAction(
+    "jump to next control",
+    ["BRAILLE LINE:  'Panel'",
+     "     VISIBLE:  'Panel', cursor=1",
+     "SPEECH OUTPUT: ''",
+     "SPEECH OUTPUT: 'panel'"]))
+    
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_8"))
+sequence.append(utils.AssertPresentationAction(
+    "switch to flat-review",
+    ["BRAILLE LINE:  'I lose focus'",
+     "     VISIBLE:  'I lose focus', cursor=1",
+     "SPEECH OUTPUT: 'I lose focus'"]))
+
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("Tab"))
+sequence.append(utils.AssertPresentationAction(
+    "jump to next control",
+    ["BRAILLE LINE:  'Panel'",
+     "     VISIBLE:  'Panel', cursor=1",
+     "SPEECH OUTPUT: ''",
+     "SPEECH OUTPUT: 'panel'"]))
+sequence.append(utils.StartRecordingAction())
+sequence.append(KeyComboAction("KP_8"))
+sequence.append(utils.AssertPresentationAction(
+    "switch to flat-review",
+    ["BRAILLE LINE:  'I got it'",
+     "     VISIBLE:  'I got it', cursor=1",
+     "SPEECH OUTPUT: 'I got it'"]))
+    
 sequence.append(utils.AssertionSummaryAction())
 
 sequence.start()
