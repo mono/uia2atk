e7a7a0da3efdfb0e7a0d20cf95a300d4e6bd8dec
Neville Gao <ngao@mono-cvs.ximian.com>
2009-08-28 10:32:22 +0000

    Added SL2 Button sample.
    svn path=/trunk/uia2atk/; revision=140870

---
+++ test/samples/moonlight/button/ButtonSample/ButtonSample/Page.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ButtonSample
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private int button2ClickedNum = 0;
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Successfully clicked Button1!");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            int count = ++button2ClickedNum;
+            string s = count == 1 ? "" : "s";
+            textBlock.Text = string.Format("Button2 is clicked {0} time{1}.", count.ToString(), s);
+        }
+    }
+}
