378ebf2ab74bc838fa7c7e090b94fa0698dde2b5
Calen Chen <cachen@mono-cvs.ximian.com>
2009-09-01 10:01:42 +0000

    add SL2 MediaElement sample
    svn path=/trunk/uia2atk/; revision=141021

---
+++ test/samples/moonlight/mediaelement/MediaElementSample/MediaElementSample/Page.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MediaElementSample
+{
+    public partial class Page : UserControl
+    {
+        private DispatcherTimer timer;
+        public Page()
+        {
+            InitializeComponent();
+            btnPlayPause.Checked += new RoutedEventHandler(btnPlayPause_Checked);
+            btnPlayPause.Unchecked += new RoutedEventHandler(btnPlayPause_Unchecked);
+
+            VideoElement.CurrentStateChanged += new RoutedEventHandler(VideoElement_CurrentStateChanged);
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (VideoElement.NaturalDuration.TimeSpan.TotalSeconds > 0)
+            {
+                txtVideoPosition.Text =
+                    string.Format("{0:00}:{1:00}", VideoElement.Position.Minutes, VideoElement.Position.Seconds);
+
+                sliderScrubber.Value = VideoElement.Position.TotalSeconds /
+                    VideoElement.NaturalDuration.TimeSpan.TotalSeconds;
+            }
+        }
+
+        void VideoElement_CurrentStateChanged(object sender, RoutedEventArgs e)
+        {
+            if (VideoElement.CurrentState == MediaElementState.Playing)
+            {
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+
+        void btnPlayPause_Unchecked(object sender, RoutedEventArgs e)
+        {
+            VideoElement.Play();
+            btnPlayPause.Content = "Pause";
+        }
+
+        private void btnPlayPause_Checked(object sender, RoutedEventArgs e)
+        {
+            VideoElement.Pause();
+            btnPlayPause.Content = "Play";
+        }
+    }
+}
+
---
+++ test/samples/moonlight/mediaelement/MediaElementSample/MediaElementSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MediaElementSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaElementSample")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d342766-5bd1-4488-9955-3e26ee9a58e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
