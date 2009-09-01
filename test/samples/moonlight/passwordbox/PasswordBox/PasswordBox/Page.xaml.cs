3d7e35793bb8e239b97fdeabb6da4cadbf7e0319
Neville Gao <ngao@mono-cvs.ximian.com>
2009-09-01 10:08:36 +0000

    Added SL2 samples.
    svn path=/trunk/uia2atk/; revision=141023

---
+++ test/samples/moonlight/passwordbox/PasswordBox/PasswordBox/Page.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PasswordBox
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private int pwdChanges = 0;
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            int count = ++pwdChanges;
+            string s = count == 1 ? "" : "s";
+            textBlock.Text = string.Format("You changed {0} time{1}.", Convert.ToString(count), s);
+        }
+    }
+}
