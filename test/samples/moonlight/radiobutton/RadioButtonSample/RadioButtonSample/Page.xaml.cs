a3af2a610c026c53c58818e497a6c29eee2108a4
Calen Chen <cachen@mono-cvs.ximian.com>
2009-09-02 09:22:02 +0000

    add SL2 radiobutton sample
    svn path=/trunk/uia2atk/; revision=141105

---
+++ test/samples/moonlight/radiobutton/RadioButtonSample/RadioButtonSample/Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace RadioButtonSample
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+        void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb.Name == "radiobutton4")
+                choiceTextBlock.Text = "chose: " + rb.GroupName + ": " + rb.Name;
+            else
+                choiceTextBlock.Text = "chose: " + rb.GroupName + ": " + rb.Content;
+        }
+    }
+}
