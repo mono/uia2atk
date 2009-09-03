4b6e95c351ab652cd9f100b156f4abfcf85b6027
Neville Gao <ngao@mono-cvs.ximian.com>
2009-09-03 10:29:26 +0000

    Added SL2 samples.
    svn path=/trunk/uia2atk/; revision=141225

---
+++ test/samples/moonlight/progressbar/ProgressBar/ProgressBar/Page.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ProgressBar
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (progressBar.Value < progressBar.Maximum)
+                progressBar.Value += 20;
+            else
+                progressBar.Value = 0;
+
+            textBlock.Text = string.Format("It is {0} out of 100%.", progressBar.Value);
+        }
+    }
+}
---
+++ test/samples/moonlight/scrollbar/ScrollBar/ScrollBar/Page.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ScrollBar
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void hscrollBar_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+            textBlock1.Text = string.Format("Value of Horizontal: {0}", hscrollBar.Value);
+        }
+
+        private void vscrollBar_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+            textBlock2.Text = string.Format("Value of Vertical: {0}", vscrollBar.Value);
+        }
+    }
+}
