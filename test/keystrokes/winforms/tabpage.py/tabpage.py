5341194c1d96d8928eaa2ce53ee3d6a5745e4744
Marco Skambraks <mskambraks@mono-cvs.ximian.com>
2009-09-03 11:22:04 +0000

    - tabpage.py update
    svn path=/trunk/uia2atk/; revision=141233

--- test/keystrokes/winforms/tabpage.py/tabpage.py
+++ test/keystrokes/winforms/tabpage.py/tabpage.py
@@ -5,13 +5,14 @@ import utils
 
 sequence = MacroSequence()
 
-sequence.append(WaitForWindowActivate("TabControl & TabPage control",None))
+
 sequence.append(utils.StartRecordingAction())
+sequence.append(WaitForWindowActivate("TabControl & TabPage control",None))
 sequence.append(WaitForFocus("Tab 0", acc_role=pyatspi.ROLE_PAGE_TAB))
 sequence.append(utils.AssertPresentationAction(
     "focus on tab0",
     ["BRAILLE LINE:  'TabControl & TabPage control Frame'",
-     "     VISIBLE:  'TabControl & TabPage control Frame', cursor=1",
+     "     VISIBLE:  'TabControl & TabPage control Fra', cursor=1",
      "BRAILLE LINE:  'Tab 0",
      "     VISIBLE:  'Tab 0', cursor=1",
      "SPEECH OUTPUT: 'TabControl & TabPage control frame'",
@@ -30,8 +31,8 @@ sequence.append(utils.StartRecordingAction())
 sequence.append(KeyComboAction("KP_Subtract"))
 sequence.append(utils.AssertPresentationAction(
     "flat-review tablist",
-    ["BRAILLE LINE:  'Tab 0 Tab 1 Tab 2 Tab 3 Tab 6'",
-    "     VISIBLE:  'Tab 0 Tab 1 Tab 2 Tab 3 Tab 4 Tab 5 Tab 6', cursor=1",
+    ["BRAILLE LINE:  'Tab 0 Tab 1 Tab 2 Tab 3 Tab 4 Tab 5 Tab 6'",
+    "     VISIBLE:  'Tab 0 Tab 1 Tab 2 Tab 3 Tab 4 Ta', cursor=1",
     "SPEECH OUTPUT: 'Tab'"]))
 
 
