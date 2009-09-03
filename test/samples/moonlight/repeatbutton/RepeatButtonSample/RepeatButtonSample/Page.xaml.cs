03f82d5b65e116dc37570c81dacf2f7609b8d2cd
Calen Chen <cachen@mono-cvs.ximian.com>
2009-09-03 09:12:30 +0000

    add SL2 RepeatButton sample
    svn path=/trunk/uia2atk/; revision=141215

---
+++ test/samples/moonlight/repeatbutton/RepeatButtonSample/RepeatButtonSample/Page.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace RepeatButtonSample
+{
+    public partial class Page : UserControl
+    {
+        static int click_time = 0;
+        public Page()
+        {
+            InitializeComponent();
+        }
+        void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            //this.label.Text = DateTime.Now.ToString(System.Globalization.CultureInfo.InvariantCulture);
+            click_time += 1;
+            this.label.Text = "Number of Clicks: " + click_time;
+        }
+    }
+}
