d91ce941e7d8f5c30b25eae8a454fb619afcfce9
Andrés G. Aragoneses <knocte@gmail.com>
2009-09-09 16:42:37 +0000

    * MoonAtkBridge/DummyEntryPoint.cs: Reference IsAccessibilityEnabled to be inspected by the tuner.
    * MoonAtkBridge/Makefile.am: Make the 'descriptors' target work with
    latest tuner.
    
    svn path=/trunk/uia2atk/; revision=141624

--- MoonAtkBridge/DummyEntryPoint.cs
+++ MoonAtkBridge/DummyEntryPoint.cs
@@ -5,13 +5,16 @@ namespace Application
 {
 	class DummyEntryPoint
 	{
-		static void Main ()
+		static int Main ()
 		{
 			//be sure we're referencing moon's corlib
 			new System.Security.SecurityCriticalAttribute ();
 
 			//be sure we're referencing MoonAtkBridge
 			Moonlight.AtkBridge.AutomationBridge.CreateAutomationBridge ().GetAccessibleHandle ();
+
+			//be sure we're referencing private MoonAtkBridge's API used by moonlight
+			return Moonlight.AtkBridge.AutomationBridge.IsAccessibilityEnabled () ? 0 : 1;
 		}
 	}
 }
