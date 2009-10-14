32b309f7abdd94e075d0bcfb57832e51a3185427
Neville Gao <ngao@mono-cvs.ximian.com>
2009-10-14 14:25:18 +0000

    Added accidentally deleted files.
    svn path=/trunk/uia2atk/; revision=144103

---
+++ test/samples/moonlight/ListBox/Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ListBoxSample
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+
+            textBlock.Text = "You selected no item.";
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem listBoxItem = ((sender as ListBox).SelectedItem as ListBoxItem);
+            textBlock.Text = "You selected " + listBoxItem.Content.ToString() + ".";
+        }
+    }
+}
