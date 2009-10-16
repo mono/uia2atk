01be3e0800e859f00aa905b682e9e9bbef751697
Calen Chen <cachen@mono-cvs.ximian.com>
2009-10-16 07:41:08 +0000

    move imagebin to the uper dir, add 2 cs for mouse event
    svn path=/trunk/uia2atk/; revision=144255

---
+++ test/samples/moonlight/MultiScaleImage/Mouse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MultiScaleImageSample
+{
+    public class Mouse
+    {
+        public Point DownPosition
+        {
+            get;
+            set;
+        }
+        public Point UpPosition
+        {
+            get;
+            set;
+        }
+        public Point MovePosition
+        {
+            get;
+            set;
+        }
+        public Point UpInterval
+        {
+            get
+            {
+                return new Point((DownPosition.X - UpPosition.X) / 1000, (DownPosition.Y - UpPosition.Y) / 1000);
+            }
+        }
+    }
+}
