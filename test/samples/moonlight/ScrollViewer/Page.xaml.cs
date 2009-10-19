49dc0ab563b7854170a780967f9db00a131ece08
Calen Chen <cachen@mono-cvs.ximian.com>
2009-10-19 08:42:59 +0000

    add buttons and events
    svn path=/trunk/uia2atk/; revision=144355

--- test/samples/moonlight/ScrollViewer/Page.xaml.cs
+++ test/samples/moonlight/ScrollViewer/Page.xaml.cs
@@ -18,5 +18,48 @@ namespace ScrollViewerSample
         {
             InitializeComponent();
         }
+        void Button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Name == "Button1")
+            {
+                if (ScrollViewer1.VerticalScrollBarVisibility == ScrollBarVisibility.Auto)
+                {
+                    ScrollViewer1.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+                    button.Content = "Auto Vertical";
+                }
+                else
+                {
+                    ScrollViewer1.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+                    button.Content = "Hidden Vertical";
+                }
+            }
+            else if (button.Name == "Button2")
+            {
+                if (ScrollViewer1.HorizontalScrollBarVisibility == ScrollBarVisibility.Auto)
+                {
+                    ScrollViewer1.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
+                    button.Content = "Auto Horizontal";
+                }
+                else
+                {
+                    ScrollViewer1.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                    button.Content = "Hidden Vertical";
+                }
+            }
+            else
+            {
+                if (ScrollViewer1.Width == 300)
+                {
+                    ScrollViewer1.Width = 500;
+                    ScrollViewer1.Height = 500;
+                }
+                else
+                {
+                    ScrollViewer1.Width = 300;
+                    ScrollViewer1.Height = 300;
+                }
+            }
+        }
     }
 }
