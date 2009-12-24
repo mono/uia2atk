8875c16601bbecae470ddc615b60aee07c84efc0
Felicia Mu <fxmu@mono-cvs.ximian.com>
2009-12-24 11:20:36 +0000

    Rename the sample name
    svn path=/trunk/uia2atk/; revision=148887

---
+++ test/samples/uiaclient/DockProvider.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+
+namespace DockProviderTest
+{
+	public class DockProviderForm : Form
+	{
+		public DockProviderForm ()
+		{
+			Controls.Add (new DockProviderLabel ());
+		}
+
+		[STAThread]
+		static void Main ()
+		{
+			Application.EnableVisualStyles ();
+			Application.SetCompatibleTextRenderingDefault (false);
+			Application.Run (new DockProviderForm ());
+		}
+	}
+
+	public class DockProviderLabel : Label, IRawElementProviderSimple, IDockProvider
+	{
+		public DockProviderLabel ()
+			: base ()
+		{
+			Dock = DockStyle.Top;
+			Text = "Top";
+			BackColor = Color.Black;
+			ForeColor = Color.White;
+		}
+
+		protected override void WndProc (ref Message m)
+		{
+			if (m.Msg == WM_GETOBJECT
+                            && (int)m.LParam == AutomationInteropProvider.RootObjectId) {
+				m.Result = AutomationInteropProvider.ReturnRawElementProvider (
+					this.Handle, m.WParam, m.LParam,
+					(IRawElementProviderSimple) this);
+				return;
+			}
+
+			base.WndProc (ref m);
+		}
+
+#region IRawElementProviderSimple Members
+		public object GetPatternProvider (int patternId)
+		{
+			if (patternId == DockPattern.Pattern.Id)
+				return this;
+
+			return null;
+		}
+
+		public object GetPropertyValue (int propertyId)
+		{
+			if (propertyId == DockPattern.DockPositionProperty.Id)
+				return DockPosition;
+			else if (propertyId == AutomationElementIdentifiers.NameProperty.Id)
+				return DockPosition.ToString ();
+
+			return null;
+		}
+
+		public IRawElementProviderSimple HostRawElementProvider
+		{
+			get { return this; }
+		}
+
+		public ProviderOptions ProviderOptions
+		{
+			get { return ProviderOptions.ServerSideProvider; }
+		}
+#endregion
+
+#region IDockProvider Members
+		public DockPosition DockPosition {
+			get;
+			private set;
+		}
+
+		public void SetDockPosition (DockPosition dockPosition)
+		{
+			DockPosition oldValue = DockPosition;
+			DockPosition = dockPosition;
+
+			this.Invoke ((MethodInvoker) delegate () {
+				switch (dockPosition) {
+				case DockPosition.Top:
+					Dock = DockStyle.Top;
+					break;
+				case DockPosition.Bottom:
+					Dock = DockStyle.Bottom;
+					break;
+				case DockPosition.Left:
+					Dock = DockStyle.Left;
+					break;
+				case DockPosition.Right:
+					Dock = DockStyle.Right;
+					break;
+				case DockPosition.Fill:
+					Dock = DockStyle.Fill;
+					break;
+				default:
+					Dock = DockStyle.None;
+					break;
+				}
+				Text = Dock.ToString ();
+			});
+
+			if (AutomationInteropProvider.ClientsAreListening) {
+				AutomationInteropProvider.RaiseAutomationPropertyChangedEvent(this,
+					new AutomationPropertyChangedEventArgs (DockPattern.DockPositionProperty,
+					                                        oldValue, DockPosition));
+				AutomationInteropProvider.RaiseAutomationPropertyChangedEvent (this,
+					new AutomationPropertyChangedEventArgs (AutomationElementIdentifiers.NameProperty,
+										oldValue.ToString (), DockPosition.ToString ()));
+			}
+		}
+#endregion
+
+#region Private Fields
+		private const int WM_GETOBJECT = 0x003D;
+#endregion
+	}
+}
---
+++ test/samples/uiaclient/TransformProvider.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Automation;
+using System.Windows.Automation.Provider;
+
+namespace TransformProviderTest
+{
+    public class TransformProviderForm : Form
+    {
+        public TransformProviderForm()
+        {
+            var t = new TransformProviderControl();
+            t.Name = "TransformProviderControl1";
+            t.Width = 100;
+            t.Height = 100;
+            t.Top = 25;
+            t.Left = 25;
+            Controls.Add(t);
+            Text = "Transform Test";
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TransformProviderForm());
+        }
+    }
+
+    public class TransformProviderControl : Control, IRawElementProviderSimple, ITransformProvider
+    {
+        public TransformProviderControl()
+            : base()
+        {
+            BackColor = Color.Black;
+            RotateDegree = 0.0;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_GETOBJECT
+                            && (int)m.LParam == AutomationInteropProvider.RootObjectId)
+            {
+                m.Result = AutomationInteropProvider.ReturnRawElementProvider(
+                    this.Handle, m.WParam, m.LParam,
+                    (IRawElementProviderSimple)this);
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            int centerX = Width / 2;
+            int centerY = Height / 2;
+            const int CenterRadius = 4;
+            e.Graphics.FillEllipse(Brushes.Red, centerX - CenterRadius, centerY - CenterRadius,
+                CenterRadius * 2, CenterRadius * 2);
+            e.Graphics.DrawLine(new Pen(Color.Red), centerX, centerY,
+                centerX + (float)(Math.Cos(RotateDegree / 180.0 * Math.PI) * centerX),
+                centerY - (float)(Math.Sin(RotateDegree / 180.0 * Math.PI) * centerY));
+        }
+
+        public double RotateDegree { get; set; }
+
+        #region IRawElementProviderSimple Members
+        public object GetPatternProvider(int patternId)
+        {
+            if (patternId == TransformPattern.Pattern.Id)
+                return this;
+
+            return null;
+        }
+
+        public object GetPropertyValue(int propertyId)
+        {
+            if (propertyId == TransformPattern.CanMoveProperty.Id)
+                return CanMove;
+            else if (propertyId == TransformPattern.CanResizeProperty.Id)
+                return CanResize;
+            else if (propertyId == TransformPattern.CanRotateProperty.Id)
+                return CanRotate;
+            else if (propertyId == AutomationElementIdentifiers.NameProperty.Id)
+                return string.Format ("{0}, r:{1}", Name, RotateDegree);
+
+            return null;
+        }
+
+        public IRawElementProviderSimple HostRawElementProvider
+        {
+            get { return this; }
+        }
+
+        public ProviderOptions ProviderOptions
+        {
+            get { return ProviderOptions.ServerSideProvider; }
+        }
+        #endregion
+
+        #region ITransformProvider Members
+
+        public bool CanMove
+        {
+            get { return true; }
+        }
+
+        public bool CanResize
+        {
+            get { return true; }
+        }
+
+        public bool CanRotate
+        {
+            get { return true; }
+        }
+
+        public new void Move(double x, double y)
+        {
+            this.Invoke((MethodInvoker)delegate()
+            {
+                this.Left = (int)x;
+                this.Top = (int)y;
+                this.Invalidate();
+            });
+        }
+
+        public new void Resize(double width, double height)
+        {
+            this.Invoke((MethodInvoker)delegate()
+            {
+                this.Width = (int)width;
+                this.Height = (int)height;
+                this.Invalidate();
+            });
+        }
+
+        public void Rotate(double degrees)
+        {
+            this.RotateDegree = degrees;
+            this.Invoke((MethodInvoker)delegate()
+            {
+                this.Invalidate();
+            });
+        }
+
+        #endregion
+
+        #region Private Fields
+        private const int WM_GETOBJECT = 0x003D;
+        #endregion
+    }
+}
