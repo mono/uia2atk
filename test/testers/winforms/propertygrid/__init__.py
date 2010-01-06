68e68734aa2a07e6b6d0527baf44db5ac28ea6e7
Neville Gao <ngao@mono-cvs.ximian.com>
2010-01-06 09:29:11 +0000

    Fixed an indentation issue.
    svn path=/trunk/uia2atk/; revision=149115

--- test/testers/winforms/propertygrid/__init__.py
+++ test/testers/winforms/propertygrid/__init__.py
@@ -20,12 +20,12 @@ def launchPropertyGrid(exe=None):
 		# make sure we can find the sample application
 		harness_dir = path[0]
 		i = harness_dir.rfind("/")
-        j = harness_dir[:i].rfind("/")
+		j = harness_dir[:i].rfind("/")
 		uiaqa_path = harness_dir[:j]
 		exe = '%s/samples/winforms/propertygrid.py' % uiaqa_path
 		if not exists(exe):
 			raise IOError, "Could not find file %s" % exe
-  
+
 	args = [exe]
 
 	(app, subproc) = cache.launchApplication(args=args, name='ipy', wait=config.LONG_DELAY)
@@ -44,6 +44,5 @@ class PropertyGrid(accessibles.Application):
 	def __init__(self, accessible, subproc=None): 
 		'Get a reference to the PropertyGrid window'
 		super(PropertyGrid, self).__init__(accessible, subproc)
-        
-		self.findFrame(re.compile('^PropertyGrid control'), logName='Property Grid')
 
+		self.findFrame(re.compile('^PropertyGrid control'), logName='Property Grid')
