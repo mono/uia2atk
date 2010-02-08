1604a77922458978b725292db5c9aa966ed99f9e
Brad Taylor <brad@getcoded.net>
2010-02-08 18:39:14 +0000

    2010-02-08  Brad Taylor  <brad@getcoded.net>
    In MoonAtkBridge/:
    	* configure.ac: Add Slider test.
    
    In MoonAtkBridge/tests/:
    	* test_slider.py:
    	* assets/Makefile.am:
    	* assets/SliderTest/App.xaml:
    	* assets/SliderTest/Page.xaml:
    	* assets/SliderTest/App.xaml.cs:
    	* assets/SliderTest/Makefile.am:
    	* assets/SliderTest/Page.xaml.cs: Add new test for Slider.
    
    svn path=/trunk/uia2atk/; revision=151050

---
+++ MoonAtkBridge/tests/test_slider.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Copyright (c) 2009 Novell, Inc. (http://www.novell.com)
+#
+# Authors:
+#      Brad Taylor <brad@getcoded.net>
+#
+
+from moonlight import *
+from strongwind import *
+from strongwind.events import EventListener
+
+import pyatspi
+from time import sleep
+from os.path import abspath
+
+class Slider(TestCase):
+    """
+    Exercises the Slider control
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.app = launchAddress(abspath('assets/SliderTest/SliderTest.html'))
+        cls.label = cls.app.slControl.findLabel('0')
+
+        cls.slider = cls.app.slControl.findSlider('')
+        cls.slider_value = cls.slider._accessible.queryValue()
+
+        cls.large_decrease = cls.slider.getChildAtIndex(0)
+        cls.thumb = cls.slider.getChildAtIndex(1)
+        cls.large_increase = cls.slider.getChildAtIndex(2)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.app.kill()
+
+    def test_slider_basic(self):
+        self.assertEqual('', self.slider.name)
+
+        self.assertInterfaces(self.slider, [
+            'accessible', 'component', 'value',
+        ])
+
+        self.assertStates(self.slider, [
+            'enabled', 'sensitive', 'showing', 'visible',
+        ])
+
+        self.assertEqual(3, self.slider.childCount)
+
+        for i in xrange(0, 2):
+            child = self.slider.getChildAtIndex(i)
+            self.assertEqual(pyatspi.ROLE_PUSH_BUTTON, child.role)
+            self.assertEqual('', child.name)
+            self.assertEqual(0, child.childCount)
+
+        for button in (self.large_increase, self.large_decrease):
+            self.assertInterfaces(button, [
+                'accessible', 'action', 'component',
+            ])
+            self.assertStates(button, [
+                'enabled', 'sensitive', 'showing', 'visible',
+            ])
+
+        self.assertInterfaces(self.thumb, [
+            'accessible', 'component',
+        ])
+        self.assertStates(self.thumb, [
+            'enabled', 'focusable', 'sensitive', 'showing', 'visible',
+        ])
+
+    def test_minimum_value(self):
+        self.assertEqual(self.slider_value.minimumValue, 0)
+
+    def test_maximum_value(self):
+        self.assertEqual(self.slider_value.maximumValue, 100)
+
+    def test_minimum_increment(self):
+        self.assertEqual(self.slider_value.minimumIncrement, 0)
+
+    def test_current_value(self):
+        self.slider_value.currentValue = 0
+        self.assertEqual(self.slider_value.currentValue, 0)
+        self.assertEqual(self.label.name, '0')
+
+        listener = EventListener()
+        with listener.listenTo(self.slider):
+            self.slider_value.currentValue = 100
+            self.assertEqual(self.label.name, '100')
+            self.assertEqual(self.slider_value.currentValue, 100)
+
+        assert listener.containsEvent(self.slider,
+                                      'object:property-change:accessible-value',
+                                      qty=1)
+
+        with listener.listenTo(self.slider):
+            self.slider_value.currentValue = 100
+            self.assertEqual(self.label.name, '100')
+            self.assertEqual(self.slider_value.currentValue, 100)
+
+        assert listener.containsEvent(self.slider,
+                                      'object:property-change:accessible-value',
+                                      qty=0)
+
+
+        with listener.listenTo(self.slider):
+            self.slider_value.currentValue = 10000
+            self.assertEqual(self.label.name, '100')
+            self.assertEqual(self.slider_value.currentValue, 100)
+
+        assert listener.containsEvent(self.slider,
+                                      'object:property-change:accessible-value',
+                                      qty=0)
+
+        with listener.listenTo(self.slider):
+            self.slider_value.currentValue = 0
+            self.assertEqual(self.slider_value.currentValue, 0)
+            self.assertEqual(self.label.name, '0')
+
+        assert listener.containsEvent(self.slider,
+                                      'object:property-change:accessible-value',
+                                      qty=1)
+
+    def test_slider_buttons(self):
+        self.slider_value.currentValue = 0
+        self.assertEqual(self.slider_value.currentValue, 0)
+        self.assertEqual(self.label.name, '0')
+
+        listener = EventListener()
+        for value in (25, 50, 75, 100):
+            with listener.listenTo(self.slider):
+                    self.large_increase.click()
+                    self.assertEqual(self.label.name, str(value))
+                    self.assertEqual(self.slider_value.currentValue, value)
+
+            assert listener.containsEvent(self.slider,
+                                          'object:property-change:accessible-value',
+                                          qty=1)
+
+        with listener.listenTo(self.slider):
+                self.large_increase.click()
+                self.assertEqual(self.label.name, '100')
+                self.assertEqual(self.slider_value.currentValue, 100)
+
+        assert listener.containsEvent(self.slider,
+                                      'object:property-change:accessible-value',
+                                      qty=0)
+
+        for i in (75, 50, 25, 0):
+            with listener.listenTo(self.slider):
+                    self.large_decrease.click()
+
+                    sleep(config.MEDIUM_DELAY)
+
+                    self.assertEqual(self.label.name, str(i))
+                    self.assertEqual(self.slider_value.currentValue, i)
+
+            assert listener.containsEvent(self.slider,
+                                          'object:property-change:accessible-value',
+                                          qty=1)
+
+        with listener.listenTo(self.slider):
+                self.large_decrease.click()
+                self.assertEqual(self.label.name, '0')
+                self.assertEqual(self.slider_value.currentValue, 0)
+
+        assert listener.containsEvent(self.slider,
+                                      'object:property-change:accessible-value',
+                                      qty=0)
