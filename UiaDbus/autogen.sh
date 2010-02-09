c97cd3482209a46dc8e8c975a9ef6af372f26d96
Sandy Armstrong <sanfordarmstrong@gmail.com>
2010-02-09 18:50:40 +0000

    In UiaDbus/: 	* autogen.sh: Pull ndesk-dbus from out git branch during autogen. 	* ndesk-dbus/*.cs: Delete copy of ndesk-dbus source from SVN. 	* UiaDbus/Makefile.am: 	* UiaDbus/UiaDbus.dll.config.in: Add a .dll.config file for ndesk-dbus.
    svn path=/trunk/uia2atk/; revision=151138

--- UiaDbus/autogen.sh
+++ UiaDbus/autogen.sh
@@ -79,5 +79,22 @@ $AUTOMAKE --add-missing --gnu $am_opt
 echo "Running $AUTOCONF ..."
 $AUTOCONF
 
+#Set up ndesk-dbus bundling
+echo "Getting a fresh ndesk-dbus git checkout to bundle..."
+if test -d $srcdir/ndesk-dbus-git; then
+  rm -rf $srcdir/ndesk-dbus-git
+  rm $srcdir/ndesk-dbus/*.cs
+fi
+git clone git://gitorious.org/dbus-sharp/dbus-sharp.git $srcdir/ndesk-dbus-git
+(cd $srcdir/ndesk-dbus-git && git checkout -b mono-a11y origin/mono-a11y)
+cp $srcdir/ndesk-dbus-git/src/*.cs $srcdir/ndesk-dbus/
+#TODO: Only copy a specific list, use a .sources file
+(cd $srcdir/ndesk-dbus; rm AssemblyInfo* Server* Daemon.cs TypeRental.cs)
+echo "Internalizing bundled ndesk-dbus source..."
+perl -pi -e 's/public ((class)|(enum)|(struct)|(abstract class)|(interface)|(sealed)|(partial))/internal $1/g' $srcdir/ndesk-dbus/*.cs
+#TODO: Update master to stop using [Obsolete] API
+perl -pi -e 's/(\[Obsolete[^\]]*\])/\/\/$1/g' $srcdir/ndesk-dbus/*.cs
+echo "Completed bundling ndesk-dbus."
+
 echo Running $srcdir/configure $conf_flags "$@" ...
 $srcdir/configure --enable-maintainer-mode $conf_flags "$@" \
