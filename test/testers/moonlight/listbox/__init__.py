f634855285395d43bf88fcff4c69294d057ec20b
Calen Chen <cachen@mono-cvs.ximian.com>
2010-03-05 05:17:07 +0000

    change firefox path
    svn path=/trunk/uia2atk/; revision=153086

--- test/testers/moonlight/listbox/__init__.py
+++ test/testers/moonlight/listbox/__init__.py
@@ -17,7 +17,7 @@ init_dir = path[0]
 uiaqa_path = dirname(dirname(init_dir))
 # Variable the path of Firefox to run the application, Please install
 # Firefox3.5.1 first which is accessible by accerciser
-firefox_path = '/usr/local/bin/firefox'
+firefox_path = '/usr/bin/firefox'
 
 def launchListBox(exe=None):
     '''Launch Moonlight ListBox with accessibility enabled and return a
