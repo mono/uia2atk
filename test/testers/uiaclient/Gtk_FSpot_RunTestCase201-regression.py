82ebc560d67a4278f5a59aa178444e46437837be
Calen Chen <cachen@mono-cvs.ximian.com>
2010-05-17 10:16:47 +0000

    add uiaclient scripts for harness to auto run
    svn path=/trunk/uia2atk/; revision=157429

---
+++ test/testers/uiaclient/Gtk_FSpot_RunTestCase201-regression.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+###############################################################################
+# Written by:  calen chen <cachen@novell.com>
+# Date:        May 12 2010
+# Description: RunTestCase201 for Gtk FSpot application via UIAClientAPI
+###############################################################################
+
+import os
+from sys import path
+
+test_dir = path[0]
+i = test_dir.rfind("/")
+test_path = test_dir[:i]
+dll_path = "uiaclient/Tests/bin/Debug"
+test_dll = "MonoTests.Mono.UIAutomation.UIAClientAPI.dll"
+uiatest_path = os.path.join(test_path, dll_path)
+uiatest_dll = os.path.join(uiatest_path, test_dll)
+app = "f-spot"
+app_path = os.path.join("/usr/bin/", app)
+
+if not os.path.exists(app_path):
+    print "Please install %s to %s" % (app, uiatest_path)
+    
+if not os.path.exists(uiatest_dll):
+    os.system("%s/uiaclient/autogen.sh && make" % test_path)
+
+os.system("nunit-console2 %s -run=MonoTests.Mono.UIAutomation.UIAClientAPI.Gtk.FSpot.RunTestCase201 >%s%s" % \
+(uiatest_dll, uiatest_path, "/Resources/logs"))
+
+
---
+++ test/testers/uiaclient/Moonlight_PuzzleGameTest_RunTestCase302-regression.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+###############################################################################
+# Written by:  calen chen <cachen@novell.com>
+# Date:        May 12 2010
+# Description: RunTestCase302 for Moonlight PuzzleGame application via UIAClientAPI
+###############################################################################
+
+import os
+from sys import path
+
+test_dir = path[0]
+i = test_dir.rfind("/")
+test_path = test_dir[:i]
+dll_path = "uiaclient/Tests/bin/Debug"
+test_dll = "MonoTests.Mono.UIAutomation.UIAClientAPI.dll"
+uiatest_path = os.path.join(test_path, dll_path)
+uiatest_dll = os.path.join(uiatest_path, test_dll)
+    
+if not os.path.exists(uiatest_dll):
+    os.system("%s/uiaclient/autogen.sh && make" % test_path)
+
+os.system("nunit-console2 %s -run=MonoTests.Mono.UIAutomation.UIAClientAPI.Moonlight.PuzzleGameTest.RunTestCase302 >%s%s" % \
+(uiatest_dll, uiatest_path, "/Resources/logs"))
+
+
