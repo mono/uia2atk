65c08999dca1b8ace7c2386ef97d366bb1f000c7
Calen Chen <cachen@mono-cvs.ximian.com>
2010-05-20 10:52:17 +0000

    remove bug515457 reference
    svn path=/trunk/uia2atk/; revision=157610

--- test/testers/winforms/checkedlistbox-regression.py
+++ test/testers/winforms/checkedlistbox-regression.py
@@ -67,8 +67,7 @@ statesCheck(clbFrame.listitem[20], "CheckBoxListItem")
 
 # use keyTab to change the focus from listbox1 to listbox2
 clbFrame.keyCombo("Tab", grabFocus = False)
-# BUG515457: treetable remain 'focused' state after focus is moved
-#statesCheck(clbFrame.listbox1, "List")
+statesCheck(clbFrame.listbox1, "List")
 statesCheck(clbFrame.listbox2, "List", add_states=["focused"])
 
 # use keySpace to check listitem20
@@ -79,8 +78,7 @@ statesCheck(clbFrame.listbox2, "List", add_states=["focused"])
 # press "space" again to uncheck but still focused
 clbFrame.keyCombo("space", grabFocus = False)
 statesCheck(clbFrame.listitem[20], "ListItem", add_states=["focused"])
-# BUG515457: treetable remain 'focused' state after focus is moved
-#statesCheck(clbFrame.listbox1, "List")
+statesCheck(clbFrame.listbox1, "List")
 
 # do click for listitem 1 to rise selected state
 clbFrame.click(clbFrame.listitem[1])
@@ -160,8 +158,7 @@ clbFrame.selectChildAndCheckStates(clbFrame.listbox2,
 clbFrame.clearAccessibleSelection(clbFrame.listbox1)
 sleep(config.SHORT_DELAY)
 statesCheck(clbFrame.listitem[0], "ListItem")
-# BUG515457: treetable remain 'focused' state after focus is moved
-#statesCheck(clbFrame.listbox1, "List")
+statesCheck(clbFrame.listbox1, "List")
 
 clbFrame.clearAccessibleSelection(clbFrame.listbox2)
 sleep(config.SHORT_DELAY)
